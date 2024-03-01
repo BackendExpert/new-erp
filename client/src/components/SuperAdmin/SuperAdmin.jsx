2065613d6fa10ddf33a92726df951c357eab1bc2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 18:43:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -4,12 +4,20 @@ import sidemenu from './SideList'
 import Icons from "@reacticons/ionicons"
 
 const SuperAdmin = () => {
-    const [Sidebar, SetSidebar] = useState()
+    const [Sidebar, SetSidebar] = useState(false)
   return (
     <div className='bg-gray-100 py-2'>
         <div className="bg-white py-4 px-4 rounded my-2 ml-3 h-full shadow-xl border-b-4 border-blue-200">
             <div className="flex justify-between">
                 <h1 className="text-xl font-semibold text-[#3B71CA]">Super Admin</h1>
+                <div className="flex">
+                    <div className="text-[#3B71CA] lg:invisible visible ">
+                        <Icons name={Sidebar ? 'menu' : 'close'} size='large'></Icons>
+                    </div>
+                    <div className="">
+                        
+                    </div>
+                </div>
             </div>
         </div>
         <div className="shadow-2xl rounded py-1 px-4 bg-white lg:w-1/6 h-screen my-2 mx-3 border-r-4 border-blue-200">
