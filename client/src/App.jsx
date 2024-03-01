f7bbb4aea3277b56c97635fbcf2ad999df47eb30
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 14:48:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./components/LoginSignUp/Login"
 import { SignUp } from "./components/LoginSignUp/SignUp"
-import SuperAdminDash from "./components/LoginSignUp/SuperAdminDash"
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} ></Route>
         <Route path="/register" element={<SignUp />} ></Route>
-        <Route path="/superAdmin" element={<SuperAdminDash />} ></Route>
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/LoginSignUp/SuperAdminDash.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SuperAdminDash = () => {
-  return (
-    <div>SuperAdminDash</div>
-  )
-}
-
-export default SuperAdminDash
