dc93031019e457afa9ecb379cdbb5b0e946dfcdc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 15:35:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -4,15 +4,15 @@ import sidemenu from './SideList'
 const SuperAdmin = () => {
   return (
     <div className='bg-white'>
-        <div className="rounded py-4 px-4 bg-gray-200 lg:w-1/6 h-full my-2 mx-3">
+        <div className="rounded py-4 px-4 bg-gray-100 lg:w-1/6 h-full my-2 mx-3 border-r-4">
             <h1 className="text-xl font-semibold">Super Admin</h1>
             <hr className='my-2 border border-gray-400'/>
-            <div className="py-4">
+            <div className="">
                 {
                     sidemenu.map((sidem) => (
-                        <div className="flex py-2">
-                            <p>{sidem.icon}</p>
-                            <p>{sidem.name}</p>
+                        <div className="flex bg-gray-300 mt-2 rounded py-1">
+                            <p className='pl-2'>{sidem.icon}</p>
+                            <p className='pt-1 pl-2'>{sidem.name}</p>
                         </div>
                     ))
                 }
