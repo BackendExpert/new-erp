55759f242e0239241531cf8b9d040da9df1868aa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 18:33:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -9,7 +9,7 @@ const SuperAdmin = () => {
     <div className='bg-gray-100 py-2'>
         <div className="bg-white py-4 px-4 rounded my-2 ml-3 h-full shadow-xl border-b-4 border-blue-200">
             <div className="flex justify-between">
-                <h1 className="text-xl font-semibold">Super Admin</h1>
+                <h1 className="text-xl font-semibold text-[#3B71CA]">Super Admin</h1>
             </div>
         </div>
         <div className="rounded py-1 px-4 bg-white lg:w-1/6 h-screen my-2 mx-3 border-r-4 border-blue-200">
