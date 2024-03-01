ed76aa35570a10cd239cc52de2f2a5a954ea819e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 15:04:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -1,11 +1,14 @@
 const sidemenu = [
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
-    {name: "",  link: "#", icon: ""},
+    {name: "Employess",  link: "#", icon: ""},
+    {name: "Divisions",  link: "#", icon: ""},
+    {name: "Projects",  link: "#", icon: ""},
+    {name: "Program",  link: "#", icon: ""},
+    {name: "Designation",  link: "#", icon: ""},
+    {name: "Vehicle",  link: "#", icon: ""},
+    {name: "Equipment",  link: "#", icon: ""},
+    {name: "Library",  link: "#", icon: ""},
+    {name: "Account",  link: "#", icon: ""},
+    {name: "Profile",  link: "#", icon: ""}
 ]
 
 export default sidemenu
