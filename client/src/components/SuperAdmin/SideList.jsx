ebf4e86c598b97ed6007362563e590fb70755363
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 15:21:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -5,12 +5,12 @@ const sidemenu = [
     {name: "Divisions",  link: "#", icon: <Icons name="business"></Icons>},
     {name: "Projects",  link: "#", icon: <Icons name="newspaper"></Icons>},
     {name: "Program",  link: "#", icon: <Icons name="book"></Icons>},
-    {name: "Designation",  link: "#", icon: <Icons name="id-card"></Icons>},
+    {name: "Designation",  link: "#", icon: <Icons name="easel"></Icons>},
     {name: "Vehicle",  link: "#", icon: <Icons name="car"></Icons>},
-    {name: "Equipment",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Library",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Account",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Profile",  link: "#", icon: <Icons name=""></Icons>}
+    {name: "Equipment",  link: "#", icon: <Icons name="build"></Icons>},
+    {name: "Library",  link: "#", icon: <Icons name="library"></Icons>},
+    {name: "Accounts",  link: "#", icon: <Icons name="people-circle"></Icons>},
+    {name: "Profile",  link: "#", icon: <Icons name="person"></Icons>}
 ]
 
 export default sidemenu
