556787657db2528c4626e175f8cbd47056a555d1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 14:01:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -6,17 +6,17 @@ export const SignUp = () => {
     <div className='bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'>
         <div className="lg:py-24 lg:mx-40 py-16 mx-8">
             <div className="bg-white lg:py-8 lg:px-24 py-10 px-8 rounded-md">
-                <h1 className="text-3xl font-semibold">Login Here</h1>
+                <h1 className="text-3xl font-semibold">Register Here</h1>
                 
                 <div className="pl-4">
                     <form>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Email : </label>
-                            <input type="email" className="my-2 w-full h-12 border rounded pl-2" />
+                            <input type="email" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Email Address'/>
                         </div>
                         <div className="my-4">
                             <label htmlFor="" className='text-xl'>Email : </label>
-                            <input type="password" className="my-2 w-full h-12 border rounded pl-2" />
+                            <input type="password" className="my-2 w-full h-11 border rounded pl-2" required placeholder='Password'/>
                         </div>
                         <div className="my-4">
                             <button type="submit" className="w-1/2 h-12 border border-blue-500 rounded text-blue-500 duration-500 hover:text-white hover:bg-blue-500">Login</button>
