422d27b4bccb369c76ae85aba9321f13d2ca7081
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 18:20:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -6,17 +6,17 @@ import Icons from "@reacticons/ionicons"
 const SuperAdmin = () => {
     const [Sidebar, SetSidebar] = useState()
   return (
-    <div className='bg-white'>
-        <div className="bg-gray-100 py-4 px-4 rounded my-2 ml-3 h-full ">
+    <div className='bg-gray-200 py-2'>
+        <div className="bg-white py-4 px-4 rounded my-2 ml-3 h-full ">
             <div className="flex justify-between">
                 <h1 className="text-xl font-semibold">Super Admin</h1>
             </div>
         </div>
-        <div className="rounded py-1 px-4 bg-gray-100 lg:w-1/6 h-full my-2 mx-3 border-r-4">
+        <div className="rounded py-1 px-4 bg-white lg:w-1/6 h-screen my-2 mx-3 border-r-4">
             <div className="">
                 {
                     sidemenu.map((sidem) => (
-                        <div className="flex bg-gray-300 mt-2 rounded py-1">
+                        <div className="flex bg-blue-100 mt-2 rounded py-1 text-blue-700">
                             <p className='pl-2'>{sidem.icon}</p>
                             <p className='pt-1 pl-2'>{sidem.name}</p>
                         </div>
