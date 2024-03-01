aee525de055fb45971c0878d439051156e5ff070
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 13:46:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/Login.jsx
+++ client/src/components/LoginSignUp/Login.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='bg-[url(https://wallpapercave.com/wp/qkz7ffi.jpg)] bg-center bg-cover h-screen w-full'>
+        <div className="lg:py-24 lg:mx-40 py-16 mx-8">
+            <div className="bg-white lg:py-20 lg:px-24 py-10 px-8 rounded-md">
+                <h1 className="text-2xl font-semibold">Login Here</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
