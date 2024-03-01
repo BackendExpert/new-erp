e97fb997c9641d4ae7097903ec39b38a9d115b57
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 15:44:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/SuperAdmin/NavList.jsx
@@ -0,0 +1,11 @@
+const navlists = [
+    {nmae: "Home", link: "#"},
+    {nmae: "Leave Requests", link: "#"},
+    {nmae: "Vehicle Requests", link: "#"},
+    {nmae: "SRN Requests", link: "#"},
+    {nmae: "Work Requests", link: "#"},
+    {nmae: "Getepass Requests", link: "#"},
+    {nmae: "Logout", link: "#"}
+]
+
+export default navlists
