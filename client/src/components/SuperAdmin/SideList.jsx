cf88ffe9bb272d70e476341b2b65286b5b60a8f0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 15:17:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -2,11 +2,11 @@ import Icons from "@reacticons/ionicons"
 
 const sidemenu = [
     {name: "Employess",  link: "#", icon: <Icons name="people"></Icons>},
-    {name: "Divisions",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Projects",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Program",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Designation",  link: "#", icon: <Icons name=""></Icons>},
-    {name: "Vehicle",  link: "#", icon: <Icons name=""></Icons>},
+    {name: "Divisions",  link: "#", icon: <Icons name="business"></Icons>},
+    {name: "Projects",  link: "#", icon: <Icons name="newspaper"></Icons>},
+    {name: "Program",  link: "#", icon: <Icons name="book"></Icons>},
+    {name: "Designation",  link: "#", icon: <Icons name="id-card"></Icons>},
+    {name: "Vehicle",  link: "#", icon: <Icons name="car"></Icons>},
     {name: "Equipment",  link: "#", icon: <Icons name=""></Icons>},
     {name: "Library",  link: "#", icon: <Icons name=""></Icons>},
     {name: "Account",  link: "#", icon: <Icons name=""></Icons>},
