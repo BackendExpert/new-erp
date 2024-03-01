55f560ecf805df929180302011a8a0c15b2421b5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 19:54:07 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/NavList.jsx
+++ client/src/components/SuperAdmin/NavList.jsx
@@ -1,11 +1,11 @@
 const navlists = [
-    {nmae: "Home", link: "#"},
-    {nmae: "Leave Requests", link: "#"},
-    {nmae: "Vehicle Requests", link: "#"},
-    {nmae: "SRN Requests", link: "#"},
-    {nmae: "Work Requests", link: "#"},
-    {nmae: "Getepass Requests", link: "#"},
-    {nmae: "Logout", link: "#"}
+    {name: "Home", link: "#"},
+    {name: "Leave Requests", link: "#"},
+    {name: "Vehicle Requests", link: "#"},
+    {name: "SRN Requests", link: "#"},
+    {name: "Work Requests", link: "#"},
+    {name: "Getepass Requests", link: "#"},
+    {name: "Logout", link: "#"}
 ]
 
 export default navlists
