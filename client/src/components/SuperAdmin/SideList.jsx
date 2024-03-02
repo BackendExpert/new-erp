f120f14d235e809651fdf37bcd95e55725e9d0e4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 10:24:22 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -1,7 +1,18 @@
 import Icons from "@reacticons/ionicons"
 
 const SideList = () => {
-    
+    const sidemenu = [
+        {name: "Employess",  link: "#", icon: <Icons name="people" size="large"></Icons>},
+        {name: "Divisions",  link: "#", icon: <Icons name="business" size="large"></Icons>},
+        {name: "Projects",  link: "#", icon: <Icons name="newspaper" size="large"></Icons>},
+        {name: "Program",  link: "#", icon: <Icons name="book" size="large"></Icons>},
+        {name: "Designation",  link: "#", icon: <Icons name="easel" size="large"></Icons>},
+        {name: "Vehicle",  link: "#", icon: <Icons name="car" size="large"></Icons>},
+        {name: "Equipment",  link: "#", icon: <Icons name="build" size="large"></Icons>},
+        {name: "Library",  link: "#", icon: <Icons name="library" size="large"></Icons>},
+        {name: "Accounts",  link: "#", icon: <Icons name="people-circle" size="large"></Icons>},
+        {name: "Profile",  link: "#", icon: <Icons name="person" size="large"></Icons>}
+    ]
     return (
       <div className='bg-gray-100 py-2'>
          SideList
@@ -14,18 +25,5 @@ export default SideList
 
 
 
-// const sidemenu = [
-//     {name: "Employess",  link: "#", icon: <Icons name="people" size="large"></Icons>},
-//     {name: "Divisions",  link: "#", icon: <Icons name="business" size="large"></Icons>},
-//     {name: "Projects",  link: "#", icon: <Icons name="newspaper" size="large"></Icons>},
-//     {name: "Program",  link: "#", icon: <Icons name="book" size="large"></Icons>},
-//     {name: "Designation",  link: "#", icon: <Icons name="easel" size="large"></Icons>},
-//     {name: "Vehicle",  link: "#", icon: <Icons name="car" size="large"></Icons>},
-//     {name: "Equipment",  link: "#", icon: <Icons name="build" size="large"></Icons>},
-//     {name: "Library",  link: "#", icon: <Icons name="library" size="large"></Icons>},
-//     {name: "Accounts",  link: "#", icon: <Icons name="people-circle" size="large"></Icons>},
-//     {name: "Profile",  link: "#", icon: <Icons name="person" size="large"></Icons>}
-// ]
 
-// export default sidemenu
 
