5164673a7a7ee4b7c64a5593b90985eb17cebdbf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 10:29:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Login from "./components/LoginSignUp/Login"
 import { SignUp } from "./components/LoginSignUp/SignUp"
 import SuperAdmin from "./components/SuperAdmin/SuperAdmin"
+import About from "./components/AdminTest/About"
+import Dash from "./components/AdminTest/Dash"
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<Login />} ></Route>
         <Route path="/register" element={<SignUp />} ></Route>
         <Route path="/superAdmin" element={<SuperAdmin />} ></Route>
+        <Route path="/dash" element={<Dash />} ></Route>
+        <Route path="/about" element={<About />} ></Route>
       </Routes>
     </BrowserRouter>
   )
