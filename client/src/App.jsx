1799d899d04f2e053799f7f0988933f7f2ed9501
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 10:31:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { SignUp } from "./components/LoginSignUp/SignUp"
 import SuperAdmin from "./components/SuperAdmin/SuperAdmin"
 import About from "./components/AdminTest/About"
 import Dash from "./components/AdminTest/Dash"
+import SideList from "./components/SuperAdmin/SideList"
 
 export default function App() {
   return (
@@ -12,9 +13,14 @@ export default function App() {
         <Route path="/" element={<Login />} ></Route>
         <Route path="/register" element={<SignUp />} ></Route>
         <Route path="/superAdmin" element={<SuperAdmin />} ></Route>
-        <Route path="/dash" element={<Dash />} ></Route>
-        <Route path="/about" element={<About />} ></Route>
+      
       </Routes>
+      <SideList>
+        <Routes>
+          <Route path="/dash" element={<Dash />} ></Route>
+          <Route path="/about" element={<About />} ></Route>
+        </Routes>
+      </SideList>
     </BrowserRouter>
   )
 }
