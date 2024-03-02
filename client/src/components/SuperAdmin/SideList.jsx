ff7b189da634e0ac2166ee5cee5975857cbdb1b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-02 11:38:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -18,7 +18,7 @@ const SideList = () => {
       <div className='border-r-4 border-blue-300 shadow-xl my-4 mx-2 rounded bg-white lg:w-72 h-full pl-8 py-4'>
         <div className="py-2">
             <div className="text-2xl pb-4 text-[#3B71CA] font-bold">SuperAdmin</div>
-            <div className="text-xl text-gray-400">
+            <div className="text-xl text-gray-400 duration-500 hover:text-[#3B71CA]">
                 <Link to={'/superAdmin'}>
                     Dashboard
                 </Link>
@@ -30,7 +30,7 @@ const SideList = () => {
             {
                 sidemenu.map((sidem) => (
                     <Link to={sidem.link}>
-                    <div className="flex py-4 text-gray-400">                        
+                    <div className="flex py-4 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
                         <p>{sidem.icon}</p>
                         <p className="pt-2 pl-2">{sidem.name}</p>                        
                     </div>
