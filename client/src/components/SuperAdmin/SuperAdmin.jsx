b9d9a1581458eacf86c41be6f6ed21c4346fff41
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 03:25:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -7,6 +7,14 @@ import { useEffect } from "react";
 const SuperAdmin = ({children}) => {
     const navigete = useNavigate();
 
+    // this is for prevent unauthorized access for this page
+    /*
+    
+      exmaple
+      Any use can access this after login there accounts 
+      using following code prevent unauthorized access
+       
+    */
     useEffect(() => {
       const RoleUser = secureLocalStorage.getItem("loginNew");
     
