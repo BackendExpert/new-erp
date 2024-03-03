41c6d22b7ccb076ebd0c1b84cfd1314899ec9c5c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-03 20:54:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/navList.jsx
+++ client/src/components/NavBar/navList.jsx
@@ -2,18 +2,34 @@ import { useState } from "react"
 
 const navList = () => {
     const navLists = [
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""},
-        {name: "", link: ""}       
+        {name: "Leave Requests", link: ""},
+        {name: "Vehicle Reservations", link: ""},
+        {name: "SRN Requests", link: ""},
+        {name: "Work Requests", link: ""},
+        {name: "Gatepass Requests", link: ""},
+        {name: "Increment Requests", link: ""},
+        {name: "Profile", link: ""},
+        {name: "Logout", link: "", desc: "logout"}       
     ];
   return (
-    <div className="border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
-        List Name
+    <div className="flex justify-between border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
+        NIFS
+        <div className="flex">
+            {
+                navLists.map((nav) => {
+                    if(nav.desc === "logout"){
+                        return (
+                            <div className="text-red-500 px-4">{nav.name}</div>
+                        )
+                    }
+                    else{
+                        return (
+                            <div className="text-blue-500 px-4">{nav.name}</div>
+                        )
+                    }
+                })
+            }
+        </div>
     </div>
   )
 }
