b6f078445dab7cd2a29f4b46c15def1b9a02cefe
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 01:51:13 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -15,50 +15,50 @@ const SuperAdmin = ({children}) => {
                   <hr className="mt-2 border-blue-100 border-2" />
 
                   <div className="">
-                      <div className="lg:grid grid-cols-3 gap-6">
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                      <div className="lg:grid grid-cols-3 gap-6 my-4">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">SuperAdmins</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Admins</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Head of Departments</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Transport Officers</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Librarian</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Labmanager</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Accountant</p>
                             <p className="text-2xl font-semibold">50</p>
                           </div>
                         </div>
-                        <div className="rounded bg-green-500 py-10 px-6 lg:my-0 my-2">
+                        <div className="rounded-xl border-4 border-green-500 text-green-500 cursor-pointer py-10 px-6 lg:my-0 my-2 duration-500 hover:bg-green-500 hover:text-white">
                           <div className="flex justify-between ">
                             <p className="text-xl font-semibold">Users</p>
                             <p className="text-2xl font-semibold">50</p>
