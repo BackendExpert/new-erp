4cd2f052990793d02dbebd215230441ca5c5cd5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 02:23:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/NavBar/navListAdmin.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import Icons from "@reacticons/ionicons"
+import { useNavigate } from "react-router-dom"
+
+const navList = () => {
+    const [navopen, SetNavOpen] = useState()
+
+    const nagigate = useNavigate();
+
+    const navLists = [
+        {name: "Home", link: ""},
+        {name: "Leave Requests", link: ""},
+        {name: "Vehicle Reservations", link: ""},
+        {name: "SRN Requests", link: ""},
+        {name: "Work Requests", link: ""},
+        {name: "Gatepass Requests", link: ""},
+        {name: "Submit Increment", link: ""},
+        {name: "Logout", link: "", desc: "logout"}       
+    ];
+
+    const logout = () => {
+        localStorage.removeItem('Logintoken');
+        nagigate('/')
+    }
+
+  return (
+    <div className="flex justify-between border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
+        NIFS ERP
+        <div className="text-xl absolute cursor-pointer lg:hidden right-8" onClick={() => SetNavOpen(!navopen)}>
+            <Icons name={navopen ? 'caret-down' : 'caret-up'} ></Icons>
+        </div>
+        <div className={`rounded lg:border-0 border-b-4 border-blue-400 lg:flex lg:items-center absolute lg:static bg-white transition-all duration-500 ease-in lg:mt-0 mt-12 ${navopen ? 'opacity-100 ':'lg:opacity-100 opacity-0'}`}>
+
+            {
+                navLists.map((nav) => {
+                    if(nav.desc === "logout"){
+                        return (
+                            <div onClick={logout} className="cursor-pointer text-red-500 px-4 lg:py-0 py-4">{nav.name}</div>
+                        )
+                    }
+                    else{
+                        return (
+                            <div className="cursor-pointer text-blue-500 px-4 lg:py-0 py-4 lg:border-0 border-b-4 border-blue-200">{nav.name}</div>
+                        )
+                    }
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default navList
