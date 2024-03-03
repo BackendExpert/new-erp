544ade56ae493e020e154e8843344aa34904d74a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-03 07:38:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 8081
 
 //make connection between dbsever and node app
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "db_erp"
+})
+
+// middleware
+app.use(express.json())
 
 
 //check the server is working
