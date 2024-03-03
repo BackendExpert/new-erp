6be10a313d9035438aea0f4e285fdc88144858ba
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-03 21:26:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SuperAdmin.jsx
+++ client/src/components/SuperAdmin/SuperAdmin.jsx
@@ -11,7 +11,8 @@ const SuperAdmin = ({children}) => {
             <div className="w-full mx-2">
               <Navlist />
               <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
-                  Super Admin Dash
+                  <h1 className="text-2xl">Welcome to SuperAdmin Dashboard</h1>
+                  <hr className="mt-2 border-blue-100 border-2" />
               </div>
             </div>
 
