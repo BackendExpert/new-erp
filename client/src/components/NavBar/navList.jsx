361865bfb55cb8d6acb800a5c2c4bf31e9a814a6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-03 21:22:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/navList.jsx
+++ client/src/components/NavBar/navList.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import Icons from "@reacticons/ionicons"
 
 const navList = () => {
+    const [navopen, SetNavOpen] = useState()
+
     const navLists = [
         {name: "Leave Requests", link: ""},
         {name: "Vehicle Reservations", link: ""},
@@ -11,21 +14,25 @@ const navList = () => {
         {name: "Profile", link: ""},
         {name: "Logout", link: "", desc: "logout"}       
     ];
-    
+
   return (
     <div className="flex justify-between border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
         NIFS
-        <div className="flex">
+        <div className="text-xl absolute cursor-pointer lg:hidden right-8" onClick={() => SetNavOpen(!navopen)}>
+            <Icons name={navopen ? 'caret-down' : 'caret-up'} ></Icons>
+        </div>
+        <div className={`lg:flex lg:items-center absolute lg:static bg-white transition-all duration-500 ease-in lg:mt-0 mt-12 ${navopen ? 'opacity-100 ':'lg:opacity-100 opacity-0'}`}>
+
             {
                 navLists.map((nav) => {
                     if(nav.desc === "logout"){
                         return (
-                            <div className="text-red-500 px-4">{nav.name}</div>
+                            <div className="text-red-500 px-4 lg:py-0 py-4">{nav.name}</div>
                         )
                     }
                     else{
                         return (
-                            <div className="text-blue-500 px-4">{nav.name}</div>
+                            <div className="text-blue-500 px-4 lg:py-0 py-4">{nav.name}</div>
                         )
                     }
                 })
