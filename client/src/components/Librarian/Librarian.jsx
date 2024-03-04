df25bcfd03c91c1b6d8cbd302ae419a45ff42ca5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 11:32:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Librarian/Librarian.jsx
+++ client/src/components/Librarian/Librarian.jsx
@@ -1,11 +1,31 @@
 import LibSide from "./LibSide"
 
 const Librarian = () => {
+  const navigete = useNavigate();
+
+  // this is for prevent unauthorized access for this page
+  /*
+  
+    exmaple
+    Any use can access this after login there accounts 
+    using following code prevent unauthorized access
+     
+  */
+  useEffect(() => {
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+  
+    if(RoleUser !== "Librarian"){
+      navigete('/');
+      localStorage.clear();
+    }
+  }, []);
+
+
   return (
     <div className="bg-gray-200">
       <div className="flex">
         <LibSide />
-        
+
       </div>
     </div>
   )
