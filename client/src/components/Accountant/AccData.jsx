8956a8f739971dc3ff044ab314d255de9f4dc693
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 12:12:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accountant/AccData.jsx
+++ client/src/components/Accountant/AccData.jsx
@@ -1,5 +1,27 @@
+import secureLocalStorage from "react-secure-storage"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 const AccData = () => {
+    const navigete = useNavigate();
+
+    // this is for prevent unauthorized access for this page
+    /*
+    
+      exmaple
+      Any use can access this after login there accounts 
+      using following code prevent unauthorized access
+       
+    */
+    useEffect(() => {
+      const RoleUser = secureLocalStorage.getItem("loginNew");
+    
+      if(RoleUser !== "Accountant"){
+        navigete('/');
+        localStorage.clear();
+      }
+    }, []);
+  
   return (
     <div>AccData</div>
   )
