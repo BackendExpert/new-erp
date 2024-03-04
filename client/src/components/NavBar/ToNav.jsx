a33d857b3da086b68b8d5d57a138bbc33398bf8c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 10:42:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/ToNav.jsx
+++ client/src/components/NavBar/ToNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ToNav = () => {
   return (
-    <div>ToNav</div>
+    <div className='rounded shadow-xl border-b-4 border-blue-300 bg-white my-4 py-4 px-4'>ToNav</div>
   )
 }
 
