a193ab30874b1625945e3025e4b29266d15cc507
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 10:47:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/ToNav.jsx
+++ client/src/components/NavBar/ToNav.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToNav = () => {
+    const [navopen, SetNavOpen] = useState();
+
+    const navlists = [
+        {name: "Recommend Leave", link: ""},
+        {name: "Recommend Reservations", link: ""},
+        {name: "Recommend WRs", link: ""},
+        {name: "Recommend SRN", link: ""},
+        {name: "Recommend Gatepass", link: ""},
+        {name: "Request Leave", link: ""},
+        {name: "Reserve Vehicle", link: ""},
+        {name: "Submit SRN", link: ""},
+        {name: "Request Work", link: ""},
+        {name: "Request Gatepass", link: ""}
+    ] 
   return (
     <div className='rounded shadow-xl border-b-4 border-blue-300 bg-white my-4 py-4 px-4'>ToNav</div>
   )
