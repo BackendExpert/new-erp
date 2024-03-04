52066c87766619954dbac6678e8172f2d080e8d3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 12:23:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LabManager/LabManager.jsx
+++ client/src/components/LabManager/LabManager.jsx
@@ -1,7 +1,38 @@
+import secureLocalStorage from "react-secure-storage"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import LabSide from "./LabSide"
+import Navlist from "../NavBar/navList"
 
 const LabManager = () => {
+  const navigete = useNavigate();
+
+  // this is for prevent unauthorized access for this page
+  /*
+  
+    exmaple
+    Any use can access this after login there accounts 
+    using following code prevent unauthorized access
+     
+  */
+  useEffect(() => {
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+  
+    if(RoleUser !== "Labmanager"){
+      navigete('/');
+      localStorage.clear();
+    }
+  }, []);
   return (
-    <div>LabManager</div>
+    <div className="bg-gray-200">
+      <div className="flex">
+        <LabSide />
+        <div className="w-full mx-2">
+          <Navlist />
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
