89c38b5708b819ee9ae53d31456db9c073bd3f60
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 09:21:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- db/db_erp_users.sql
+++ db/db_erp_users.sql
@@ -31,7 +31,7 @@ CREATE TABLE `users` (
   `create_at` timestamp NOT NULL,
   `update_at` timestamp NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (5,'Jehan','jehan@123.com','SuperAdmin','$2a$10$ucr7KfcrQ18HEnIys9PWw.SWolOSZzp6D0U6iVaknCe4EWbC8.2by','2024-03-03 04:58:06','2024-03-03 04:58:06'),(6,'Amara','amara@456.com','HOD','$2a$10$oaTOVDpogZGN6fcq8HsH2et0WUtSPa0JVEK.OMG7aEFFJPqKmzTtW','2024-03-03 04:58:21','2024-03-03 04:58:21');
+INSERT INTO `users` VALUES (5,'Jehan','jehan@123.com','SuperAdmin','$2a$10$ucr7KfcrQ18HEnIys9PWw.SWolOSZzp6D0U6iVaknCe4EWbC8.2by','2024-03-03 04:58:06','2024-03-03 04:58:06'),(6,'Amara','amara@456.com','HOD','$2a$10$oaTOVDpogZGN6fcq8HsH2et0WUtSPa0JVEK.OMG7aEFFJPqKmzTtW','2024-03-03 04:58:21','2024-03-03 04:58:21'),(7,'kamal','kamal@789.com','Admin','$2a$10$Gw3RpY4a6e1tJS4dBbyz7uGKKb9TTJ6whs7Rig25CLz6gk7CINjF2','2024-03-03 20:04:37','2024-03-03 20:04:37'),(8,'nimal','nimal@741.com','TO','$2a$10$gsRBzV/ikAZKMkef6tGw4emRwFhL8ZvPJnG0G3k4oB0y8Ly1S4LJ.','2024-03-03 20:05:55','2024-03-03 20:05:55'),(9,'some','some@852.com','Librarian','$2a$10$UtW2sZMsPUTq33iz31Ny4erhHzVlfZhYLOo1ThntrBKMrGxYp1ADu','2024-03-03 20:06:28','2024-03-03 20:06:28'),(10,'perera','perera@963.com','Labmanager','$2a$10$fNjdqO4SVeIGwGSzsN9KceyuoxXsJ.ot369seDAN2idgCatZKNSDO','2024-03-03 20:06:52','2024-03-03 20:06:52'),(11,'nimali','nimali@951.com','Accountant','$2a$10$KFUluX0fOYyRpQkGg47ntOn0U1HD.Q1O3NoDgNxqSNFE4hf8zfMBy','2024-03-03 20:07:19','2024-03-03 20:07:19'),(12,'kamala','kamala@753.com','User','$2a$10$33np5e9XT4qkBJ3dRiiDxOGT1m2lxgjn4ucfx6dPqeVRGxd1Mnw6q','2024-03-03 20:07:33','2024-03-03 20:07:33'),(13,'abcd','abcd@abcd.com','Admin','$2a$10$62jc9FOi5Oh8ocJigycv4.4DRAg4m/HzdMTCEfmMSebjnl8v.zAMm','2024-03-04 01:02:14','2024-03-04 01:02:14');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-03 10:32:01
+-- Dump completed on 2024-03-04  6:49:47
