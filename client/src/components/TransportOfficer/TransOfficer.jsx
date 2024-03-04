3483e07831c7be7e2d5966d65ddd09a661fa0931
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 10:40:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/TransportOfficer/TransOfficer.jsx
+++ client/src/components/TransportOfficer/TransOfficer.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import  secureLocalStorage  from  "react-secure-storage";
 import { useNavigate } from "react-router-dom"
 import ToSide from "./ToSide";
+import ToNav from "../NavBar/ToNav";
 
 const TransOfficer = ({children}) => {
   
@@ -28,8 +29,8 @@ const TransOfficer = ({children}) => {
     <div className="bg-gray-200">
       <div className="flex">
         <ToSide />
-        <div className="">
-          TO
+        <div className="w-full mx-2">
+            <ToNav />
         </div>
       </div>
     </div>
