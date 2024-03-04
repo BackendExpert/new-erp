6e3cc52b90551b929b9a321d1cd731e7d1e62480
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 10:33:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/TransportOfficer/ToSide.jsx
+++ client/src/components/TransportOfficer/ToSide.jsx
@@ -2,6 +2,7 @@ import Icons from "@reacticons/ionicons"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
+
 const ToSide = () => {
     const [openSide, SetSideOpen] = useState();
 
--- client/src/components/TransportOfficer/TransOfficer.jsx
+++ client/src/components/TransportOfficer/TransOfficer.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useEffect } from "react"
+import  secureLocalStorage  from  "react-secure-storage";
+import { useNavigate } from "react-router-dom"
 
 const TransOfficer = ({children}) => {
+  
+  const navigete = useNavigate();
+
+  // this is for prevent unauthorized access for this page
+  /*
+  
+    exmaple
+    Any use can access this after login there accounts 
+    using following code prevent unauthorized access
+     
+  */
+  useEffect(() => {
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+  
+    if(RoleUser !== "TO"){
+      navigete('/');
+      localStorage.clear();
+    }
+  }, []);
+
   return (
     <div>TransOfficer</div>
   )
