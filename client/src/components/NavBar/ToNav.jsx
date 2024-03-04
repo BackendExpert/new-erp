1db3b163e4ef919769a1686d51a8205810e7c309
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 11:00:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/ToNav.jsx
+++ client/src/components/NavBar/ToNav.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Icons from "@reacticons/ionicons"
 
 const ToNav = () => {
     const [navopen, SetNavOpen] = useState();
@@ -16,7 +17,23 @@ const ToNav = () => {
         {name: "Request Gatepass", link: ""}
     ] 
   return (
-    <div className='rounded shadow-xl border-b-4 border-blue-300 bg-white my-4 py-4 px-4'>ToNav</div>
+    <div className='flex justify-between rounded shadow-xl border-b-4 border-blue-300 bg-white my-4 py-4 px-4'>
+        <div className="">NIFS ERP</div>
+        <div className={`lg:top-0 top-5 text-xl cursor-pointer right-8"`} onClick={() => SetNavOpen(!navopen)}>
+            <Icons name={navopen ? 'close' : 'menu'} ></Icons>
+        </div>
+        <div className={`lg:right-8 px-4 rounded lg:border-0 border-b-4 border-blue-400 absolute bg-white transition-all mt-12 ${navopen ? 'visible':'invisible'}`}>
+            {
+                navlists.map((nav) => {
+                    return (
+                        <div className="">
+                            <div className="cursor-pointer text-blue-500 py-4 border-b-4 border-blue-200">{nav.name}</div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
   )
 }
 
