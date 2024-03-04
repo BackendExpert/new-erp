a1360f820ca17724c634b976dfa6c7f521dafa72
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 06:37:04 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,15 +1,36 @@
 import SideListAdmin from "./AdminSide"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import NavlistAdmin from "../NavBar/navListAdmin"
 import SummaryDash from "../SummaryDash/SummaryDash"
+import { useEffect } from "react"
+import  secureLocalStorage  from  "react-secure-storage";
 
 const Admin = () => {
+  const navigete = useNavigate();
+
+  // this is for prevent unauthorized access for this page
+  /*
+  
+    exmaple
+    Any use can access this after login there accounts 
+    using following code prevent unauthorized access
+     
+  */
+  useEffect(() => {
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+  
+    if(RoleUser !== "Admin"){
+      navigete('/');
+      localStorage.clear();
+    }
+  }, []);
+
   return (
     <div className="bg-gray-200">
         <div className="flex">
-            {/* <SideListAdmin/> */}
+            <SideListAdmin/>
             <div className="w-full mx-2">
-                {/* <NavlistAdmin /> */}
+                <NavlistAdmin />
                 <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
                   <h1 className="text-2xl">Welcome to Admin Dashboard</h1>
                   <hr className="mt-2 border-blue-100 border-2" />
