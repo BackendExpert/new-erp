fcc7d6d07fc1124ec3b5532a105d5cb97ea17989
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 11:58:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accountant/AccSide.jsx
+++ client/src/components/Accountant/AccSide.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const AccSide = () => {
+    const [openSide, SetSideOpen] = useState();
+
+    const sidemenu = [
+        {name: "Employess",  link: "/dash", icon: <Icons name="people" size="large"></Icons>},
+        {name: "Divisions",  link: "/about", icon: <Icons name="business" size="large"></Icons>},
+        {name: "Projects",  link: "#", icon: <Icons name="newspaper" size="large"></Icons>},
+        {name: "Program",  link: "#", icon: <Icons name="book" size="large"></Icons>},
+        {name: "Designation",  link: "#", icon: <Icons name="easel" size="large"></Icons>},
+        {name: "Vehicle",  link: "#", icon: <Icons name="car" size="large"></Icons>},
+        {name: "Equipment",  link: "#", icon: <Icons name="build" size="large"></Icons>},
+        {name: "Increment",  link: "#", icon: <Icons name="cash" size="large"></Icons>},
+        {name: "Profile",  link: "#", icon: <Icons name="person" size="large"></Icons>}
+    ]
   return (
-    <div>AccSide</div>
+    <div className={`${openSide ? 'w-[300px]' : 'w-[80px]' } duration-500 border-r-4 border-blue-300 shadow-xl my-4 mx-2 rounded bg-white w-screen h-full pl-4 py-4 `}>
+        <div className="py-2">
+            <div className="flex">
+            <div className="text-[#3B71CA] pt-1" onClick={() => SetSideOpen(!openSide)}><Icons size="large" name="menu"></Icons></div>
+                <div className={`text-2xl pb-4 text-[#3B71CA] font-bold ${!openSide && 'scale-0'}`}>Admin</div>                    
+            </div>
+            <div className={`pl-2 text-xl text-gray-400 duration-500 hover:text-[#3B71CA]`}>
+                <Link to={'/Admin'}>
+                    {openSide ? <p>Dashbord</p> : <Icons name="speedometer"></Icons> }
+                </Link>
+            </div>
+            <hr className="mt-2 mr-4 border-b-2 border-blue-300"/>
+        </div>
+
+        <div className="pl-2">
+            {
+                sidemenu.map((sidem, index) => (
+                    <Link to={sidem.link}>
+                        <div className="flex py-2 text-gray-400 duration-500 hover:text-[#3B71CA]">                        
+                            <p>{sidem.icon}</p>
+                            <p className={`pt-2 pl-2 ${!openSide && 'scale-0'}`}>{sidem.name}</p>                        
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+    
+    </div>
+
   )
 }
 
