69ab4d7a8cdd379efa569ef7231e8d917c1ddef1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-04 09:39:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/HOD/HeadSide.jsx
@@ -0,0 +1,11 @@
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+const HeadSide = () => {
+  return (
+    <div>HeadSide</div>
+  )
+}
+
+export default HeadSide
