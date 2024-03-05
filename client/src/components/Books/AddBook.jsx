d48feabeb24e96b59580b884ead563555945b4ef
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-05 14:04:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Books/AddBook.jsx
+++ client/src/components/Books/AddBook.jsx
@@ -15,6 +15,18 @@ const AddBook = () => {
     SuperAdmin, Librarian
   
   */
+
+    // headleBack according to current login user role
+
+    const headleBack = () => {
+      if(RoleUser === "Librarian"){
+        navigate('librarian');
+      }
+      else if(RoleUser === "SuperAdmin"){
+        navigate('superAdmin');
+      }
+    }
+
   if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
 
     return (
@@ -22,7 +34,7 @@ const AddBook = () => {
         <div className="bg-white my-2 mx-8 py-4 shadow-xl rounded border-b-4 border-blue-400 px-4">
           <h1 className="text-xl font-bold">Add Book</h1>
           <hr className='mb-4'/>
-          <button className="border border-blue-500 text-[#3B71CA] bg-white py-2 px-12 rounded duration-500 font-semibold hover:bg-[#3B71CA] hover:text-white hover:shadow-xl" >Back</button>
+          <button onClick={headleBack} className="border border-blue-500 text-[#3B71CA] bg-white py-2 px-12 rounded duration-500 font-semibold hover:bg-[#3B71CA] hover:text-white hover:shadow-xl" >Back</button>
         </div>
       </div>
     )
