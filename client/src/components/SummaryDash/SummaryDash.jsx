fbf9aeae9828097d7b9650a4fb9f8a3c2a9eee72
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-05 13:28:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/SummaryDash.jsx
+++ client/src/components/SummaryDash/SummaryDash.jsx
@@ -4,12 +4,19 @@ import React, { useEffect, useState } from "react"
 
 const SummaryDash = () => {
   const [count, setCount] = useState(0);
+  const [books, setBooks] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:8081/AdminCount');
         setCount(response.data.count);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+      try {
+        const response = await axios.get('http://localhost:8081/BookCount');
+        setBooks(response.data.books);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -21,6 +28,7 @@ const SummaryDash = () => {
   return (
     <div>
       {count}
+      {books}
     </div>
   )
 }
