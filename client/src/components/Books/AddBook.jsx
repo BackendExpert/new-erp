faa8d924d23052b259cda52c5559a7dc0255a8f9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-05 13:39:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Books/AddBook.jsx
+++ client/src/components/Books/AddBook.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddBook = () => {
-  return (
-    <div>AddBook</div>
-  )
+  const navigate = useNavigate();
+  //check the current login user
+  const RoleUser = secureLocalStorage.getItem("loginNew");
+
+  //check access roles
+
+  /*
+  
+    Access by 
+    SuperAdmin, Librarian
+  
+  */
+  if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
+    return (
+      <div>AddBook</div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear();
+      navigate('/');
+    })
+  }
+
 }
 
 export default AddBook
