3bfc9d9be3e585270e37c18662d54ddd9411040c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-05 11:52:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LabManager/LabData.jsx
+++ client/src/components/LabManager/LabData.jsx
@@ -13,14 +13,14 @@ const LabData = () => {
       using following code prevent unauthorized access
        
     */
-    useEffect(() => {
-      const RoleUser = secureLocalStorage.getItem("loginNew");
+    // useEffect(() => {
+    //   const RoleUser = secureLocalStorage.getItem("loginNew");
     
-      if(RoleUser !== "Labmanager"){
-        navigete('/');
-        localStorage.clear();
-      }
-    }, []);
+    //   if(RoleUser !== "Labmanager"){
+    //     navigete('/');
+    //     localStorage.clear();
+    //   }
+    // }, []);
   return (
     <div className="bg-white py-4 px-6 my-4 rounded border-t-4 border-blue-200">LabData</div>
   )
--- client/src/components/LabManager/LabManager.jsx
+++ client/src/components/LabManager/LabManager.jsx
@@ -1,6 +1,6 @@
 import secureLocalStorage from "react-secure-storage"
 import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import LabSide from "./LabSide"
 import Navlist from "../NavBar/navList"
 import LabData from "./LabData"
@@ -16,25 +16,36 @@ const LabManager = () => {
     using following code prevent unauthorized access
      
   */
-  useEffect(() => {
+  // useEffect(() => {
     const RoleUser = secureLocalStorage.getItem("loginNew");
   
-    if(RoleUser !== "Labmanager"){
-      navigete('/');
-      localStorage.clear();
-    }
-  }, []);
-  return (
-    <div className="bg-gray-200">
-      <div className="flex">
-        <LabSide />
-        <div className="w-full mx-2">
-          <Navlist />
-          <LabData />
+  //   if(RoleUser !== "Labmanager"){
+  //     navigete('/');
+  //     localStorage.clear();
+  //   }
+  // }, []);
+
+  if(RoleUser === "Labmanager"){
+    return (
+      <div className="bg-gray-200">
+        <div className="flex">
+          <LabSide />
+          <div className="w-full mx-2">
+            <Navlist />
+            <LabData />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear();
+      navigete('/')
+    })
+  }
+
+
 }
 
 export default LabManager
