5dce2a4329123a16ff5ed6f64aa8d8d95dec00e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 10:52:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Books/Books.jsx
+++ client/src/components/library/Books/Books.jsx
@@ -33,11 +33,48 @@ const Books = () => {
               <h1 className="text-xl font-semibold">All Book List</h1>
               <hr className="mb-4" />
               <Link to={'/bookadd'}>
-                <div className="flex py-2 px-10 font-semibold rounded border w-[20%] border-[#14A44D] text-[#14A44D] duration-500 hover:bg-[#14A44D] hover:text-white hover:shadow-xl">
-                  <p className="pt-[3px] pr-2"><Icons name='add'></Icons></p>
-                  <p className="">Add New Book</p>
-                </div>
+                <span className="py-2 px-10 font-semibold rounded border border-[#14A44D] text-[#14A44D] duration-500 hover:bg-[#14A44D] hover:text-white hover:shadow-xl">
+                  <span className="pt-[3px] pr-2"><Icons name='add'></Icons></span>
+                  <span className="">Add New Book</span>
+                </span>
               </Link>
+
+              <div class="relative overflow-x-auto my-8">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Product name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Color
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Category
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Price
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
