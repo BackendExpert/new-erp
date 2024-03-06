2401c51baa76d5951286e5a4c0fdef8b79c67bbc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 09:11:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Books/Books.jsx
+++ client/src/components/library/Books/Books.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
+
 
 const Books = () => {
-  return (
-    <div>Books</div>
-  )
+  const navigate = useNavigate();
+
+  //get the current login user 
+  const RoleUser = secureLocalStorage.getItem("loginNew");
+
+  // check login user is Librarian or SuperAdmin
+  if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
+    return (
+      <div>Books</div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear();
+      navigate('/');
+    })
+  }
 }
 
 export default Books
