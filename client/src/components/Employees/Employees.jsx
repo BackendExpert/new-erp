00797d082f5751b7b5395b215facee04983b3efa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 15:34:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/Employees.jsx
+++ client/src/components/Employees/Employees.jsx
@@ -30,8 +30,12 @@ const Employees = () => {
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
                     <h1 className="text-xl font-semibold">All Employee</h1>
                     <hr className="mb-4" />
-
-                    <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    <div className="flex">
+                        <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        <Link>
+                            <button className="border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl mx-2">Add New Employee</button>
+                        </Link>
+                    </div>
 
                 </div>
             </div>
