63d83ce08c44ebc347aeb5f6ad180278a2b6cdf7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 13:38:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Books/BookBorrow.jsx
+++ client/src/components/library/Books/BookBorrow.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
+
 
 const BookBorrow = () => {
-  return (
-    <div>BookBorrow</div>
-  )
+    const navigate = useNavigate()
+
+    //get the current login user 
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
+        return (
+            <div className="bg-gray-200 py-4 w-full h-full">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    Hi all
+                </div>
+            </div>
+        )    
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/');
+        }, [])
+    }
 }
 
 export default BookBorrow
