a462fd38b5899695913fe4ef5cc24e190eca1d2f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 22:39:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/AddEmployee.jsx
+++ client/src/components/Employees/AddEmployee.jsx
@@ -1,5 +1,5 @@
 import secureLocalStorage from "react-secure-storage"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 
 const AddEmployee = () => {
@@ -18,6 +18,32 @@ const AddEmployee = () => {
         navigate('/Employee');
     }
 
+    const [empData, SetEmpData] = useState({
+        eid:'',
+        initial:'',
+        surname:'',
+        address:'',
+        phone:'',
+        email:'',
+        password:'',
+        salary:'',
+        image:'',
+        category:'',
+        nic:'',
+        dob:'',
+        type:'',
+        emgcontact:'',
+        civilstatus:'',
+        gender:'',
+        designation:'',
+        relig:''
+    })
+
+    const headleEmpSubmit = (e) => {
+        e.preventDefault(); 
+    }
+
+
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin" || RoleUser === "Accountant"){
         return (
             <div className="bg-gray-200 py-4">
@@ -29,62 +55,63 @@ const AddEmployee = () => {
                     </div>
 
                     <div className="my-4">
-                        <form>
+                        <form onSubmit={headleEmpSubmit}>
                             <div className="my-4 lg:grid grid-cols-3 gap-2">
                                 <div className="">
                                     <label htmlFor="">Employee No</label>
                                     <input type="text" name="eid" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Employee ID" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, eid:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Initials</label>
                                     <input type="text" name="initial" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Initials" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, initial:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Surname</label>
                                     <input type="text" name="surname" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Surname" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, surname:e.target.value})}/>
                                 </div>
                             </div>
                             <div className="my-4 lg:grid grid-cols-1">
                                 <div className="">
                                     <label htmlFor="">Address</label>
                                     <input type="text" name="address" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Address" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, address:e.target.value})}/>
                                 </div>
                             </div>
                             <div className="my-4 lg:grid grid-cols-4 gap-2">
                                 <div className="">
                                     <label htmlFor="">Phone No</label>
                                     <input type="text" name="phone" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Phone Number" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, phone:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Email</label>
                                     <input type="email" name="email" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Email" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, email:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Password</label>
                                     <input type="password" name="password" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Password" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, password:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Salary</label>
                                     <input type="text" name="salary" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Salary" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, salary:e.target.value})}/>
                                 </div>
                             </div>
                             <div className="my-4 lg:grid grid-cols-4 gap-2">
                                 <div className="">
                                     <label htmlFor="">Designation</label>
                                     <input type="text" name="designation" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Designation" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, designation:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Job Category</label>
-                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2">
+                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetEmpData({...empData, category:e.target.value})}>
                                         <option>Select Option</option>
                                         <option value="Director">Director</option>
                                         <option value="Secretary">Secretary</option>
@@ -95,7 +122,8 @@ const AddEmployee = () => {
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Employement Type</label>
-                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2">
+                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetEmpData({...empData, type:e.target.value})}>
                                         <option>Select Option</option>
                                         <option value="Permanent">Permanent</option>
                                         <option value="Contract">Contract</option>
@@ -106,7 +134,8 @@ const AddEmployee = () => {
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Marital Status</label>
-                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2">
+                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetEmpData({...empData, civilstatus:e.target.value})}>
                                         <option>Select Option</option>
                                         <option value="Married">Married</option>
                                         <option value="Single">Single</option>
@@ -117,26 +146,27 @@ const AddEmployee = () => {
                                 <div className="">
                                     <label htmlFor="">Date of Birth</label>
                                     <input type="date" name="dob" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Date of Birth" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, dob:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Religous</label>
                                     <input type="text" name="religous" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Religous" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, relig:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">NIC No</label>
                                     <input type="text" name="nic" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter NIC Number" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, nic:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Emergency Phone No</label>
                                     <input type="text" name="emgcontact" className="pl-2 border border-blue-400 rounded w-full h-12" required placeholder="Enter Emergency Phone No" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, emgcontact:e.target.value})}/>
                                 </div>
                                 <div className="">
                                     <label htmlFor="">Your Gender</label>
-                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2">
+                                    <select name="" id="" className="w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetEmpData({...empData, gender:e.target.value})}>
                                         <option>Select Option</option>
                                         <option value="Male">Male</option>
                                         <option value="Female">Female</option>
@@ -146,11 +176,11 @@ const AddEmployee = () => {
                                 <div className="">
                                     <label htmlFor="">Select Image</label>
                                     <input type="file" name="image" className="pl-2 rounded w-full" required placeholder="Upload File" 
-                                    />
+                                    onChange={e => SetEmpData({...empData, image:e.target.files[0]})}/>
                                 </div>
                             </div>
                             <div className="">
-                                <button type="submit" className="py-2 px-16 border border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl"></button>
+                                <button type="submit" className="py-2 px-16 border border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Add Employee</button>
                             </div>
                         </form>
                     </div>
