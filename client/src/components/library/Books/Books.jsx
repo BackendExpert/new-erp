298be546bf7b5be99501ceb5dace3ea528adf8b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 10:55:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Books/Books.jsx
+++ client/src/components/library/Books/Books.jsx
@@ -42,19 +42,17 @@ const Books = () => {
               <div class="relative overflow-x-auto my-8">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">
-                                Product name
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Color
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Category
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Price
-                            </th>
+                        <tr className='text-blue-500'>
+                            <th scope='col' className='px-6 py-3'>ID</th>
+                            <th scope='col' className='px-6 py-3'>ISBN</th>
+                            <th scope='col' className='px-6 py-3'>Title</th>
+                            <th scope='col' className='px-6 py-3'>Category</th>
+                            <th scope='col' className='px-6 py-3'>Status</th>
+                            <th scope='col' className='px-6 py-3'>Publisher</th>
+                            <th scope='col' className='px-6 py-3'>Year</th>
+                            <th scope='col' className='px-6 py-3'>Authors</th>
+                            <th scope='col' className='px-6 py-3'>Value</th>
+                            <th scope='col' className='px-6 py-3'>Action</th>
                         </tr>
                     </thead>
                     <tbody>
