18625ee9b71728b7ee61770ac5df478eeef7020a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 06:16:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- db/db_erp_reservations.sql
+++ db/db_erp_reservations.sql
@@ -16,40 +16,45 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `srn`
+-- Table structure for table `reservations`
 --
 
-DROP TABLE IF EXISTS `srn`;
+DROP TABLE IF EXISTS `reservations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `srn` (
-  `Name` varchar(60) DEFAULT NULL,
-  `Email` varchar(60) NOT NULL,
-  `project` varchar(30) DEFAULT NULL,
-  `division` varchar(100) DEFAULT NULL,
-  `Rdate` date NOT NULL,
-  `srnType` varchar(90) DEFAULT NULL,
-  `PType` varchar(90) DEFAULT NULL,
-  `PIype` varchar(90) DEFAULT NULL,
-  `estimate` decimal(8,2) DEFAULT NULL,
-  `vote` varchar(100) DEFAULT NULL,
+CREATE TABLE `reservations` (
+  `RID` int NOT NULL AUTO_INCREMENT,
+  `StartDate` date DEFAULT NULL,
+  `time` varchar(10) DEFAULT NULL,
+  `loc_route` varchar(100) DEFAULT NULL,
   `HoDEmail` varchar(60) DEFAULT NULL,
-  `description` varchar(254) DEFAULT NULL,
-  `ReqNo` varchar(30) DEFAULT NULL,
-  `Status` varchar(20) DEFAULT NULL,
-  `SID` int NOT NULL AUTO_INCREMENT,
-  `Cdate` date DEFAULT NULL,
-  PRIMARY KEY (`SID`)
+  `other_passengers` varchar(235) DEFAULT NULL,
+  `Name` varchar(100) DEFAULT NULL,
+  `EndDate` date DEFAULT NULL,
+  `mode_travel` varchar(50) DEFAULT NULL,
+  `Status` varchar(15) DEFAULT NULL,
+  `Email` varchar(60) DEFAULT NULL,
+  `designation` varchar(30) DEFAULT NULL,
+  `fundingsource` varchar(50) DEFAULT NULL,
+  `division` varchar(50) DEFAULT NULL,
+  `purpose` varchar(150) DEFAULT NULL,
+  `veh_type` varchar(50) DEFAULT NULL,
+  `driver` varchar(45) DEFAULT NULL,
+  `DEmail` varchar(60) DEFAULT NULL,
+  `milage` int DEFAULT NULL,
+  `uprice` int DEFAULT NULL,
+  `cost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`RID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `srn`
+-- Dumping data for table `reservations`
 --
 
-LOCK TABLES `srn` WRITE;
-/*!40000 ALTER TABLE `srn` DISABLE KEYS */;
-/*!40000 ALTER TABLE `srn` ENABLE KEYS */;
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
