0ed88c30da22cfcf06e94fe7980200c16090bb6e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 06:15:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- db/db_erp_reservations.sql
+++ db/db_erp_reservations.sql
@@ -16,35 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `borrowal`
+-- Table structure for table `srn`
 --
 
-DROP TABLE IF EXISTS `borrowal`;
+DROP TABLE IF EXISTS `srn`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `borrowal` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `bdate` date DEFAULT NULL,
-  `borrower` int DEFAULT NULL,
-  `bookid` int DEFAULT NULL,
-  `bname` varchar(60) DEFAULT NULL,
-  `btitle` varchar(100) DEFAULT NULL,
-  `erdate` date DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `borrower` (`borrower`),
-  KEY `bookid` (`bookid`),
-  CONSTRAINT `borrowal_ibfk_1` FOREIGN KEY (`borrower`) REFERENCES `employee` (`eid`),
-  CONSTRAINT `borrowal_ibfk_2` FOREIGN KEY (`bookid`) REFERENCES `books` (`BookID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `srn` (
+  `Name` varchar(60) DEFAULT NULL,
+  `Email` varchar(60) NOT NULL,
+  `project` varchar(30) DEFAULT NULL,
+  `division` varchar(100) DEFAULT NULL,
+  `Rdate` date NOT NULL,
+  `srnType` varchar(90) DEFAULT NULL,
+  `PType` varchar(90) DEFAULT NULL,
+  `PIype` varchar(90) DEFAULT NULL,
+  `estimate` decimal(8,2) DEFAULT NULL,
+  `vote` varchar(100) DEFAULT NULL,
+  `HoDEmail` varchar(60) DEFAULT NULL,
+  `description` varchar(254) DEFAULT NULL,
+  `ReqNo` varchar(30) DEFAULT NULL,
+  `Status` varchar(20) DEFAULT NULL,
+  `SID` int NOT NULL AUTO_INCREMENT,
+  `Cdate` date DEFAULT NULL,
+  PRIMARY KEY (`SID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `borrowal`
+-- Dumping data for table `srn`
 --
 
-LOCK TABLES `borrowal` WRITE;
-/*!40000 ALTER TABLE `borrowal` DISABLE KEYS */;
-/*!40000 ALTER TABLE `borrowal` ENABLE KEYS */;
+LOCK TABLES `srn` WRITE;
+/*!40000 ALTER TABLE `srn` DISABLE KEYS */;
+/*!40000 ALTER TABLE `srn` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
