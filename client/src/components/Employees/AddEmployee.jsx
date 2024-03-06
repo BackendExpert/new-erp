10be3a5e660895a8f4604d6529e58570fc671593
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 15:44:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/AddEmployee.jsx
+++ client/src/components/Employees/AddEmployee.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 const AddEmployee = () => {
-  return (
-    <div>AddEmployee</div>
-  )
+    const navigate = useNavigate();
+
+    //get the login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+
+    //this page can access by following users
+    // SuperAdmin, Admin, Accountant
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin" || RoleUser === "Accountant"){
+        return (
+            <div>AddEmployee</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/');
+        }, [])
+    }
+
 }
 
 export default AddEmployee
