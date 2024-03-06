c0228d8a0677c2b9ad30428056da75d1fe1b62f1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 13:46:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Books/BookBorrow.jsx
+++ client/src/components/library/Books/BookBorrow.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import secureLocalStorage from 'react-secure-storage'
-
 
 const BookBorrow = () => {
     const navigate = useNavigate()
@@ -14,7 +13,13 @@ const BookBorrow = () => {
         return (
             <div className="bg-gray-200 py-4 w-full h-full">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    Hi all
+                    <h1 className='text-xl font-semibold'>Book Borrow </h1>
+                    <hr className="mb-4" />
+
+                    <Link to={'/booklist'} >
+                        <button className='py-3 px-16 rounded text-blue-400 font-semibold border border-blue-400 duration-500 hover:bg-blue-400 hover:text-white hover:shadow-xl'>Back</button>
+                    </Link>
+                    
                 </div>
             </div>
         )    
