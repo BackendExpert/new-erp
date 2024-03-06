fff10bf01cd8507addcf49f2f679c5da674ca238
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-06 13:47:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/SideList.jsx
+++ client/src/components/SuperAdmin/SideList.jsx
@@ -13,7 +13,7 @@ const SideList = () => {
         {name: "Designation",  link: "#", icon: <Icons name="easel" size="large"></Icons>},
         {name: "Vehicle",  link: "#", icon: <Icons name="car" size="large"></Icons>},
         {name: "Equipment",  link: "#", icon: <Icons name="build" size="large"></Icons>},
-        {name: "Library",  link: "#", icon: <Icons name="library" size="large"></Icons>},
+        {name: "Library",  link: "/librarian", icon: <Icons name="library" size="large"></Icons>},
         {name: "Accounts",  link: "#", icon: <Icons name="people-circle" size="large"></Icons>},
         {name: "Profile",  link: "#", icon: <Icons name="person" size="large"></Icons>}
     ]
