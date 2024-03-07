ad84983318a0d2a98f9dd1df87fa75a0f8e55718
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-07 10:49:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const stroge = multer.diskStorage({
         cb(null, 'public/images')
     },
     filename:(req, file, cb) =>{
-        cb(null, file.fieldname + '_' + DataTransfer.now() + Path.extname(file.originalname));
+        cb(null, file.fieldname + '_' + Date.now() + Path.extname(file.originalname));
     }
 })
 
