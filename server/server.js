641d643e18316f3fec66e82f5dc797b8948ec02c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-07 10:57:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
-import express from "express";
-import cors from 'cors';
-import mysql from "mysql2";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import nodemailer from 'nodemailer';
-import cookieParser from "cookie-parser";
-import multer from "multer";
-import path from "path";
-import { resourceLimits } from "worker_threads";
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 8081
