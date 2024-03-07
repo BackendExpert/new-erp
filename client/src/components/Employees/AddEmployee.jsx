90df8e1932252aaf195aab5c687fade3774af725
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-07 09:47:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/AddEmployee.jsx
+++ client/src/components/Employees/AddEmployee.jsx
@@ -205,7 +205,9 @@ const AddEmployee = () => {
                                 </div>
                             </div>
                             <div className="">
-                                <button type="submit" className="px-16 py-2 border border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl"></button>
+                                <button type="submit" className="px-16 py-2 border border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">
+                                    Add Employee
+                                </button>
                             </div>
                         </form>
                     </div>
