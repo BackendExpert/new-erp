65d6666f99cc2e8a3c80d25532ad2cf45960fa86
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-08 12:06:16 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/Employees/UpdateEmployee.jsx
+++ client/src/components/Employees/UpdateEmployee.jsx
@@ -11,10 +11,10 @@ const UpdateEmployee = () => {
       return (
         <div className="bg-gray-200 py-4">
           <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Add New Employee</h1>
+            <h1 className="text-xl font-semibold">Update Employee</h1>
             <hr className="mb-4" />
             <div className="flex">
-              <Link to={'/Employees'}>
+              <Link to={'/Employee'}>
                 <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
               </Link>
             </div>
