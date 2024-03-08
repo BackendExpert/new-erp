1e1f7ddd73f2ac739530d1cfc137901e9e775b52
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-08 11:57:42 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/Employees/UpdateEmployee.jsx
+++ client/src/components/Employees/UpdateEmployee.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const UpdateEmployee = () => {
-  return (
-    <div>UpdateEmployee</div>
-  )
+    const navigate = useNavigate();
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin" || RoleUser === "Accountant"){
+      return (
+        <div>UpdateEmployee</div>
+      )
+    }
+    else{
+      useEffect(() => {
+        localStorage.clear();
+        navigate('/');
+      }, [])
+    }
+
 }
 
 export default UpdateEmployee
