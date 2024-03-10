9a9c376f88af1206723a5f5c0ee13a17063eb49b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-10 19:42:04 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/Designations.jsx
+++ client/src/components/Designation/Designations.jsx
@@ -9,11 +9,30 @@ const Designations = () => {
     //check current login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
+    //go back according to login user
+    const headleBack = () => {
+        if(RoleUser === "SuperAdmin"){
+
+        }
+        else if(RoleUser === "Admin"){
+            navigate('/admin');
+        }
+    }
+
     //this route can access only by SuperAdmin and Admin
 
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
         return (
-            <div>Designations</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                <h1 className="text-xl font-semibold">Designations</h1>        
+                <hr className="mb-4" />
+                <div className="flex">
+                    <button className="border py-4 px-16 rounded border-blue-500 text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    <button className="ml-4 border py-4 px-16 rounded border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Add New Designation</button>
+                </div>
+                </div>
+            </div>
         )
     }
     else{
