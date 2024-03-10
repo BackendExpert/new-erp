20f71654893f6bd7501baee729b7a12744320083
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-10 20:04:25 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/AddDesignation.jsx
+++ client/src/components/Designation/AddDesignation.jsx
@@ -19,6 +19,12 @@ const AddDesignation = () => {
                     <Link to={'/Designations'}>
                         <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                     </Link>
+
+                    <div className="">
+                        <form>
+                            
+                        </form>
+                    </div>
                 </div>
             </div>
         )
