584a5e7df23fdad912245b4e96b11ebfb118cb8e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-10 20:03:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/AddDesignation.jsx
+++ client/src/components/Designation/AddDesignation.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddDesignation = () => {
-  return (
-    <div>AddDesignation</div>
-  )
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Add New Designations</h1>        
+                    <hr className="mb-4" />
+        
+                    <Link to={'/Designations'}>
+                        <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/');
+        }, []);
+    }
+
+
 }
 
 export default AddDesignation
