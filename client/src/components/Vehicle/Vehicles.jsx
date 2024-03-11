d45cc3c11eeb1e8545a4a1a62f2e5a182b109211
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 11:40:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Vehicle/Vehicles.jsx
+++ client/src/components/Vehicle/Vehicles.jsx
@@ -4,20 +4,38 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 
 const Vehicles = () => {
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Designations</h1>        
-            <hr className="mb-4" />
-            <div className="lg:flex">
-                <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                <Link to={'/AddDesignation'}>
-                    <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Designation</button>
-                </Link>
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    //this route can access only by admin and Transport Officer
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "TO"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Designations</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                        <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        <Link to={'/AddDesignation'}>
+                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Designation</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/');
+        }, [])
+    }
+
+
+
 }
 
 export default Vehicles
