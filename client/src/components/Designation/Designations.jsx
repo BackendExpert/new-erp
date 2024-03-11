37e08230a8905d59f41d41d87674e0bacc15f4e9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 07:08:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/Designations.jsx
+++ client/src/components/Designation/Designations.jsx
@@ -53,6 +53,24 @@ const Designations = () => {
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            designationData.map((designation, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td className='px-6 py-4 font-bold'>{designation.Code}</td>
+                                        <td className='px-6 py-4 font-bold'>{designation.DName}</td>
+                                        <td className='px-6 py-4 font-bold'>{designation.Basic_Salary}</td>
+                                        <td className='px-6 py-4 font-bold'>{designation.increment}</td>
+                                        <td className='px-6 py-4 font-bold'>
+                                            <Link>
+                                                <button className="rounded border border-blue-500 text-blue-500 font-semibold  mx-2 py-2 px-8 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Update</button>
+                                            </Link>
+                                            <button className="rounded border border-red-500 text-red-500 font-semibold  mx-2 py-2 px-8 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
 
                     </tbody>
                 </table>
