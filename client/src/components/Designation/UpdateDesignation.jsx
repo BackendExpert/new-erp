5376a00317927d6c07d08d01ef0c260543d1d8bf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 09:37:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/UpdateDesignation.jsx
+++ client/src/components/Designation/UpdateDesignation.jsx
@@ -13,7 +13,7 @@ const UpdateDesignation = () => {
       return (
         <div className="bg-gray-200 py-4">
           <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Add New Designations</h1>        
+            <h1 className="text-xl font-semibold">Update Designation</h1>        
             <hr className="mb-4" />
 
             <Link to={'/Designations'}>
