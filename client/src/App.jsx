d50f36ae5ca961b5a6057e03915a8bdc417b498e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 19:02:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,25 +36,37 @@ export default function App() {
 
         {/* PrivateRoute for protect the following routes from unauthrized access */}
         <Route path="/superAdmin" element={<PrivateRoute><SuperAdmin /></PrivateRoute>} />
+        
         <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />
+        
         <Route path="/hod" element={<PrivateRoute><HeadDep /></PrivateRoute>} />
+        
         <Route path="/to" element={<PrivateRoute><TransOfficer /></PrivateRoute>} />
+        
         <Route path="/librarian" element={<PrivateRoute><Librarian /></PrivateRoute>} />
+        
         <Route path="/labManager" element={<PrivateRoute><LabManager /></PrivateRoute>} />
+        
         <Route path="/accountant" element={<PrivateRoute><Accountant /></PrivateRoute>} />
+        
         <Route path="/user" element={<PrivateRoute><Users /></PrivateRoute>} />
+        
         <Route path="/bookadd" element={<PrivateRoute><AddBook /></PrivateRoute>} />
         <Route path="/booklist" element={<PrivateRoute><Books /></PrivateRoute>} />
         <Route path="/BorrowBook/:id" element={<PrivateRoute><BookBorrow /></PrivateRoute>} />
+        
         <Route path="/Employee" element={<PrivateRoute><Employees /></PrivateRoute>} />
         <Route path="/AddEmployee" element={<PrivateRoute><AddEmployee /></PrivateRoute>} />
         <Route path="/UpdateEmp/:id" element={<PrivateRoute><UpdateEmployee /></PrivateRoute>} />
+        
         <Route path="/AddDesignation" element={<PrivateRoute><AddDesignation /></PrivateRoute>} />
         <Route path="/Designations" element={<PrivateRoute><Designations /></PrivateRoute>} />
         <Route path="/UpdateDesignation/:id" element={<PrivateRoute><UpdateDesignation /></PrivateRoute>} />
+        
         <Route path="/AddVehicle" element={<PrivateRoute><AddVehicle /></PrivateRoute>} />
         <Route path="/Vehicles" element={<PrivateRoute><Vehicles /></PrivateRoute>} />
         <Route path="/UpdateVehicle/:id" element={<PrivateRoute><UpdateVehicle /></PrivateRoute>} />
+        
         <Route path="/AddProgram" element={<PrivateRoute><AddProgram /></PrivateRoute>} />
         <Route path="/Programs" element={<PrivateRoute><Programs /></PrivateRoute>} />
         <Route path="/UpdateProgram/:id" element={<PrivateRoute><UpdateProgram /></PrivateRoute>} />
