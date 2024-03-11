73b39458c9423bdf522b64f106cb4a584184fd05
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 18:45:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/Program/AddProgram.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddProgram = () => {
+  return (
+    <div>AddProgram</div>
+  )
+}
+
+export default AddProgram
---
+++ client/src/components/Program/UpdateProgram.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateProgram = () => {
+  return (
+    <div>UpdateProgram</div>
+  )
+}
+
+export default UpdateProgram
