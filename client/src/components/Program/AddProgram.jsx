f02af1d98110a41a80b9c15db9c4c4b3576dd537
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 19:24:02 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/AddProgram.jsx
+++ client/src/components/Program/AddProgram.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddProgram = () => {
-  return (
-    <div>AddProgram</div>
-  )
+    const navigate = useNavigate();
+
+    //get the login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    //this route access only by admin and SuperAdmin
+    
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        return (
+            <div>AddProgram</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/');
+        }, [])
+    }
+
 }
 
 export default AddProgram
