6bdc5ed89cd92c6d1815d18613313f90ae11e098
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 10:33:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/UpdateDesignation.jsx
+++ client/src/components/Designation/UpdateDesignation.jsx
@@ -1,7 +1,8 @@
 import secureLocalStorage from "react-secure-storage"
 import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import axios from "axios";
+
 
 const UpdateDesignation = () => {
     const navigate = useNavigate();
