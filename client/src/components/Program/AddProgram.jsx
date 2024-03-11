1e2f46b49eb804f4bd542ce7fb7edb54e310a4d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 19:34:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/AddProgram.jsx
+++ client/src/components/Program/AddProgram.jsx
@@ -28,29 +28,29 @@ const AddProgram = () => {
                             <div className="lg:grid grid-cols-2 gap-2">
                                 <div className="my-2">
                                     <label htmlFor="">Program Name</label>
-                                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Program Name"
+                                    <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Program Name"
                                     />
                                 </div>
                                 <div className="my-2">
                                     <label htmlFor="">Program Location</label>
-                                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Program Location"
+                                    <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Program Location"
                                     />
                                 </div>
                             </div>
                             <div className="lg:grid grid-cols-3 gap-2">
                                 <div className="my-2">
                                     <label htmlFor="">HOD Email</label>
-                                    <input type="email" className="w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter HOD Email"
+                                    <input type="email" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter HOD Email"
                                     />
                                 </div>
                                 <div className="my-2">
                                     <label htmlFor="">Scientist 1</label>
-                                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Scientist 1"
+                                    <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Scientist 1"
                                     />
                                 </div>
                                 <div className="my-2">
                                     <label htmlFor="">Scientist 2</label>
-                                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Scientist 2"
+                                    <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Scientist 2"
                                     />
                                 </div>
                             </div>
