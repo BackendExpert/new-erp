7f403671de47b59ae64d6078474ed8267ababf3c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 09:35:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/UpdateDesignation.jsx
+++ client/src/components/Designation/UpdateDesignation.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const UpdateDesignation = () => {
-  return (
-    <div>UpdateDesignation</div>
-  )
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+      return (
+        <div>UpdateDesignation</div>
+      )
+    }    
+    else{
+      useEffect(() => {
+        localStorage.clear();
+        navigate('/');
+      }, [])
+    }
+
 }
 
 export default UpdateDesignation
