fc9cb39a3e36d961920e164479d9dedbc0e5ed44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 19:27:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/AddProgram.jsx
+++ client/src/components/Program/AddProgram.jsx
@@ -13,7 +13,17 @@ const AddProgram = () => {
     
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
         return (
-            <div>AddProgram</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Add Program</h1>
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                       <Link to={'/Programs'}>
+                            <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                       </Link>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
