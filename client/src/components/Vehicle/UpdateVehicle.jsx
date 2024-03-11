91191f56e8249e83bd6bd09570327ae826a10ecf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 13:59:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Vehicle/UpdateVehicle.jsx
+++ client/src/components/Vehicle/UpdateVehicle.jsx
@@ -11,7 +11,17 @@ const UpdateVehicle = () => {
 
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin" || RoleUser === "TO"){
         return (
-            <div>UpdateVehicle</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Update Vehicle</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                        <Link to={'/Vehicles'}>
+                            <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
