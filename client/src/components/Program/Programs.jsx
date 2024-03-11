bfe057dd0a96e8e88c04b987a06ea4669b0fc761
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 18:50:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/Programs.jsx
+++ client/src/components/Program/Programs.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const Programs = () => {
-  return (
-    <div>Programs</div>
-  )
+    const navigate = useNavigate();
+
+    //get the login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+
+    //this page can access by following users
+    // SuperAdmin, Admin
+
+    if(RoleUser === "" || RoleUser === ""){
+        return (
+            <div>Programs</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear();
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default Programs
