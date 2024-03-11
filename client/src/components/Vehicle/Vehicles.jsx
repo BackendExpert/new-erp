241cb7b95383d233d7ff924d7c89c20b6fb6fe96
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-11 11:43:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Vehicle/Vehicles.jsx
+++ client/src/components/Vehicle/Vehicles.jsx
@@ -21,7 +21,7 @@ const Vehicles = () => {
 
     //this route can access only by admin and Transport Officer
 
-    if(RoleUser === "SuperAdmin" || RoleUser === "TO"){
+    if(RoleUser === "SuperAdmin" || RoleUser === "admin" || RoleUser === "TO"){
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
