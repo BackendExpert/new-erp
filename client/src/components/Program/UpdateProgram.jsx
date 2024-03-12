804cc61750acae714715a41fdf4db2215100e2c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-12 06:47:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/UpdateProgram.jsx
+++ client/src/components/Program/UpdateProgram.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import axios from "axios";
 
 const UpdateProgram = () => {
+    const navigate = useNavigate();
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    const {id} = useParams();
+
+    const [ProgramValue, SetProgramValue] = useState({
+      address: '',
+      phone: '',
+      salary: '',
+      category: '',
+      type: '',
+      designation: '',
+      emgcontact: '',
+      civilstatus: '',
+      email: ''
+    })
+
   return (
     <div>UpdateProgram</div>
   )
