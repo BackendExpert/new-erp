457066355fb45511792ddacfcc7ef115326321a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-12 10:48:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/Login.jsx
+++ client/src/components/LoginSignUp/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 
            if(res.data.Msg === "success"){
                 if(res.data.CheckRole[0].is_active === 0){
-                    alert('Your Account has been Suspended due to unauthorized access.')
+                    alert('Your Account has been Suspended due to irregular activities.')
                     navigete('/');
                 }
                 else if(res.data.CheckRole[0].role === 'SuperAdmin'){
