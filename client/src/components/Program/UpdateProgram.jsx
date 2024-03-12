837283b1be5833a225f51a5cb3a610d5b1ece024
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-12 06:55:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/UpdateProgram.jsx
+++ client/src/components/Program/UpdateProgram.jsx
@@ -17,7 +17,7 @@ const UpdateProgram = () => {
     })
 
     //fetch data to update
-    
+
 
     //this route access only by admin and SuperAdmin
     
@@ -25,12 +25,48 @@ const UpdateProgram = () => {
       return (
         <div className="bg-gray-200 py-4">
           <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Update Employee</h1>
+            <h1 className="text-xl font-semibold">Update Program</h1>
             <hr className="mb-4" />
             <div className="flex">
               <Link to={'/Programs'}>
                 <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
               </Link>
+            </div>
+
+            <div className="my-2">
+              <form>
+                <div className="lg:grid grid-cols-2 gap-2">
+                  <div className="my-2">
+                    <label htmlFor="">New Program Name: </label>
+                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2 rounded" required placeholder="Enter New Program Name"
+                    />
+                  </div>
+                  <div className="my-2">
+                    <label htmlFor="">New Program Location: </label>
+                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2 rounded" required placeholder="Enter New Program Location"
+                    />
+                  </div>
+                </div>
+                <div className="lg:grid grid-cols-3 gap-2">
+                  <div className="my-2">
+                    <label htmlFor="">New HOD: </label>
+                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2 rounded" required placeholder="Enter New HOD"
+                    />
+                  </div>
+
+                  <div className="my-2">
+                    <label htmlFor="">New Scientist 1: </label>
+                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2 rounded" required placeholder="Enter New Scientist 1"
+                    />
+                  </div>
+
+                  <div className="my-2">
+                    <label htmlFor="">New Scientist 2: </label>
+                    <input type="text" className="w-full h-12 border border-blue-500 pl-2 my-2 rounded" required placeholder="Enter New Scientist 2"
+                    />
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
