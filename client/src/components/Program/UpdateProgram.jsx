169e976fb86ce0e8df093b0ce7db2ffc9eef5374
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-12 06:50:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Program/UpdateProgram.jsx
+++ client/src/components/Program/UpdateProgram.jsx
@@ -13,14 +13,24 @@ const UpdateProgram = () => {
       location: '',
       hod: '',
       scients1: '',
-      scients1: ''
+      scients2: ''
     })
 
     //this route access only by admin and SuperAdmin
     
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
       return (
-        <div>UpdateProgram</div>
+        <div className="bg-gray-200 py-4">
+          <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Update Employee</h1>
+            <hr className="mb-4" />
+            <div className="flex">
+              <Link to={'/Employee'}>
+                <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+              </Link>
+            </div>
+          </div>
+        </div>
       )
     }
     else{
