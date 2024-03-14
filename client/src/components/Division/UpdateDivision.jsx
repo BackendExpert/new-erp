f48da8034e572878433459896b00e9c3dac783cc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 17:41:26 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Division/UpdateDivision.jsx
+++ client/src/components/Division/UpdateDivision.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const UpdateDivision = () => {
-  return (
-    <div>UpdateDivision</div>
-  )
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+      return (
+        <div>UpdateDivision</div>
+      )
+    }
+    else{
+      useEffect(() => {
+          navigate('/UnAccess');
+      }, [])
+    }
+
 }
 
 export default UpdateDivision
