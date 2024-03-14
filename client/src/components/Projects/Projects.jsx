7e98b5df10bdf53d9b0699e310f64df75371bbb8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:08:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -8,12 +8,12 @@ const Projects = () => {
   return (
     <div className="bg-gray-200 py-4">
       <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-        <h1 className="text-xl font-semibold">All Programs</h1>
+        <h1 className="text-xl font-semibold">All Projects</h1>
         <hr className="mb-4" />
         <div className="lg:flex">
             <button /*onClick={headleBack} */ className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
             <Link to={'/AddProject'}>
-                <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Program</button>
+                <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Project</button>
             </Link>
           </div>
       </div>
