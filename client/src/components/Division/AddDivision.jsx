2ac68e0f4d4cd34acbd3e3c7938d338b9a6afb9f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:58:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Division/AddDivision.jsx
+++ client/src/components/Division/AddDivision.jsx
@@ -15,29 +15,30 @@ const AddDivision = () => {
             </Link>
 
             <div className="my-2">
-
-                <div className="lg:grid grid-cols-2 gap-2">
-                    <div className="my-2">
-                        <label htmlFor="">Division Name</label>
-                        <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Division Name"
-                        />
+                <form>
+                    <div className="lg:grid grid-cols-2 gap-2">
+                        <div className="my-2">
+                            <label htmlFor="">Division Name</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Division Name"
+                            />
+                        </div>
+                        <div className="my-2">
+                            <label htmlFor="">Location</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Location"
+                            />
+                        </div>
                     </div>
-                    <div className="my-2">
-                        <label htmlFor="">Location</label>
-                        <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Location"
-                        />
+                    <div className="lg:grid grid-cols-1">
+                        <div className="my-2">
+                            <label htmlFor="">HOD Email</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter HOD Email"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="lg:grid grid-cols-1">
-                    <div className="my-2">
-                        <label htmlFor="">HOD Email</label>
-                        <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter HOD Email"
-                        />
+                    <div className="">
+                        <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Add Designation</button>
                     </div>
-                </div>
-                <div className="">
-                    <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Add Designation</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
