1727c9de603865a3a5e036d4670ec4120040d3cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:46:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Division/Divisions.jsx
+++ client/src/components/Division/Divisions.jsx
@@ -24,12 +24,12 @@ const Divisions = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                <h1 className="text-xl font-semibold">Designations</h1>        
+                <h1 className="text-xl font-semibold">Divisions</h1>        
                 <hr className="mb-4" />
                     <div className="lg:flex">
                         <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                        <Link to={'/AddDesignation'}>
-                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Designation</button>
+                        <Link to={'/AddDivision'}>
+                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Division</button>
                         </Link>
                     </div>
                 </div>
