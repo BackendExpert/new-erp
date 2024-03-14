f87c314a2b27a3034b72131eb4f42f719ad8970e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:12:30 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -19,20 +19,30 @@ const Projects = () => {
     }
   } 
 
-  return (
-    <div className="bg-gray-200 py-4">
-      <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-        <h1 className="text-xl font-semibold">All Projects</h1>
-        <hr className="mb-4" />
-        <div className="lg:flex">
-            <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-            <Link to={'/AddProject'}>
-                <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Project</button>
-            </Link>
-          </div>
+  // this page can access by following users
+  // SuperAdmin, Admin
+
+  if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+    return (
+      <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+          <h1 className="text-xl font-semibold">All Projects</h1>
+          <hr className="mb-4" />
+          <div className="lg:flex">
+              <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+              <Link to={'/AddProject'}>
+                  <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Project</button>
+              </Link>
+            </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else{
+    useEffect(() => {
+        navigete('/UnAccess');
+      }, [])
+  }
 }
 
 export default Projects
