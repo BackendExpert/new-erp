ba525d2bc5c851b7b007a3f2a1562e2f4d147ea3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:44:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Division/Divisions.jsx
+++ client/src/components/Division/Divisions.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 const Divisions = () => {
-  return (
-    <div>Divisions</div>
-  )
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    //go back according to login user
+    const headleBack = () => {
+        if(RoleUser === "SuperAdmin"){
+            navigate('/superAdmin');
+        }
+        else if(RoleUser === "Admin"){
+            navigate('/admin');
+        }
+    }
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        return (
+            <div>Divisions</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
 }
 
 export default Divisions
