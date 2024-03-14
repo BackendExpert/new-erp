9c4be8627513d0924fb70534d55afd8125a42921
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:58:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Division/AddDivision.jsx
+++ client/src/components/Division/AddDivision.jsx
@@ -8,7 +8,7 @@ const AddDivision = () => {
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Add New Designations</h1>        
+            <h1 className="text-xl font-semibold">Add New Division</h1>        
             <hr className="mb-4" />
             <Link to={'/Divisions'}>
                 <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
