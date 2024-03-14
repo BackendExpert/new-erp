78c79439a8a7706cbe3fbf12204fad566b3702e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-14 16:14:26 +0530

    UPdate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Projects/AddProject.jsx
+++ client/src/components/Projects/AddProject.jsx
@@ -8,11 +8,29 @@ const AddProject = () => {
 
     //get the login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
+  // this page can access by following users
+  // SuperAdmin, Admin
 
-    
-  return (
-    <div>AddProject</div>
-  )
+  if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+    return (
+      <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+          <h1 className="text-xl font-semibold">Add New Projects</h1>
+          <hr className="mb-4" />
+          <div className="lg:flex">
+              <Link to={'/Projects'}>
+                <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+              </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+        navigate('/UnAccess');
+      }, [])
+  }
 }
 
 export default AddProject
