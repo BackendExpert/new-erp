31d45d943420eacb7fd03b10118c8ff8b5d95a72
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-15 21:06:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Equipments/Equipments.jsx
+++ client/src/components/Equipments/Equipments.jsx
@@ -27,7 +27,7 @@ const Equipments = () => {
                     <hr className="mb-4" />
                         <div className="lg:flex">
                             <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                            <Link to={'/AddDivision'}>
+                            <Link to={'/AddEquipment'}>
                                 <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Division</button>
                             </Link>
                         </div>
