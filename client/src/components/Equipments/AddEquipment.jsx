a29ab44eea8f322e295aafe841412393844a2ca1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-15 20:51:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Equipments/AddEquipment.jsx
+++ client/src/components/Equipments/AddEquipment.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddEquipment = () => {
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    
   return (
     <div>AddEquipment</div>
   )
