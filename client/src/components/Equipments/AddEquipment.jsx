83e6c67b763c2b42c367ba89318768f170ee02bb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-15 20:47:09 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/Equipments/AddEquipment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddEquipment = () => {
+  return (
+    <div>AddEquipment</div>
+  )
+}
+
+export default AddEquipment
---
+++ client/src/components/Equipments/UpdateEquipment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateEquipment = () => {
+  return (
+    <div>UpdateEquipment</div>
+  )
+}
+
+export default UpdateEquipment
