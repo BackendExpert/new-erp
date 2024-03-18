789fce3a268e9ff3f48a2deb556fd914ce429121
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 20:29:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Journals/AddJournal.jsx
+++ client/src/components/library/Journals/AddJournal.jsx
@@ -13,11 +13,17 @@ const AddJournal = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Add New Equipment</h1>        
+                    <h1 className="text-xl font-semibold">Add New Journal</h1>        
                     <hr className="mb-4" />
                     <Link to={'/Journals'}>
                         <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                     </Link>
+
+                    <div className="my-2">
+                        <form>
+                            
+                        </form>
+                    </div>
                 </div>
             </div>    
         )
