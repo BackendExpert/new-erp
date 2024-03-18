6397d7738841c0c74aa5f87450b89287bf0f3895
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 19:52:24 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/library/Journals/AddJournal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddJournal = () => {
+  return (
+    <div>AddJournal</div>
+  )
+}
+
+export default AddJournal
---
+++ client/src/components/library/Journals/Journals.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Journals = () => {
+  return (
+    <div>Journals</div>
+  )
+}
+
+export default Journals
