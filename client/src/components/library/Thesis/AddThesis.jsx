638fe7e42cdc144f85aaeab0a18ad90cdcc9d385
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 22:44:44 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/library/Thesis/AddThesis.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddThesis = () => {
+  return (
+    <div>AddThesis</div>
+  )
+}
+
+export default AddThesis
---
+++ client/src/components/library/Thesis/Thesis.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Thesis = () => {
+  return (
+    <div>Thesis</div>
+  )
+}
+
+export default Thesis
