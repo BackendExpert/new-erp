e0a2257740bb1029d3f5a1df98b67d2aa79304fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 11:15:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/UnAccessUsers.jsx
+++ client/src/components/SuperAdmin/UnAccessUsers.jsx
@@ -20,7 +20,21 @@ const UnAccessUsers = () => {
 
     if(RoleUser === "SuperAdmin"){
         return (
-            <div>Hi all</div>
+            <div className="relative overflow-x-auto my-8">
+            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                    <tr className='text-blue-500'>
+                        <th scope='col' className='px-6 py-3'>Email</th>
+                        <th scope='col' className='px-6 py-3'>Username</th>
+                        <th scope='col' className='px-6 py-3'>User Role</th>                            
+                        <th scope='col' className='px-6 py-3'>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
         )
     }
     else{
