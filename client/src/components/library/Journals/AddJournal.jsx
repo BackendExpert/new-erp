795013b8c69525ca5b1e34c284b7a0f2157b0e14
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 20:27:54 +0530

    Update AddJournal.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Journals/AddJournal.jsx
+++ client/src/components/library/Journals/AddJournal.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import secureLocalStorage from 'react-secure-storage'
 
 const AddJournal = () => {
