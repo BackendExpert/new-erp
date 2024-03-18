611785ce8ec7f9733d2ba682f2901d20fd1a6a58
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 12:16:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/UsersUnAccess.jsx
+++ client/src/components/SuperAdmin/UsersUnAccess.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const UsersUnAccess = () => {
-  return (
-    <div>UsersUnAccess</div>
-  )
+    const navigate = useNavigate();
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+
+    if(RoleUser === "SuperAdmin"){
+        return (
+            <div>UsersUnAccess</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default UsersUnAccess
