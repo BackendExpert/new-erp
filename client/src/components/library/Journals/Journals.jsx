aa44b67139b263871c50f314752523d637a81d93
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 21:07:43 +0530

    Update Journals.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Journals/Journals.jsx
+++ client/src/components/library/Journals/Journals.jsx
@@ -23,6 +23,24 @@ const Journals = () => {
                         <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Journals</button>
                     </Link>
                 </div>
+                    <div className="relative overflow-x-auto my-8">
+                        <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                <tr className='text-blue-500'>
+                                <th scope='col' className='px-6 py-3'>ID</th>
+                                <th scope='col' className='px-6 py-3'>Inventory No</th>
+                                <th scope='col' className='px-6 py-3'>Equipment Name</th>                            
+                                <th scope='col' className='px-6 py-3'>Purchase Value</th>
+                                <th scope='col' className='px-6 py-3'>Purchase Date</th>
+                                <th scope='col' className='px-6 py-3'>Location</th>                                        
+                                <th scope='col' className='px-6 py-3'>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>  
         )
