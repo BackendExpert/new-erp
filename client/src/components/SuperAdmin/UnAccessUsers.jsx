e791ddeac6259440f46a589f6c08cdfe7c472981
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 06:52:12 +0530

    Create UnAccessUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/SuperAdmin/UnAccessUsers.jsx
@@ -0,0 +1,12 @@
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
+const UnAccessUsers = () => {
+  return (
+    <div>UnAccessUsers</div>
+  )
+}
+
+export default UnAccessUsers
