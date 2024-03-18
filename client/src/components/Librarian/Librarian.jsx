0eea92bc48b5af2e83ab241dc5c5f0be7addad8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 20:22:37 +0530

    Update Librarian.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Librarian/Librarian.jsx
+++ client/src/components/Librarian/Librarian.jsx
@@ -35,7 +35,7 @@ const Librarian = ({children}) => {
           <div className="w-full mx-2">
             <Navlist />
                 <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
-                    <h1 className="text-2xl">Welcome to SuperAdmin Dashboard</h1>
+                    <h1 className="text-2xl">Welcome to Librarian Dashboard</h1>
                     <hr className="mt-2 border-blue-100 border-2" />
                     <SummaryDash />
                 </div>
