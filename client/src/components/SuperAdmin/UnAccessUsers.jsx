3dc1ea03668d3764400e2628d07a0e2d7fec1562
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 11:37:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/UnAccessUsers.jsx
+++ client/src/components/SuperAdmin/UnAccessUsers.jsx
@@ -20,29 +20,36 @@ const UnAccessUsers = () => {
 
     if(RoleUser === "SuperAdmin"){
         return (
-            <div className="overflow-x-auto my-8">
-            <table className="table-auto w-full">
-                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
-                    <tr className='text-blue-500'>
-                        <th scope='col' className='px-6 py-3'>Email</th>
-                        <th scope='col' className='px-6 py-3'>Username</th>
-                        <th scope='col' className='px-6 py-3'>User Role</th>                            
-                        <th scope='col' className='px-6 py-3'>Action</th>
-                    </tr>
-                </thead>
-                <tbody>                    
-                    {
-                        unUsers.map((user, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td className='px-6 py-4 font-bold'>{user.email}</td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
+            <div className="">
+                <h1 className="text-3xl font-semibold">Unauthorized Users</h1>
+                <div className="overflow-x-auto my-8">
+                    <table className="table-auto w-full">
+                    <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                        <tr className='text-blue-500'>
+                            <th scope='col' className='px-6 py-3'>Email</th>
+                            <th scope='col' className='px-6 py-3'>Username</th>
+                            <th scope='col' className='px-6 py-3'>User Role</th>                            
+                            <th scope='col' className='px-6 py-3'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>                    
+                        {
+                            unUsers.map((user, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className='px-6 py-4 font-bold'>{user.email}</td>
+                                        <td className='px-6 py-4 font-bold'>{user.username}</td>
+                                        <td className='px-6 py-4 font-bold'>{user.role}</td>
+
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            </div>            
+
         )
     }
     else{
