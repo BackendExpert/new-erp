7ba5909be5b5dcb004c5587bdb7c25ed243aecfa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-18 20:01:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Journals/AddJournal.jsx
+++ client/src/components/library/Journals/AddJournal.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
 
 const AddJournal = () => {
+    const navigate = useNavigate()
+
+    //check the current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(R)
   return (
     <div>AddJournal</div>
   )
