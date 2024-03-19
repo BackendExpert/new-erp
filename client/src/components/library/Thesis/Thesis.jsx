60bcbaf38b8dda771cb7410eab17c6a22f45e2ac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 05:54:36 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Thesis/Thesis.jsx
+++ client/src/components/library/Thesis/Thesis.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
 
 const Thesis = () => {
+    //check the current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    const navigate = useNavigate();
+    const [DataThesis, SetDataThesis] = useState({
+        
+    })
+
   return (
-    <div>Thesis</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Thesis</h1>        
+            <hr className="mb-4" />
+            <div className="lg:flex">
+                <Link to={'/Librarian'}>
+                    <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                </Link>                     
+                <Link to={'/AddJournal'}>
+                    <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Journals</button>
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
