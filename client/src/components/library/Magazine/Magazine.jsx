22af32815b3fe63c106e36fd0ebaa56fcedb98e7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 20:16:51 +0530

    Update Magazine.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Magazine/Magazine.jsx
+++ client/src/components/library/Magazine/Magazine.jsx
@@ -23,6 +23,25 @@ const Magazine = () => {
                             <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Magazine</button>
                         </Link>
                     </div>
+                        <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                    <th scope='col' className='px-6 py-3'>Journal ID</th>
+                                    <th scope='col' className='px-6 py-3'>Journal Title</th>
+                                    <th scope='col' className='px-6 py-3'>Journal Category</th>                            
+                                    <th scope='col' className='px-6 py-3'>Publisher</th>
+                                    <th scope='col' className='px-6 py-3'>Year</th>
+                                    <th scope='col' className='px-6 py-3'>Impact Factor</th>          
+                                    <th scope='col' className='px-6 py-3'>Indexing</th>                                       
+                                    <th scope='col' className='px-6 py-3'>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         )
