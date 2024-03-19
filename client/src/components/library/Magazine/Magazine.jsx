7dba10a972bba11cdd7dc5eae71db2e2252101bf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 19:47:25 +0530

    Update Magazine.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Magazine/Magazine.jsx
+++ client/src/components/library/Magazine/Magazine.jsx
@@ -6,9 +6,33 @@ import secureLocalStorage from 'react-secure-storage'
 const Magazine = () => {
     //check the current login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
-  return (
-    <div>Magazine</div>
-  )
+
+    const navigate = useNavigate()
+
+    if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Journals</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                        <Link to={'/Librarian'}>
+                            <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        </Link>                     
+                        <Link to={'/AddJournal'}>
+                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Journals</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default Magazine
