47adaf9cfb3e7bb48cbf80aea7c0161d907cf1c0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 05:56:31 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Thesis/Thesis.jsx
+++ client/src/components/library/Thesis/Thesis.jsx
@@ -20,7 +20,7 @@ const Thesis = () => {
                         <Link to={'/Librarian'}>
                             <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                         </Link>                     
-                        <Link to={'/AddJournal'}>
+                        <Link to={'/AddThesis'}>
                             <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Journals</button>
                         </Link>
                     </div>
