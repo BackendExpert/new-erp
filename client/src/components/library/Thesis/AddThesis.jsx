35a8dee19f0c3082e30b4fb7a8078ba274f36eb1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 06:02:12 +0530

    Update AddThesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Thesis/AddThesis.jsx
+++ client/src/components/library/Thesis/AddThesis.jsx
@@ -9,6 +9,47 @@ const AddThesis = () => {
             <Link to={'/Thesis'}>
                 <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
             </Link>
+
+            <div className="my-2">
+                <form>
+
+                    <div className="lg:grid grid-cols-3 gap-3">
+                        <div className="">
+                            <label htmlFor="">Thesis Title</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Journal Title"
+                            /> 
+                        </div>
+                        <div className="">
+                            <label htmlFor="">Thesis Author</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Journal Title"
+                            /> 
+                        </div>
+                        <div className="">
+                            <label htmlFor="">Year of Publication</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Journal Title"
+                            /> 
+                        </div>
+                    </div>
+
+                    <div className="lg:grid grid-cols-3 gap-3">
+                        <div className="">
+                            <label htmlFor="">Subject Area</label>
+                            <input type="text" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Journal Title"
+                            /> 
+                        </div>
+                        <div className="">
+                            <label htmlFor="">Degree</label>
+                            <select className="w-full h-12 my-2 border border-blue-500 rounded pl-2"
+                                    onChange={e => SetjournalData({...journalData, category:e.target.value})}>
+                                    <option >Select Option</option>
+                                    <option value='Non-Refereed Journal'>Non-Refereed Journal</option>
+                                    <option value='International Journal'>International Journal</option>
+                                    <option value='Local Journal'>Local Journal</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
   )
