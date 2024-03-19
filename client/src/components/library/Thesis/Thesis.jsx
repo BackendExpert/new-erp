6ade5e2192d427f4f7733a9a7611e3e826c3f7a1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-19 06:29:39 +0530

    Update Thesis.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Thesis/Thesis.jsx
+++ client/src/components/library/Thesis/Thesis.jsx
@@ -10,6 +10,13 @@ const Thesis = () => {
     const navigate = useNavigate();
     const [DataThesis, SetDataThesis] = useState([])
 
+    //fetch data
+    useEffect(() => {
+        axios.post('http://localhost:8081/ViewThesis')
+        .then(res => SetDataThesis(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
     if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
         return (
             <div className="bg-gray-200 py-4">
@@ -24,8 +31,28 @@ const Thesis = () => {
                             <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Thesis</button>
                         </Link>
                     </div>
+                    </div>
+                        <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                    <th scope='col' className='px-6 py-3'>Journal ID</th>
+                                    <th scope='col' className='px-6 py-3'>Journal Title</th>
+                                    <th scope='col' className='px-6 py-3'>Journal Category</th>                            
+                                    <th scope='col' className='px-6 py-3'>Publisher</th>
+                                    <th scope='col' className='px-6 py-3'>Year</th>
+                                    <th scope='col' className='px-6 py-3'>Impact Factor</th>          
+                                    <th scope='col' className='px-6 py-3'>Indexing</th>                                       
+                                    <th scope='col' className='px-6 py-3'>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+  
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+
           )
     }
     else{
