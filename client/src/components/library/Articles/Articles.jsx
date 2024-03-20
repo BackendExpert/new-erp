d12c728a74b4a86fc2dd7cd75277b21790af5758
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 11:45:27 +0530

    Update Articles.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/library/Articles/Articles.jsx
+++ client/src/components/library/Articles/Articles.jsx
@@ -8,42 +8,50 @@ const Articles = () => {
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
 
+    if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Articles</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                        <Link to={'/Librarian'}>
+                            <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        </Link>                     
+                        <Link to={'/AddArticle'}>
+                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Article</button>
+                        </Link>
+                    </div>
+                    <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                    <th scope='col' className='px-6 py-3'>Article ID</th>
+                                    <th scope='col' className='px-6 py-3'>Article Title</th>
+                                    <th scope='col' className='px-6 py-3'>Article Category</th>                            
+                                    <th scope='col' className='px-6 py-3'>Journal/Conference</th>
+                                    <th scope='col' className='px-6 py-3'>Publish Year</th>
+                                    <th scope='col' className='px-6 py-3'>Authors</th>          
+                                    <th scope='col' className='px-6 py-3'>Page Numbers</th>                                       
+                                    <th scope='col' className='px-6 py-3'>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
     
-  return (
-        <div className="bg-gray-200 py-4">
-            <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                <h1 className="text-xl font-semibold">Articles</h1>        
-                <hr className="mb-4" />
-                <div className="lg:flex">
-                    <Link to={'/Librarian'}>
-                        <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                    </Link>                     
-                    <Link to={'/AddArticle'}>
-                        <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Article</button>
-                    </Link>
-                </div>
-                <div className="relative overflow-x-auto my-8">
-                        <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
-                                <tr className='text-blue-500'>
-                                <th scope='col' className='px-6 py-3'>Article ID</th>
-                                <th scope='col' className='px-6 py-3'>Article Title</th>
-                                <th scope='col' className='px-6 py-3'>Article Category</th>                            
-                                <th scope='col' className='px-6 py-3'>Journal/Conference</th>
-                                <th scope='col' className='px-6 py-3'>Publish Year</th>
-                                <th scope='col' className='px-6 py-3'>Authors</th>          
-                                <th scope='col' className='px-6 py-3'>Page Numbers</th>                                       
-                                <th scope='col' className='px-6 py-3'>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-  )
+      )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
+
 }
 
 export default Articles
