15bb96b44c79c5ade71c801c0e1066af3864c127
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 11:03:00 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/library/Articles/AddArticles.jsx
+++ client/src/components/library/Articles/AddArticles.jsx
@@ -7,7 +7,15 @@ const AddArticles = () => {
     //check the current login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
   return (
-    <div>AddArticles</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Add New Journal</h1>        
+            <hr className="mb-4" />
+            <Link to={'/Journals'}>
+                <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
--- client/src/components/library/Articles/Articles.jsx
+++ client/src/components/library/Articles/Articles.jsx
@@ -7,9 +7,22 @@ const Articles = () => {
     //check the current login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
-    
+
   return (
-    <div>Articles</div>
+        <div className="bg-gray-200 py-4">
+            <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                <h1 className="text-xl font-semibold">Journals</h1>        
+                <hr className="mb-4" />
+                <div className="lg:flex">
+                    <Link to={'/Librarian'}>
+                        <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    </Link>                     
+                    <Link to={'/AddJournal'}>
+                        <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Journals</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
   )
 }
 
