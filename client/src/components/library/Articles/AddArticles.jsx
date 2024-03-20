1620a2fba5a4d2a3fb2f7fce219c5e4b2454cc64
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 11:01:50 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/library/Articles/AddArticles.jsx
+++ client/src/components/library/Articles/AddArticles.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
 
 const AddArticles = () => {
+    //check the current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
   return (
     <div>AddArticles</div>
   )
--- client/src/components/library/Articles/Articles.jsx
+++ client/src/components/library/Articles/Articles.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
 
 const Articles = () => {
+    //check the current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    
   return (
     <div>Articles</div>
   )
