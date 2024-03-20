b6790b7e51e7b9e09c1751adb440267b9cc8d095
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 11:06:53 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/library/Articles/Articles.jsx
+++ client/src/components/library/Articles/Articles.jsx
@@ -11,7 +11,7 @@ const Articles = () => {
   return (
         <div className="bg-gray-200 py-4">
             <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                <h1 className="text-xl font-semibold">Journals</h1>        
+                <h1 className="text-xl font-semibold">Articles</h1>        
                 <hr className="mb-4" />
                 <div className="lg:flex">
                     <Link to={'/Librarian'}>
