0711e733a4f1a41a6358c6c58441bd4e2ec5873c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 11:56:20 +0530

    Update Articles.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/Articles/Articles.jsx
+++ client/src/components/library/Articles/Articles.jsx
@@ -52,6 +52,12 @@ const Articles = () => {
                                         return(
                                             <tr key={index}>
                                                 <td className='px-6 py-4 font-bold'>{article.Aid}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.title}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.category}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.journal}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.Aid}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.Aid}</td>
+                                                <td className='px-6 py-4 font-bold'>{article.Aid}</td>
                                             </tr>
                                         )
                                     })
