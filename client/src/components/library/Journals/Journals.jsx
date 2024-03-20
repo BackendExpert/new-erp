5b558cfb82495b3a48b6528ed839519a91bab547
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-20 08:55:15 +0530

    Update
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- client/src/components/library/Journals/Journals.jsx
+++ client/src/components/library/Journals/Journals.jsx
@@ -34,7 +34,7 @@ const Journals = () => {
 
     if(RoleUser === "Librarian" || RoleUser === "SuperAdmin"){
         return (
-        <div className="bg-gray-200 py-4">
+            <div className="bg-gray-200 py-4">
             <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
                 <h1 className="text-xl font-semibold">Journals</h1>        
                 <hr className="mb-4" />
