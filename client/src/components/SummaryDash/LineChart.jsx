77e7c2774e765082f2e5737a4035d1b8a47cf7ee
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-22 19:16:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/LineChart.jsx
+++ client/src/components/SummaryDash/LineChart.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import { Line, defaults } from 'react-chartjs-2'
+import axios from "axios";
+import React, { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
 
 const LineChart = () => {
   return (
