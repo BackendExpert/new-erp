5a1b890c43a18ab5eba64fb6640c97069f4b9dc2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-22 12:30:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Projects/UpdateProject.jsx
+++ client/src/components/Projects/UpdateProject.jsx
@@ -34,52 +34,61 @@ const UpdateProject = () => {
 
     //get the login user
     const RoleUser = secureLocalStorage.getItem("loginNew");
-  return (
-    <div className="bg-gray-200 py-4">
-      <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-        <h1 className="text-xl font-semibold">Update Project</h1>
-        <hr className="mb-4" />
-        <div className="flex">
-          <Link to={'/Projects'}>
-            <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-          </Link>
-        </div>
 
-        <div className="my-2">
-          <form onSubmit={headleUpdate}>
-            <div className="lg:grid grid-cols-2 gap-2">
-                <div className="my-2">
-                  <label htmlFor="">New Division No</label>
-                  <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Division No"
-                  value={updateProject.divno} onChange={e => SetProjectData({...ProjectData, divno:e.target.value})}/>
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+      return (
+        <div className="bg-gray-200 py-4">
+          <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Update Project</h1>
+            <hr className="mb-4" />
+            <div className="flex">
+              <Link to={'/Projects'}>
+                <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+              </Link>
+            </div>
+    
+            <div className="my-2">
+              <form onSubmit={headleUpdate}>
+                <div className="lg:grid grid-cols-2 gap-2">
+                    <div className="my-2">
+                      <label htmlFor="">New Division No</label>
+                      <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Division No"
+                      value={updateProject.divno} onChange={e => SetProjectData({...ProjectData, divno:e.target.value})}/>
+                    </div>
+    
+                    <div className="my-2">
+                      <label htmlFor="">New HOD Email</label>
+                      <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Project Name"
+                      value={updateProject.hod} onChange={e => SetProjectData({...ProjectData, hod:e.target.value})}/>
+                    </div>
                 </div>
-
-                <div className="my-2">
-                  <label htmlFor="">New HOD Email</label>
-                  <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Project Name"
-                  value={updateProject.hod} onChange={e => SetProjectData({...ProjectData, hod:e.target.value})}/>
+                <div className="lg:grid grid-cols-2 gap-2">
+                  <div className="my-2">
+                      <label htmlFor="">New First Research Assistant</label>
+                      <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter First RA Email"
+                      value={updateProject.ra1} onChange={e => SetProjectData({...ProjectData, ra1:e.target.value})}/>
+                  </div>
+                  <div className="my-2">
+                      <label htmlFor="">New Second Research Assistant</label>
+                      <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Second RA Email"
+                      value={updateProject.ra2} onChange={e => SetProjectData({...ProjectData, ra2:e.target.value})}/>
+                  </div>
                 </div>
+                <div className="">
+                  <button type="submit" className="border border-green-500 rounded py-4 px-16 my-4 text-green-500 font-semibold duration-500 hover:bg-green-400 hover:text-white hover:shadow-xl">Update Program</button>
+                </div>
+              </form>
             </div>
-            <div className="lg:grid grid-cols-2 gap-2">
-              <div className="my-2">
-                  <label htmlFor="">New First Research Assistant</label>
-                  <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter First RA Email"
-                  value={updateProject.ra1} onChange={e => SetProjectData({...ProjectData, ra1:e.target.value})}/>
-              </div>
-              <div className="my-2">
-                  <label htmlFor="">New Second Research Assistant</label>
-                  <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Second RA Email"
-                  value={updateProject.ra2} onChange={e => SetProjectData({...ProjectData, ra2:e.target.value})}/>
-              </div>
-            </div>
-            <div className="">
-              <button type="submit" className="border border-green-500 rounded py-4 px-16 my-4 text-green-500 font-semibold duration-500 hover:bg-green-400 hover:text-white hover:shadow-xl">Update Program</button>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
-  )
+      )
+    }
+    else{
+      useEffect(() => {
+        navigate('/UnAccess');
+      }, [])
+    }
+
 }
 
 export default UpdateProject
