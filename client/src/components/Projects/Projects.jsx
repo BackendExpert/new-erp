648cae27502ae15037e084e08fa9efccd566941b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-22 11:53:16 +0530

    Update Projects.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -67,6 +67,15 @@ const Projects = () => {
                                         <td className='px-6 py-4 font-bold'>{project.hod}</td>
                                         <td className='px-6 py-4 font-bold'>{project.ra1}</td>
                                         <td className='px-6 py-4 font-bold'>{project.ra2}</td>
+                                        <td className='px-6 py-4 font-bold'>
+                                          <div className="flex">
+                                              <Link to={'/UpdateProgram/' + program.pid}>
+                                                  <button className="rounded mx-2 py-2 px-6 border border-blue-500 text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Update</button>
+                                              </Link>
+                                              <button onClick={()=>{handleDelete(program.pid)}} className="rounded mx-2 py-2 px-6 border border-red-500 text-red-500 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Delete</button>
+                                          </div>
+                                        </td>
+
                                       </tr>
                                     )
                                 })
