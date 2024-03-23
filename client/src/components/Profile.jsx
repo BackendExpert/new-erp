dfd8c5441cc6a013aba878d3903d02f713965f44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-23 12:26:20 +0530

    Update Profile.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import axios from "axios";
 
 const Profile = () => {
+    const navigate = useNavigate()
+    
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    
   return (
     <div>Profile</div>
   )
