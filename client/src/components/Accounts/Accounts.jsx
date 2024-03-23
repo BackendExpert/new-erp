5bc16791822b7e2a5e19b8f5646291d65eeb1f5c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-23 10:08:49 +0530

    Update Accounts.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/Accounts.jsx
+++ client/src/components/Accounts/Accounts.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const Accounts = () => {
-  return (
-    <div>Accounts</div>
-  )
+
+    const navigate = useNavigate();
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        return (
+            <div>Accounts</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default Accounts
