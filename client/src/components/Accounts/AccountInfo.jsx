4e4dbb5662608eda2397f4bdc9fc878564a5d994
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-23 11:22:29 +0530

    Update AccountInfo.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/AccountInfo.jsx
+++ client/src/components/Accounts/AccountInfo.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AccountInfo = () => {
-  return (
-    <div>AccountInfo</div>
-  )
+    const navigate = useNavigate()
+
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        return (
+            <div>AccountInfo</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default AccountInfo
