23d8333d0cb51da401931f63644fe2f8cab9c6f0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-23 11:26:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/AccountInfo.jsx
+++ client/src/components/Accounts/AccountInfo.jsx
@@ -13,7 +13,15 @@ const AccountInfo = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+    const [viewUserData, SetviewUserData] = useState({
+        username: '',
+        email: '',
+        role: '',
+        is_active: ''
+    })
+
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin"){
+        
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
