b677f816b3e30a5717edd1d326a4680d2ea402a9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-23 11:23:42 +0530

    Update AccountInfo.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Accounts/AccountInfo.jsx
+++ client/src/components/Accounts/AccountInfo.jsx
@@ -18,8 +18,10 @@ const AccountInfo = () => {
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
                     <h1 className="text-xl font-semibold">Designations</h1>        
                     <hr className="mb-4" />
-                        <div className="lg:flex">
+                    <div className="lg:flex">
+                        <Link>
                             <button className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        </Link>        
                     </div>
                 </div>
             </div>
