6f03021213da8fe6f2440372302fbc41cb47d4e0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:21:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/Director/DirectorDash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DirectorDash = () => {
+  return (
+    <div>DirectorDash</div>
+  )
+}
+
+export default DirectorDash
---
+++ client/src/components/Director/DirectorSide.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DirectorSide = () => {
+  return (
+    <div>DirectorSide</div>
+  )
+}
+
+export default DirectorSide
