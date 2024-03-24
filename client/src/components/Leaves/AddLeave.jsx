6ddc4552fe8c34fe778042837f75e192e96014d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:14:48 +0530

    Update AddLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/AddLeave.jsx
+++ client/src/components/Leaves/AddLeave.jsx
@@ -4,6 +4,10 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 
 const AddLeave = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
         //go back according to login user
         const headleBack = () => {
             if(RoleUser === "SuperAdmin"){
@@ -34,7 +38,7 @@ const AddLeave = () => {
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Add New Division</h1>        
+            <h1 className="text-xl font-semibold">New Leave Request</h1>        
             <hr className="mb-4" />
             <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
         </div>
