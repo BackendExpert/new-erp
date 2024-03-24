b0b7f19b5adcb0be0edad357f56678d584299190
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 18:50:55 +0530

    Update AddLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/AddLeave.jsx
+++ client/src/components/Leaves/AddLeave.jsx
@@ -50,6 +50,18 @@ const AddLeave = () => {
       Email:''
     })
 
+    const headleSubmit = (e) =>{
+      e.preventDefault(); 
+      axios.post('http://localhost:8081/RequestLeave', LeaveData)
+      .then(res => {
+        if(res.data.Status === "Success"){
+          alert("Leave Request is Successful Added Wait for the Approve")
+        }
+        else{
+          alert(res.data.Error)
+        }
+      })
+    }
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
@@ -58,7 +70,7 @@ const AddLeave = () => {
             <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
 
             <div className="my-2">
-              <form>
+              <form onSubmit={headleSubmit}>
                 <div className="lg:grid grid-cols-3 gap-4">
 
                   <div className="">
@@ -133,6 +145,9 @@ const AddLeave = () => {
                     onChange={e => SetLeaveData({...LeaveData, designation:e.target.value})}/>
                   </div>
 
+                </div>
+                <div className="">
+                  <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Leave</button>
                 </div>
               </form>
             </div>                
