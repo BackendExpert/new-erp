fead17c6f1adbddccbb1749344eb3f59735b3d62
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:25:54 +0530

    Update DirSecNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/DirSecNav.jsx
+++ client/src/components/NavBar/DirSecNav.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 
 const DirSecNav = () => {
+    const [navopen, SetNavOpen] = useState()
+
+    const navigate = useNavigate();
+
+    const navLists = [
+        {name: "Leave Requests", link: "/AddLeave"},
+        {name: "Vehicle Reservations", link: ""},
+        {name: "SRN Requests", link: ""},
+        {name: "Work Requests", link: ""},
+        {name: "Gatepass Requests", link: ""},
+        {name: "Increment Requests", link: ""},
+        {name: "Profile", link: "/Profile"},
+        {name: "Logout", link: "", desc: "logout"}       
+    ];
+
+    const logout = () => {
+        localStorage.clear();
+        navigate('/')
+    }
+
   return (
-    <div>DirSecNav</div>
+    <div className="flex justify-between border-b-4 border-blue-300 shadow-xl mt-4 bg-white py-4 px-6 rounded">
+        NIFS
+        <div className="text-xl absolute cursor-pointer lg:hidden right-8" onClick={() => SetNavOpen(!navopen)}>
+            <Icons name={navopen ? 'close' : 'menu'} ></Icons>
+        </div>
+        <div className={`rounded lg:border-0 border-b-4 border-blue-400 lg:flex lg:items-center absolute lg:static bg-white transition-all lg:mt-0 mt-12 ${navopen ? 'lg:visible':'lg:visible invisible'}`}>
+
+            {
+                navLists.map((nav) => {
+                    if(nav.desc === "logout"){
+                        return (
+                            <div onClick={logout} className="cursor-pointer text-red-500 px-4 lg:py-0 py-4">{nav.name}</div>
+                        )
+                    }
+                    else{
+                        return (
+                            <Link to={nav.link}>
+                                <div className="cursor-pointer text-blue-500 px-4 lg:py-0 py-4 lg:border-0 border-b-4 border-blue-200">{nav.name}</div>
+                            </Link>                            
+                        )
+                    }
+                })
+            }
+        </div>
+    </div>
   )
 }
+
 
 export default DirSecNav
