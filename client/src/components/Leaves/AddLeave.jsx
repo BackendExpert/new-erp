ff87a7a8a951363fa51ecea0dcd8bb9b38de79cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:10:02 +0530

    Update AddLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/AddLeave.jsx
+++ client/src/components/Leaves/AddLeave.jsx
@@ -4,6 +4,33 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 
 const AddLeave = () => {
+        //go back according to login user
+        const headleBack = () => {
+            if(RoleUser === "SuperAdmin"){
+                navigate('/superAdmin');
+            }
+            else if(RoleUser === "Admin"){
+                navigate('/admin');
+            }
+            else if(RoleUser === "HOD"){
+              navigate('/hod');
+            }      
+            else if(RoleUser === "TO"){
+              navigate('/to');
+            }  
+            else if(RoleUser === "Librarian"){
+              navigate('/librarian');
+            } 
+            else if(RoleUser === "Labmanager"){
+              navigate('/labManager');
+            }     
+            else if(RoleUser === "Accountant"){
+              navigate('/accountant');
+            } 
+            else if(RoleUser === "User"){
+              navigate('/user');
+            } 
+        }
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
