fc06c0989ad6a0c0041032ce6fb0808a3bbec8ba
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:37:22 +0530

    Update DirectorDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Director/DirectorDash.jsx
+++ client/src/components/Director/DirectorDash.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage";
+import DirSecSide from '../DirSecSide/DirSecSide';
+import DirSecNav from '../NavBar/DirSecNav';
+import SummaryDash from '../SummaryDash/SummaryDash';
+import DashFooter from '../SummaryDash/dashFooter';
+
 
 const DirectorDash = () => {
-  return (
-    <div>DirectorDash</div>
-  )
+    const navigate = useNavigate();
+
+    // this is for prevent unauthorized access for this page
+    /*
+    
+      exmaple
+      Any use can access this after login there accounts 
+      using following code prevent unauthorized access
+       
+    */
+    // useEffect(() => {
+      const RoleUser = secureLocalStorage.getItem("loginNew");
+    
+    //   if(RoleUser !== "SuperAdmin"){
+    //     navigate('/');
+    //     localStorage.clear();
+    //   }
+    // }, []);
+
+
+  if(RoleUser === "Director"){
+    return (
+      <div className="bg-gray-200">
+          <div className="flex">
+              <DirSecSide />
+              <div className="w-full mx-2">
+                <DirSecNav />
+                <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
+                    <h1 className="text-2xl">Welcome to SuperAdmin Dashboard</h1>
+                    <hr className="mt-2 border-blue-100 border-2" />
+                    <SummaryDash />
+                </div>
+                  <DashFooter />
+              </div>
+          </div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      navigate('/UnAccess');
+    }, [])
+  }
+
+
 }
 
 export default DirectorDash
