04c9799e8356358451508e61611b5f6cc31211ef
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:09:46 +0530

    Update AddLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/AddLeave.jsx
+++ client/src/components/Leaves/AddLeave.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddLeave = () => {
   return (
-    <div>AddLeave</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Add New Division</h1>        
+            <hr className="mb-4" />
+            <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+        </div>
+    </div>
   )
 }
 
