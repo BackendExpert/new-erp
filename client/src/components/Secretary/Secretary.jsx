d12a33ef373cdd14b46a25d8f4a9294e8a850686
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-24 08:57:59 +0530

    Update Secretary.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Secretary/Secretary.jsx
+++ client/src/components/Secretary/Secretary.jsx
@@ -38,7 +38,7 @@ const Secretary = () => {
                 <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
                     <h1 className="text-2xl">Welcome to Secretary Dashboard</h1>
                     <hr className="mt-2 border-blue-100 border-2" />
-                    <SummaryDash />
+                    {/* <SummaryDash /> */}
                 </div>
                   <DashFooter />
               </div>
