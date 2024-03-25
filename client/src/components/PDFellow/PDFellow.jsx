19ac1844880d5a8800a977cb3072ae8483a75a56
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 14:03:23 +0530

    Update PDFellow.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/PDFellow/PDFellow.jsx
+++ client/src/components/PDFellow/PDFellow.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
+import PDFellowSide from './PDFellowSide'
+import OtherUserNav from '../NavBar/OtherUserNav'
+import SummaryDash from '../SummaryDash/SummaryDash'
+import DashFooter from '../SummaryDash/dashFooter'
+
 
 const PDFellow = () => {
     const navigate = useNavigate()
@@ -8,7 +13,20 @@ const PDFellow = () => {
 
     if(RoleUser === "PDFellow" || RoleUser === "SuperAdmin"){
         return (
-            <div>PDFellow</div>
+            <div className="bg-gray-200">
+                <div className="flex">
+                    <PDFellowSide />
+                    <div className="w-full mx-2">
+                    <OtherUserNav />
+                    <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
+                        <h1 className="text-2xl">Welcome to NonAcademic Dashboard</h1>
+                        <hr className="mt-2 border-blue-100 border-2" />
+                        <SummaryDash />
+                    </div>
+                        <DashFooter />
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
