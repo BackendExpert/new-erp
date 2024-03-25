412ed6e7746d14d0a318b60d850e6ae3d93f480d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 13:58:26 +0530

    Update PDFellowSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/PDFellow/PDFellowSide.jsx
+++ client/src/components/PDFellow/PDFellowSide.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const PDFellowSide = () => {
+  const [openSide, SetSideOpen] = useState();
+
+  const sidemenu = [
+      {name: "Leave Requests",  link: "/AddLeave", icon: <Icons name="log-out" size="large"></Icons>},
+      {name: "Vehicle Reservations",  link: "#", icon: <Icons name="car" size="large"></Icons>},
+      {name: "SRN Requests",  link: "#", icon: <Icons name="newspaper" size="large"></Icons>},
+      {name: "Work Requests",  link: "#", icon: <Icons name="document-text" size="large"></Icons>},
+      {name: "Gatepass Requests",  link: "#", icon: <Icons name="car" size="large"></Icons>},
+      {name: "Increment Requests",  link: "#", icon: <Icons name="cash" size="large"></Icons>},
+  ]
+  
   return (
     <div>PDFellowSide</div>
   )
