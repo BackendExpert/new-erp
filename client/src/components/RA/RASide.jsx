2ad9ee078d9288252b176506376f5d8497769b31
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:35:49 +0530

    Update RASide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RA/RASide.jsx
+++ client/src/components/RA/RASide.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import Icons from "@reacticons/ionicons"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 const RASide = () => {
+    const [openSide, SetSideOpen] = useState();
+
+    const sidemenu = [
+        {name: "Employess",  link: "/Employee", icon: <Icons name="people" size="large"></Icons>},
+        {name: "Divisions",  link: "/Divisions", icon: <Icons name="business" size="large"></Icons>},
+        {name: "Projects",  link: "/Projects", icon: <Icons name="newspaper" size="large"></Icons>},
+        {name: "Program",  link: "/Programs", icon: <Icons name="book" size="large"></Icons>},
+        {name: "Designation",  link: "/Designations", icon: <Icons name="easel" size="large"></Icons>},
+        {name: "Vehicle",  link: "/Vehicles", icon: <Icons name="car" size="large"></Icons>},
+        {name: "Equipment",  link: "/Equipments", icon: <Icons name="build" size="large"></Icons>},
+        {name: "Library",  link: "/librarian", icon: <Icons name="library" size="large"></Icons>},
+        {name: "Accounts",  link: "/Accounts", icon: <Icons name="people-circle" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>}
+    ]
   return (
     <div>RASide</div>
   )
