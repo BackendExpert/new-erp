8135a0d70e024a0bccf3b95e5add8b99ef2ce84f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:23:34 +0530

    Update OtherUserNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NavBar/OtherUserNav.jsx
+++ client/src/components/NavBar/OtherUserNav.jsx
@@ -3,6 +3,25 @@ import Icons from "@reacticons/ionicons"
 import { Link, useNavigate } from "react-router-dom"
 
 const OtherUserNav = () => {
+    const [navopen, SetNavOpen] = useState()
+
+    const navigate = useNavigate();
+
+    const navLists = [
+        {name: "Leave Requests", link: "/AddLeave"},
+        {name: "Vehicle Reservations", link: ""},
+        {name: "SRN Requests", link: ""},
+        {name: "Work Requests", link: ""},
+        {name: "Gatepass Requests", link: ""},
+        {name: "Increment Requests", link: ""},
+        {name: "Profile", link: "/Profile"},
+        {name: "Logout", link: "", desc: "logout"}       
+    ];
+
+    const logout = () => {
+        localStorage.clear();
+        navigate('/')
+    }
   return (
     <div>OtherUserNav</div>
   )
