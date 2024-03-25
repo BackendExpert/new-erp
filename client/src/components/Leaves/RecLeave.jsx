b834a41145c5b6630c33f5775974786d70ee77c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 15:17:08 +0530

    Update RecLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/RecLeave.jsx
+++ client/src/components/Leaves/RecLeave.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const RecLeave = () => {
   return (
-    <div>RecLeave</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Equipments</h1>        
+            <hr className="mb-4" />
+            <div className="lg:flex">                
+                <Link to={'/to'}>
+                    <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                </Link>
+            </div>
+        </div>
+    </div>    
   )
 }
 
