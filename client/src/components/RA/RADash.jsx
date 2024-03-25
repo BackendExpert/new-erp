fb5afabe1bd365fb2a13e2c6815b769afd86fa4c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:41:31 +0530

    Update RADash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RA/RADash.jsx
+++ client/src/components/RA/RADash.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
-
+import RASide from './RASide'
+import OtherUserNav from '../NavBar/OtherUserNav'
+import SummaryDash from '../SummaryDash/SummaryDash'
+import DashFooter from '../SummaryDash/dashFooter'
 
 const RADash = () => {
     const navigate = useNavigate()
@@ -9,7 +12,20 @@ const RADash = () => {
 
     if(RoleUser === "RA" || RoleUser === "SuperAdmin"){
         return (
-            <div>RADash</div>
+        <div className="bg-gray-200">
+            <div className="flex">
+                <RASide />
+                <div className="w-full mx-2">
+                  <OtherUserNav />
+                  <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
+                      <h1 className="text-2xl">Welcome to SuperAdmin Dashboard</h1>
+                      <hr className="mt-2 border-blue-100 border-2" />
+                      <SummaryDash />
+                  </div>
+                    <DashFooter />
+                </div>
+            </div>
+        </div>
         )
     }
     else{
