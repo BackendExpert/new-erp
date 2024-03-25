2da0a4e936de6452227db0328ba477855c247a1a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:40:08 +0530

    Update RADash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RA/RADash.jsx
+++ client/src/components/RA/RADash.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const RADash = () => {
-  return (
-    <div>RADash</div>
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "RA" || RoleUser === "SuperAdmin"){
+        return (
+            <div>RADash</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default RADash
