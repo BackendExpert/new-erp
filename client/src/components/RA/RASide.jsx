a5df53202cf4a0209aa22029978a1dd7a9159b24
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:43:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RA/RASide.jsx
+++ client/src/components/RA/RASide.jsx
@@ -6,16 +6,15 @@ const RASide = () => {
     const [openSide, SetSideOpen] = useState();
 
     const sidemenu = [
-        {name: "Employess",  link: "/Employee", icon: <Icons name="people" size="large"></Icons>},
-        {name: "Divisions",  link: "/Divisions", icon: <Icons name="business" size="large"></Icons>},
-        {name: "Projects",  link: "/Projects", icon: <Icons name="newspaper" size="large"></Icons>},
-        {name: "Program",  link: "/Programs", icon: <Icons name="book" size="large"></Icons>},
-        {name: "Designation",  link: "/Designations", icon: <Icons name="easel" size="large"></Icons>},
-        {name: "Vehicle",  link: "/Vehicles", icon: <Icons name="car" size="large"></Icons>},
-        {name: "Equipment",  link: "/Equipments", icon: <Icons name="build" size="large"></Icons>},
-        {name: "Library",  link: "/librarian", icon: <Icons name="library" size="large"></Icons>},
-        {name: "Accounts",  link: "/Accounts", icon: <Icons name="people-circle" size="large"></Icons>},
-        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>}
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        
     ]
   return (
     <div className={`duration-500 relative border-r-4 border-blue-300 shadow-xl my-4 mx-2 rounded bg-white h-auto pl-4 py-4 ${openSide ? "w-72" : "w-20" }`}>
