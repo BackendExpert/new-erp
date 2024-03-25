6ff39ff5b4420719c0987340abfe10f63a8461ac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 15:20:55 +0530

    Update RecLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/RecLeave.jsx
+++ client/src/components/Leaves/RecLeave.jsx
@@ -4,19 +4,43 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 
 const RecLeave = () => {
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Equipments</h1>        
-            <hr className="mb-4" />
-            <div className="lg:flex">                
-                <Link to={'/to'}>
-                    <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
-                </Link>
-            </div>
-        </div>
-    </div>    
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "TO" || RoleUser === "Director" || RoleUser === "Secretary"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Leaves</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">                
+                        <Link to={'/to'}>
+                            <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                        </Link>
+                    </div>
+
+                    <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                        <th scope='col' className='px-6 py-3'>ID</th>
+                                        <th scope='col' className='px-6 py-3'>Inventory No</th>
+                                        <th scope='col' className='px-6 py-3'>Equipment Name</th>                            
+                                        <th scope='col' className='px-6 py-3'>Purchase Value</th>
+                                        <th scope='col' className='px-6 py-3'>Purchase Date</th>
+                                        <th scope='col' className='px-6 py-3'>Location</th>                                        
+                                        <th scope='col' className='px-6 py-3'>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                </div>
+            </div>    
+        )
+    }
 }
 
 export default RecLeave
