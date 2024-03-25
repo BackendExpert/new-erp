c2acb0ac22c01ad7a13172fe7c02d7c91bf1cd18
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 11:22:43 +0530

    Create OtherUserNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/NavBar/OtherUserNav.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import Icons from "@reacticons/ionicons"
+import { Link, useNavigate } from "react-router-dom"
+
+const OtherUserNav = () => {
+  return (
+    <div>OtherUserNav</div>
+  )
+}
+
+export default OtherUserNav
