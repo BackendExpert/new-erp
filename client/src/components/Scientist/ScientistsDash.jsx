35cebb263ddae42db0f8e92d29ce296e0e32bc94
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 12:08:38 +0530

    Update ScientistsDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Scientist/ScientistsDash.jsx
+++ client/src/components/Scientist/ScientistsDash.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const ScientistsDash = () => {
-  return (
-    <div>ScientistsDash</div>
-  )
+  const navigate = useNavigate()
+  const RoleUser = secureLocalStorage.getItem("loginNew");
+
+  if(RoleUser === "RA" || RoleUser === "SuperAdmin"){
+    return (
+      <div>ScientistsDash</div>
+    )
+  }
+  else{
+    useEffect(() => {
+      navigate('/UnAccess');
+    }, [])
+  }
+
 }
 
 export default ScientistsDash
