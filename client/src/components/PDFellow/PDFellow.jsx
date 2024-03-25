2bebec33ed18213dcdb31782f6fa25bd10975f1d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 14:02:39 +0530

    Update PDFellow.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/PDFellow/PDFellow.jsx
+++ client/src/components/PDFellow/PDFellow.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const PDFellow = () => {
-  return (
-    <div>PDFellow</div>
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "PDFellow" || RoleUser === "SuperAdmin"){
+        return (
+            <div>PDFellow</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default PDFellow
