7a5bb99b228043c4f51f1960d26d6acd58f8a21e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 12:27:04 +0530

    Update NonAcademic.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/NonAcademic/NonAcademic.jsx
+++ client/src/components/NonAcademic/NonAcademic.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const NonAcademic = () => {
-  return (
-    <div>NonAcademic</div>
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "NonAcademic" || RoleUser === "SuperAdmin"){
+        return (
+            <div>NonAcademic</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default NonAcademic
