007c49f6b40c282b06069689f41f85f638da480a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 21:05:42 +0530

    Update Users.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Users/Users.jsx
+++ client/src/components/Users/Users.jsx
@@ -1,8 +1,15 @@
 import Navlist from "../NavBar/navList"
+import logo from "../../assets/nifs_logo.png"
+import { useNavigate } from "react-router-dom"
 
 const Users = () => {
+    const navigate = useNavigate()
+    const GoBack = () =>{
+      localStorage.clear()
+      navigate('/')
+    }
   return (
-    <div className="bg-black">
+    <div className="">
       <div className="bg-gray-200 h-screen w-full py-24 px-40">
         <div className="bg-white py-12 px-20 rounded shadow-xl border-l-4 border-blue-500">
         <div className="lg:grid grid-cols-2 gap-2">
@@ -33,18 +40,34 @@ const Users = () => {
                 <div className="my-2">
                   <button type="submit" className="py-2 px-8 rounded bg-blue-500 text-white">Request</button>
                 </div>
-
               </form>
+              <button onChange={() => GoBack()} className="py-2 px-4 bg-blue-500 rounded text-white">Go back for Now</button>
+
+              <div className="my-4">
+                <p className="text-xl text-red-500 font-semibold">Important Notice </p>
+                <p>The request will be rejected, if you try to access any other pages on this system.</p>
+              </div>
+
             </div>
             </div>
             <div className="">
-              <p>Welcome to </p>
-              <p>ERP System </p>
-              <p>National Institute of Fundamental Studies</p>
+              <div className="my-8">
+                <img src={logo} alt="" />
+                <p className="text-3xl my-4">Welcome to </p>
+                <p className="text-3xl my-4">ERP System </p>
+                <p className="text-3xl my-4">National Institute of Fundamental Studies</p>
+              </div>
             </div>
           </div>
         </div>
+        <div className="">
+        <div className='shadow-xl border-l-4 bg-white my-4 rounded py-12 px-6 text-center'>
+            <p>Copyright &copy; 2024. All rights reserved.</p>
+            <p className='py-4'>Developed and Designed by: The National Institute of Fundamental Studies</p>
+        </div>
+        </div>
       </div>
+      
     </div>
   )
 }
