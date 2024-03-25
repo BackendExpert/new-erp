e6142991efb86fe955001dbcfcf2ba1dbf3008d7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-25 12:11:57 +0530

    Update ScientistsDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Scientist/ScientistsDash.jsx
+++ client/src/components/Scientist/ScientistsDash.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
+import ScientistSide from './ScientistSide'
+import OtherUserNav from '../NavBar/OtherUserNav'
+import SummaryDash from '../SummaryDash/SummaryDash'
+import DashFooter from '../SummaryDash/dashFooter'
 
 const ScientistsDash = () => {
   const navigate = useNavigate()
@@ -8,7 +12,20 @@ const ScientistsDash = () => {
 
   if(RoleUser === "Scientist" || RoleUser === "SuperAdmin"){
     return (
-      <div>ScientistsDash</div>
+      <div className="bg-gray-200">
+        <div className="flex">
+            <ScientistSide />
+            <div className="w-full mx-2">
+              <OtherUserNav />
+              <div className="shadow-xl border-l-4 bg-white my-4 rounded py-4 px-6">
+                  <h1 className="text-2xl">Welcome to RA Dashboard</h1>
+                  <hr className="mt-2 border-blue-100 border-2" />
+                  <SummaryDash />
+              </div>
+                <DashFooter />
+            </div>
+        </div>
+    </div>
     )
   }
   else{
