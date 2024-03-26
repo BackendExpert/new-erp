a874104a4ec44866544a506d4c9d23eccdbc1987
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-26 12:12:39 +0530

    Update UserRoleRequest.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RequestRole/UserRoleRequest.jsx
+++ client/src/components/RequestRole/UserRoleRequest.jsx
@@ -1,6 +1,12 @@
+import axios from 'axios'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const UserRoleRequest = () => {
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
   return (
     <div>UserRoleRequest</div>
   )
