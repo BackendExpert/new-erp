f3fae43fde69f0d4f4f738da651f9ddb2d122d39
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-26 12:15:37 +0530

    Update UserRoleRequest.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/RequestRole/UserRoleRequest.jsx
+++ client/src/components/RequestRole/UserRoleRequest.jsx
@@ -7,9 +7,30 @@ const UserRoleRequest = () => {
     const navigate = useNavigate()
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
+    const headleBack = () => {
+        if(RoleUser === "SuperAdmin"){
+            navigate('/superAdmin')
+        }
+        else if(RoleUser === "Admin"){
+            navigate('/admin')
+        }
+    }
+
     if(RoleUser === "SuperAdmin" || RoleUser === "Admin" ){
         return (
-            <div>UserRoleRequest</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">All Programs</h1>
+                    <hr className="mb-4" />
+                    <div className="lg:flex">
+                        <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        <Link to={'/AddProgram'}>
+                            <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Program</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>    
+
         )
     }
     else{
