b6c15f1e6047dd8a0d5e11d096d1cc8e76f34016
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-28 20:53:38 +0530

    Update PendingUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/PendingUsers.jsx
+++ client/src/components/SuperAdmin/PendingUsers.jsx
@@ -33,7 +33,13 @@ const PendingUsers = () => {
                                 viewPendingUsers.map((pendingUser, index) => {
                                     return(
                                         <tr key={index}>
-                                            
+                                            <td className='px-6 py-4'>{pendingUser.email}</td>
+                                            <td className='px-6 py-4'>{pendingUser.role}</td>
+                                            <td className='px-6 py-4'>
+                                                <Link to={'/UserViewPending/' + pendingUser.email}>
+                                                    <button className="rounded border border-blue-500 text-blue-500 font-semibold  mx-2 py-2 px-8 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">View User</button>
+                                                </Link>                                                
+                                            </td>
                                         </tr>
                                     )
                                 })
