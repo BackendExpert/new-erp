41ba849194ca985d6828e4b7e87cf7e861cf6285
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 19:14:27 +0530

    Create Mystats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ client/src/components/SummaryDash/Mystats.jsx
@@ -0,0 +1,12 @@
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
+const Mystats = () => {
+  return (
+    <div>Mystats</div>
+  )
+}
+
+export default Mystats
