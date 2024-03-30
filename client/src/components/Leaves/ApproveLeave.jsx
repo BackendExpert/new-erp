d22e22d92c3ca0ee26a475205bad6a03a4ee38cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 16:17:50 +0530

    Update ApproveLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/ApproveLeave.jsx
+++ client/src/components/Leaves/ApproveLeave.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 const ApproveLeave = () => {
-  return (
-    <div>ApproveLeave</div>
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "Director" || RoleUser === "Secretary"){
+        return (
+            <div>ApproveLeave</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default ApproveLeave
