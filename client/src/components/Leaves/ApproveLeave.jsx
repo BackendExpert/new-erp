15bc787b322dccbb2aebdec1df2b09aee6e97137
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 16:27:13 +0530

    Update ApproveLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/ApproveLeave.jsx
+++ client/src/components/Leaves/ApproveLeave.jsx
@@ -19,13 +19,18 @@ const ApproveLeave = () => {
     }
 
     const [leaveApprove, SetLaeaveApprove] = useState([])
-    
+
     //fetch data
     useEffect(() => {
         axios.get('http://localhost:8081/LeavesToApprove')
         .then(res => SetLaeaveApprove(res.data))
         .catch(err => console.log(err))
     }, [])
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
 
     if(RoleUser === "Director" || RoleUser === "Secretary"){
         return (
@@ -36,6 +41,12 @@ const ApproveLeave = () => {
                     <div className="lg:flex">                
                         <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                     </div>
+                    <div className="flex pl-2 my-4">
+                        <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Recommend Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Reject')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Reject Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Approve')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Approve Leaves</button>
+                    </div>
+                    <p className="">{buttonValue}</p>
                     <div className="relative overflow-x-auto my-8">
                             <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
