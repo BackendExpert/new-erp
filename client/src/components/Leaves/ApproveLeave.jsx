538535a2b3aa44abae5a5c6c9884efddd0c8b8b3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 16:20:34 +0530

    Update ApproveLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/ApproveLeave.jsx
+++ client/src/components/Leaves/ApproveLeave.jsx
@@ -22,11 +22,12 @@ const ApproveLeave = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Leaves</h1>        
+                    <h1 className="text-xl font-semibold">Leaves for Approve</h1>        
                     <hr className="mb-4" />
                     <div className="lg:flex">                
                         <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                     </div>
+                    
                 </div>
             </div>
         )
