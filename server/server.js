d65c166349d137e053251dc292784a41c2af259b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 20:11:59 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2124,7 +2124,7 @@ app.post('/RejectLeave/:id', (req, res) => {
 app.get('/CountMyLeavs/:id', (req, res) => {
     const CurrentEmail = req.params.id
     console.log(CurrentEmail)
-    const sql = "SELECT COUNT(LID) AS le FROM leaves";
+    const sql = "SELECT COUNT(LID) AS le FROM leaves WHERE ";
     // const sql = "SELECT COUNT(eid) AS emp FROM employee";
     
     connection.query(sql, (error, results) => {
