bafb865643e84bd3f3b52a7b20d552f585062a01
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 19:31:08 +0530

    Update Mystats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/Mystats.jsx
+++ client/src/components/SummaryDash/Mystats.jsx
@@ -7,6 +7,15 @@ const Mystats = () => {
     const navigate = useNavigate()
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
+    const StatsData = [
+        {name: "My Leaves", value: "", icon: "", style: ""},
+        {name: "My Reservations", value: "", icon: "", style: ""},
+        {name: "My SRN", value: "", icon: "", style: ""},
+        {name: "My Work Requests", value: "", icon: "", style: ""},        
+        {name: "My Gate Pass", value: "", icon: "", style: ""},  
+        {name: "My Increment Requests", value: "", icon: "", style: ""},  
+    ]
+
   return (
     <div>
         <div className="my-4 mx-4">
@@ -16,7 +25,15 @@ const Mystats = () => {
             <div className="">
                 <div className="lg:grid grid-cols-3 gap-2">
                     <div className="">
-                        
+                        {
+                            StatsData.map((stats) => {
+                                return (
+                                    <div className="">
+                                        {stats.name}
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
