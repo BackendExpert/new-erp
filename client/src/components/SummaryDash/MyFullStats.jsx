06ccd43109619c529a771c428f1192e7adf76b50
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 20:29:12 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -1,19 +1,23 @@
 import secureLocalStorage from "react-secure-storage"
 import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import axios from "axios";
 import Icons from '@reacticons/ionicons'
 import CountUp from 'react-countup'
 
 const MyFullStats = () => {
     const navigate = useNavigate()
+    const {Currentemail} = useParams()
     const RoleUser = secureLocalStorage.getItem("loginNew");
 
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+    
   return (
-    <div>MyFullStats</div>
+    <div>
+        <p>{Currentemail}</p>
+    </div>
   )
 }
 
