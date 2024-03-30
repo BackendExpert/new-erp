4f7d10979c60913a25874ce3602bf502b2c30a19
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-30 19:46:27 +0530

    Update Mystats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/Mystats.jsx
+++ client/src/components/SummaryDash/Mystats.jsx
@@ -23,9 +23,14 @@ const Mystats = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-
+            try {
+                const ArticleCount = await axios.get('http://localhost:8081/CountArticles');
+                setArticle(ArticleCount.data.art);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
-        
+
         fetchData();
     }, [])
 
