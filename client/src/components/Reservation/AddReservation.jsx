264d0ffec0bb1c62653fb6012c2f5ca84584c43b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 20:29:40 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -49,7 +49,13 @@ const AddReservation = () => {
         } 
       }
   return (
-    <div>AddReservation</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">New Leave Request</h1>        
+            <hr className="mb-4" />
+            <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+        </div>
+    </div>
   )
 }
 
