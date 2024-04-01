f8a12a4a607d5fff7539c69f100bec061f9ccf87
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 15:24:16 +0530

    Update DriverSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverSide.jsx
+++ client/src/components/Driver/DriverSide.jsx
@@ -3,6 +3,20 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 
 const DriverSide = () => {
+    const [openSide, SetSideOpen] = useState();
+    const navigate = useNavigate();
+
+    const sidemenu = [
+        {name: "Leave",  link: "#", icon: <Icons name="calendar" size="large"></Icons>},
+        {name: "Reservations",  link: "#", icon: <Icons name="document-text" size="large"></Icons>},
+        {name: "Work Requests",  link: "#", icon: <Icons name="receipt" size="large"></Icons>},
+        {name: "SRN Requests",  link: "#", icon: <Icons name="book" size="large"></Icons>},
+        {name: "Gatepass Requests",  link: "#", icon: <Icons name="enter" size="large"></Icons>},
+        {name: "Increment Requests",  link: "#", icon: <Icons name="cash" size="large"></Icons>},
+        {name: "Profile",  link: "/Profile", icon: <Icons name="person" size="large"></Icons>},
+        {name: "Logout", desc: "logout", icon: <Icons name="power" size="large"></Icons>}
+    ]
+    
   return (
     <div>DriverSide</div>
   )
