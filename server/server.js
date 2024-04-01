064f479b38fdd55350f006ff506146531dacfb2e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 12:01:07 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2150,12 +2150,12 @@ app.get('/CountMyLeavs/:id', (req, res) => {
 })
 
 // UserViewLeaves
-app.get('/UserViewLeaves/:id', (req, res) => {
-    const UserEmail = req.body.id
-    const sql = "SELECT * FROM leaves WHERE Status = ? WHERE = ?"
+app.get('/UserViewLeaves', (req, res) => {
+    // const UserEmail = req.body.id
+    const sql = "SELECT * FROM leaves"
     const status = "Requested"
 
-    connection.query(sql, [status, userEmail], (err, result) => {
+    connection.query(sql, (err, result) => {
         if(err){
             return res.json({Error: "ERROR on SERVER"})
         }
