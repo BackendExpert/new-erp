38550f11d6a659dbd84f58cd84dfa34dfaec6544
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 15:15:50 +0530

    Update DriverDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverDash.jsx
+++ client/src/components/Driver/DriverDash.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import SummaryDash from "../SummaryDash/SummaryDash"
+import { useEffect } from "react"
+import  secureLocalStorage  from  "react-secure-storage";
+import DashFooter from "../SummaryDash/dashFooter"
+import { useNavigate } from "react-router-dom";
 
 const DriverDash = () => {
-  return (
-    <div>DriverDash</div>
-  )
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    if(RoleUser === "Driver" ||RoleUser === "TO" || RoleUser === "SuperAdmin"){
+        return (
+            <div>DriverDash</div>
+        )
+    }
+
 }
 
 export default DriverDash
