9d918a4b8b26f88b050b743a7d57f1aba9ba5e94
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 11:07:51 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -75,13 +75,13 @@ const MyFullStats = () => {
                     <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                     <div className="flex pl-2 my-4">
                         <div onClick={() => HeadleButtonClick('Requested')} className="lg:ml-4 rounded py-4 px-8 bg-yellow-500 text-white font-semibold cursor-pointer duration-500 hover:shadow-xl">
-                          Request Leaves
+                          My Request Leaves
                         </div>
                         <div onClick={() => HeadleButtonClick('Reject')} className="lg:ml-4 rounded py-4 px-8 bg-red-500 text-white font-semibold cursor-pointer duration-500 hover:shadow-xl">
-                          Request Reject
+                          My Reject Reject
                         </div>
                         <div onClick={() => HeadleButtonClick('Approve')} className="lg:ml-4 rounded py-4 px-8 bg-green-500 text-white font-semibold cursor-pointer duration-500 hover:shadow-xl">
-                          Request Approve
+                          My Approve Approve
                         </div>                        
                     </div>
                     <p>{buttonValue} Leave</p>
