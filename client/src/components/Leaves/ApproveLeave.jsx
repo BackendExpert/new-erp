2ae11ee52ec6979f31cd0645fc0d6a1f80c7c38f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 10:44:27 +0530

    Update ApproveLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/ApproveLeave.jsx
+++ client/src/components/Leaves/ApproveLeave.jsx
@@ -80,10 +80,10 @@ const ApproveLeave = () => {
                     <div className="lg:flex">                
                         <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                     </div>
-                    <div className="flex pl-2 my-4">
-                        <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Recommend Leaves</button>
-                        <button onClick={() => HeadleButtonClick('Reject')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Reject Leaves</button>
-                        <button onClick={() => HeadleButtonClick('Approve')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Approve Leaves</button>
+                    <div className="lg:flex pl-2 my-4">
+                        <button onClick={() => HeadleButtonClick('Recommend')} className="my-2 ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Recommend Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Reject')} className="my-2 ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Reject Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Approve')} className="my-2 ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Approve Leaves</button>
                     </div>
                     <p className="">{buttonValue}</p>
                     <div className="relative overflow-x-auto my-8">
