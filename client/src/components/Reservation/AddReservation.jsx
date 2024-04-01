561cbc16caf742215bb61cd0220e3aea33c28e8a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 20:29:09 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -10,7 +10,44 @@ const AddReservation = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
-    
+    const headleBack = () => {
+        if(RoleUser === "SuperAdmin"){
+          navigate('/superAdmin');
+        }
+        else if(RoleUser === "Admin"){
+          navigate('/admin');
+        }
+        else if(RoleUser === "HOD"){
+          navigate('/hod');
+        }      
+        else if(RoleUser === "TO"){
+          navigate('/to');
+        }  
+        else if(RoleUser === "Librarian"){
+          navigate('/librarian');
+        } 
+        else if(RoleUser === "Labmanager"){
+          navigate('/labManager');
+        }     
+        else if(RoleUser === "Accountant"){
+          navigate('/accountant');
+        } 
+        else if(RoleUser === "User"){
+          navigate('/user');
+        } 
+        else if(RoleUser === "RA"){
+          navigate('/RADash');
+        } 
+        else if(RoleUser === "Scientist"){
+          navigate('/ScientistsDash');
+        } 
+        else if(RoleUser === "NonAcademic"){
+          navigate('/NonAcademic');
+        } 
+        else if(RoleUser === "PDFellow"){
+          navigate('/PDFellow');
+        } 
+      }
   return (
     <div>AddReservation</div>
   )
