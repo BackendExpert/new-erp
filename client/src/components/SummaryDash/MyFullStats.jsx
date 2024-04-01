82fd6a64f85fb4ffe46e5f194d17b0cbb9cee852
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 12:08:26 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -98,9 +98,7 @@ const MyFullStats = () => {
                             <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
                                     <tr className='text-blue-500'>
-                                        <th scope='col' className='px-6 py-3'>Leave ID</th>
-                                        <th scope='col' className='px-6 py-3'>Name</th>
-                                        <th scope='col' className='px-6 py-3'>Email</th>                            
+                                        <th scope='col' className='px-6 py-3'>Leave ID</th>                      
                                         <th scope='col' className='px-6 py-3'>Leave Type</th>
                                         <th scope='col' className='px-6 py-3'>Category</th>
                                         <th scope='col' className='px-6 py-3'>Starting Date</th>                                        
@@ -116,7 +114,9 @@ const MyFullStats = () => {
                                       viewLeaves.map((leaveUser, index) => {
                                         if(leaveUser.Email === EmailUser){
                                           return(
-                                            <td className='px-6 py-4 font-bold'>{leave.LID}</td>
+                                            <tr key={index}>
+                                              <td className='px-6 py-4 font-bold'>{leaveUser.LID}</td>
+                                            </tr>
                                           )
                                         }
                                         else{
