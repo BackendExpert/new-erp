e250c29d1ed98bc14d9f6621061702dae7a90307
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 09:54:09 +0530

    Update Mystats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/Mystats.jsx
+++ client/src/components/SummaryDash/Mystats.jsx
@@ -30,12 +30,12 @@ const Mystats = () => {
         }, [])
 
     const StatsData = [
-        {name: "My Leaves", value: <CountUp end={myLeaves}/>, icon: <Icons name="log-out" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},
+        {name: "My Leaves", value: <CountUp end={myLeaves}/>, icon: <Icons name="log-out" size="large"></Icons>, style: "hover:border-red-500 hover:text-red-600"},
         {name: "My Reservations", value: "2", icon: <Icons name="car" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},
-        {name: "My SRN", value: "2", icon: <Icons name="document-text" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},
-        {name: "My Work Requests", value: "2", icon: <Icons name="documents" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},        
+        {name: "My SRN", value: "2", icon: <Icons name="document-text" size="large"></Icons>, style: "hover:border-green-500 hover:text-green-600"},
+        {name: "My Work Requests", value: "2", icon: <Icons name="documents" size="large"></Icons>, style: "hover:border-purple-500 hover:text-purple-600"},        
         {name: "My Gate Pass", value: "2", icon: <Icons name="map" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},  
-        {name: "My Increment Requests", value: "2", icon: <Icons name="cash" size="large"></Icons>, style: "hover:border-yellow-500 hover:text-yellow-600"},  
+        {name: "My Increment Requests", value: "2", icon: <Icons name="cash" size="large"></Icons>, style: "hover:border-green-500 hover:text-green-600"},  
     ]
 
 
