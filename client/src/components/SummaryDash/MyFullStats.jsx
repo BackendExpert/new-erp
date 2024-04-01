cee09f47c32830f8229e5c51a2ef6d60ba62cace
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 13:08:07 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -81,6 +81,9 @@ const MyFullStats = () => {
                     <h1 className="text-xl font-semibold">My All Stats</h1>        
                     <hr className="mb-4" />
                     <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    <div className="text-xl mt-6 my-2 font-semibold">
+                      My Leaves
+                    </div>
                     <div className="lg:flex pl-2 my-4 my-2">
                         <div onClick={() => HeadleButtonClick('Requested')} className="lg:ml-4 rounded py-4 px-8 bg-yellow-500 text-white font-semibold cursor-pointer duration-500 hover:shadow-xl">
                           My Request Leaves
