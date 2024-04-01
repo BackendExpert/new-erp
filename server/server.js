94f5f3fdb30307f84a02b597243d76237b1aee75
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-01 19:55:53 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2265,5 +2265,23 @@ app.get('/CountMyRejLeave/:id', (req, res) => {
     });
 })
 
+// CountAppLeave
+app.get('/CountAppLeave/:id', (req, res) => {
+    const UserEmail = req.params.id;
+    const sql = "SELECT COUNT(LID) AS MyRejLeave FROM leaves WHERE Status = ? && Email = ?";
+    // const sql = "SELECT COUNT(eid) AS emp FROM employee";
+    const status = "Reject"
+
+    connection.query(sql, [status, UserEmail], (error, results) => {
+      if (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send({ message: 'Error fetching data' });
+        return;
+      }
+  
+      res.json({ MyRejLeave: results[0].MyRejLeave }); // Send count in JSON format
+    });
+})
+
 //check the server is working
 app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
