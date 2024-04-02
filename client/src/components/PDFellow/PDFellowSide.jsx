a40c8ed1e93e1dadb09d4e00a0a254eea303818c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-02 19:05:04 +0530

    Update PDFellowSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/PDFellow/PDFellowSide.jsx
+++ client/src/components/PDFellow/PDFellowSide.jsx
@@ -7,7 +7,7 @@ const PDFellowSide = () => {
 
   const sidemenu = [
       {name: "Leave Requests",  link: "/AddLeave", icon: <Icons name="log-out" size="large"></Icons>},
-      {name: "Vehicle Reservations",  link: "", icon: <Icons name="car" size="large"></Icons>},
+      {name: "Vehicle Reservations",  link: "/AddReservation", icon: <Icons name="car" size="large"></Icons>},
       {name: "SRN Requests",  link: "#", icon: <Icons name="newspaper" size="large"></Icons>},
       {name: "Work Requests",  link: "#", icon: <Icons name="document-text" size="large"></Icons>},
       {name: "Gatepass Requests",  link: "#", icon: <Icons name="car" size="large"></Icons>},
