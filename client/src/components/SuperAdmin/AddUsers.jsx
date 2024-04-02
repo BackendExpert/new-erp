1596248b4f0e823f03cb6ee53ed9a55b231ab4f8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-02 11:16:47 +0530

    Update AddUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/AddUsers.jsx
+++ client/src/components/SuperAdmin/AddUsers.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import axios from "axios";
 
 const AddUsers = () => {
+    const navigate = useNavigate()
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+
+    
   return (
     <div>AddUsers</div>
   )
