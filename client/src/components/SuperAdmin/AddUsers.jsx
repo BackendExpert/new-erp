9ca4be7f4c459b5e9a244af99b11f25826fee114
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-02 11:53:57 +0530

    Update AddUsers.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SuperAdmin/AddUsers.jsx
+++ client/src/components/SuperAdmin/AddUsers.jsx
@@ -9,7 +9,23 @@ const AddUsers = () => {
 
     if(RoleUser === "SuperAdmin"){
         return (
-            <div>AddUsers</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">New SuperAdmin/Admin</h1>        
+                    <hr className="mb-4" />
+                    <Link to={'/SuperAdmin'}>
+                        <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    </Link>
+
+                    <div className="my-4">
+                        <form >
+                            <div className="lg:grid grid-cols-2 gap-4">
+                                
+                            </div>
+                        </form>
+                    </div>
+                </div>                
+            </div>
         )
     }
     else{
