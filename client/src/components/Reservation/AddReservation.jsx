1ae2984bbdc02762b22cdef7bc2f0b2984a4ce31
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-02 18:43:01 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -48,6 +48,12 @@ const AddReservation = () => {
           navigate('/PDFellow');
         } 
       }
+
+      //send data to backend
+
+      const headleSubmit = (e) => {
+        
+      }
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
@@ -56,7 +62,7 @@ const AddReservation = () => {
             <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
 
             <div className="my-4">
-                <form>
+                <form onSubmit={headleSubmit}>
                     <div className="lg:grid grid-cols-3 gap-4">
                         <div className="my-2">
                             <label htmlFor="">Employee Name : </label>
