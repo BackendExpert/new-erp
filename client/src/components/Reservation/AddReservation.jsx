b47505727665a5e62bf5acace6bd42cd95a23335
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-03 19:49:41 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -69,7 +69,7 @@ const AddReservation = () => {
 
       const headleSubmit = (e) => {
         e.preventDefault(); 
-        axios.post('http://localhost:8081/AddReservation', AddRese)
+        axios.post('http://localhost:8081/AddReservation/' + EmailUser, AddRese)
         .then(res => {
             if(res.data.Status === "Success"){
                 alert("Reservation Added Successful")
