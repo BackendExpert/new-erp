81d277cfe18e8573be5afea5fd229e0eae8a38f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 10:44:35 +0530

    Update ViewEmployee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/ViewEmployee.jsx
+++ client/src/components/Employees/ViewEmployee.jsx
@@ -4,12 +4,11 @@ const ViewEmployee = () => {
   return (
     <div className="bg-gray-200 py-4">
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">All Employee</h1>
+            <h1 className="text-xl font-semibold">Employee Data</h1>
             <hr className="mb-4" />
             <div className="lg:flex">
-                <button onClick={headleBack} className="border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                <Link to={'/AddEmployee'}>
-                    <button className="lg:my-0 my-2 border border-green-500 py-3 px-16 rounded text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl lg:mx-2">Add New Employee</button>
+                <Link to={'/Employees'}>
+                    <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                 </Link>
             </div>
         </div>
