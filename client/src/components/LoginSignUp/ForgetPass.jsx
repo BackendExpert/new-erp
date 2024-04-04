fc31d963e380a57b71279bd6f3beff7516837a0a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 20:28:23 +0530

    Update ForgetPass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/ForgetPass.jsx
+++ client/src/components/LoginSignUp/ForgetPass.jsx
@@ -10,7 +10,14 @@ const ForgetPass = () => {
         <div className="bg-white my-2 lg:mx-40 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
             <h1 className="text-2xl font-semibold">Forget Password</h1>
             <div className="my-8 mx-12">
-                
+                <label htmlFor="">Email : </label>
+                <input type="email" name="" id="" required placeholder='Enter Your Email Address'/>
+
+                <p className="">The OTP (One Time Password) will send to above you Entered Email</p>
+
+                <div className="">
+                    <button type='submit' className='border broder-red-500 rounded py-2 px-8 duration-500 hover:text-white hover:bg-red-500 hover:shadow-xl'>Request OTP</button>
+                </div>
             </div>
         </div>
     </div>
