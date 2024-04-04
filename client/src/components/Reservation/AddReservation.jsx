2c7cc44f95ff163595a1489aa10465b35879fa7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 15:06:54 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -112,7 +112,7 @@ const AddReservation = () => {
             <h1 className="text-xl font-semibold">New Reservation</h1>        
             <hr className="mb-4" />
             <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-            <p>asdasd {empName}</p>
+            <p>asdasd {empName.username}</p>
             <div className="my-4">
                 <form onSubmit={headleSubmit}>
                     <div className="lg:grid grid-cols-3 gap-4">
