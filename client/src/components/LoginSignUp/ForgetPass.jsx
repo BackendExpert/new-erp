0c4b3e242c625a427c950ddca83b8b7df4a82346
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 20:24:53 +0530

    Update ForgetPass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/ForgetPass.jsx
+++ client/src/components/LoginSignUp/ForgetPass.jsx
@@ -6,8 +6,13 @@ const ForgetPass = () => {
     const navigate = useNavigate()
 
   return (
-    <div className='bg-gray-200'>
-        <div className=""></div>
+    <div className='bg-gray-200 py-4'>
+        <div className="bg-white my-2 lg:mx-40 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-2xl font-semibold">Forget Password</h1>
+            <div className="my-8 mx-12">
+                
+            </div>
+        </div>
     </div>
   )
 }
