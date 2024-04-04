682e351cd8d1ea92d64fc9d1d7f6b605ca9d52f3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 15:17:42 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2441,7 +2441,7 @@ app.get('/EmpName/:id', (req, res) => {
 app.post('/AddReservation/:id', (req, res) => {
     const UserEmail = req.params.id
     // console.log(UserEmail)
-    // console.log(req.body)
+    console.log(req.body)
 
     const checkhod = "SELECT * FROM users WHERE email = ?"
     connection.query(checkhod, [req.body.HoDEmail], (err, result) => {
