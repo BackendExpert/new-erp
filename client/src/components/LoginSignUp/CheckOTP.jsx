abbba9ee9d1b107bcf5dbdc021409e8916ba7bc1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 20:45:59 +0530

    Update CheckOTP.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/CheckOTP.jsx
+++ client/src/components/LoginSignUp/CheckOTP.jsx
@@ -5,6 +5,11 @@ import { useState } from 'react'
 const CheckOTP = () => {
     const navigate = useNavigate()
 
+    const [OTPCheck, SetOTPCheck] = useState({
+        otp: ''
+    })
+
+
   return (
     <div className='bg-gray-200 py-4'>
         <div className="bg-white my-2 lg:mx-40 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
@@ -13,11 +18,9 @@ const CheckOTP = () => {
             <form>
                 <div className="my-8 mx-12">
                     <label htmlFor="">Email : </label>
-                    <input type="email" name="" id="" className='w-full h-12 border border-blue-500 rounded pl-2 my-2' required placeholder='Enter Your Email Address'
-                    onChange={e => SetForgetPass({...ForgetPass, email:e.target.value})}/>
-
-                    <p className="text-red-500">The OTP (One Time Password) will send to above you Entered Email</p>
-
+                    <input type="number" name="" id="" className='w-full h-12 border border-blue-500 rounded pl-2 my-2' required placeholder='Enter OTP'
+                    onChange={e => SetOTPCheck({...OTPCheck, otp:e.target.value})}/>
+                    
                     <div className="my-2">
                     <button type="submit" className="rounded text-red-500 border border-red-500 py-2 px-16 my-2 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request OTP</button>
                     </div>
