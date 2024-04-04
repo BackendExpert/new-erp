3afc3f04769a1b6372af03212641e1a6f694ea15
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 18:31:56 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2500,6 +2500,22 @@ app.post('/AddReservation/:id', (req, res) => {
 })
 
 // ReseReqCount
+app.get('/ReseReqCount/:id', (req, res) => {
+    const UserEmail = req.params.id;
+    const sql = "SELECT COUNT(LID) AS MyAppLeave FROM leaves WHERE Status = ? && Email = ?";
+    // const sql = "SELECT COUNT(eid) AS emp FROM employee";
+    const status = "Approve"
+
+    connection.query(sql, [status, UserEmail], (error, results) => {
+      if (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send({ message: 'Error fetching data' });
+        return;
+      }
+  
+      res.json({ MyAppLeave: results[0].MyAppLeave }); // Send count in JSON format
+    });
+})
 
 
 // ---------------------------- Reservation END ---------------------
