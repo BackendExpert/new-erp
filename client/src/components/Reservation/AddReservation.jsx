cb77eda6184ff4b1710d898425660a211c55076f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 13:49:17 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -98,10 +98,12 @@ const AddReservation = () => {
       }, [])
 
       // employee name
-      const [empName, SetEmpname] = useState([])
+      const [empName, SetEmpname] = useState({
+        nameEmp: ''
+      })
       useEffect(() => {
         axios.get('http://localhost:8081/EmpName/' + EmailUser)
-        .then(res => SetEmpname(res.data))
+        .then(res => SetEmpname({...empName, nameEmp:res.data.Result[0].username}))
         .catch(err => console.log(err))
       }, [])
 
@@ -111,7 +113,7 @@ const AddReservation = () => {
             <h1 className="text-xl font-semibold">New Reservation</h1>        
             <hr className="mb-4" />
             <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-
+            <p>hi all {empName.email}</p>
             <div className="my-4">
                 <form onSubmit={headleSubmit}>
                     <div className="lg:grid grid-cols-3 gap-4">
