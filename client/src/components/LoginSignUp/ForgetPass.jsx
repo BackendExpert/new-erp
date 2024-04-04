dbbd55d574333c762ab7e9af301f81133e5fe5d9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-04 20:22:31 +0530

    Update ForgetPass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/ForgetPass.jsx
+++ client/src/components/LoginSignUp/ForgetPass.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 
 const ForgetPass = () => {
+    const navigate = useNavigate()
+
   return (
-    <div>ForgetPass</div>
+    <div className='bg-gray-200'>
+        <div className=""></div>
+    </div>
   )
 }
 
