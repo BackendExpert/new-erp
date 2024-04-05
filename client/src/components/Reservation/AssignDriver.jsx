e840cd269ce98279fe55a36876aff84fa3fe45c8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 11:09:42 +0530

    Update AssignDriver.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AssignDriver.jsx
+++ client/src/components/Reservation/AssignDriver.jsx
@@ -47,6 +47,11 @@ const AssignDriver = () => {
                                     </select>
                                 </div>
                             </div>
+                            <div className="">
+                                <button type="submit" className="rounded px-16 py-2 border border-green-500 text-green-500 font-semibold duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">
+                                    Add Employee
+                                </button>
+                            </div>
                         </form>
                     </div>
                 </div>
