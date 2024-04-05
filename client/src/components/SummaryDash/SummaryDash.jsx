da2d1b6c9c728f88a93e35b2bcee8a8bd3826c70
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 12:03:51 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/SummaryDash.jsx
+++ client/src/components/SummaryDash/SummaryDash.jsx
@@ -146,6 +146,13 @@ const SummaryDash = () => {
       } catch (error) {
         console.error('Error fetching data:', error);
       }
+
+      try {
+        const CountReceRese = await axios.get('http://localhost:8081/ReseReceCount');
+        SetRecommendRese(CountReceRese.data.RecRese);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };    
     
 
@@ -170,7 +177,7 @@ const SummaryDash = () => {
     {id: 15, name:"Accept Leaves" , value: <CountUp end={ApproveLeaves}/>, icons: <Icons name="log-out" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },
     {id: 16, name:"Recommend Leaves" , value: <CountUp end={RecLeaves}/>, icons: <Icons name="log-out" size="large"/>, style:"hover:border-purple-500 hover:text-purple-600" },
     {id: 17, name:"Request Reservation" , value: <CountUp end={ReqRese}/>, icons: <Icons name="car" size="large"/>, style:"hover:border-orange-500 hover:text-orange-600" },
-    {id: 18, name:"Recommend Reservation" , value: <CountUp end={ReqRese}/>, icons: <Icons name="car" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },
+    {id: 18, name:"Recommend Reservation" , value: <CountUp end={RecommendRese}/>, icons: <Icons name="car" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },
   ]
 
     const RoleUser = secureLocalStorage.getItem("loginNew");
