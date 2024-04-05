cc6f4440086ec79cfe5a07698ebb8804c4fe2df4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 09:38:02 +0530

    Update RecReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/RecReservation.jsx
+++ client/src/components/Reservation/RecReservation.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
 
 const RecReservation = () => {
+    const navigate = useNavigate()
   return (
     <div>RecReservation</div>
   )
