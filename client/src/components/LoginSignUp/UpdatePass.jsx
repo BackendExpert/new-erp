fd8b6122bd51e513097327971678c9fee2711271
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 07:42:01 +0530

    Update UpdatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/UpdatePass.jsx
+++ client/src/components/LoginSignUp/UpdatePass.jsx
@@ -5,7 +5,8 @@ import  secureLocalStorage  from  "react-secure-storage"
 
 const UpdatePass = () => {
     //Stroge the token in local stroge
-    const loingToken = res.data.token
+    const PassToken = res.data.token
+    localStorage.setItem('Token2', PassToken)
 
     console.log(loingToken)
   return (
