d89d257bfbd52b9150d731f5fda9ddcfbff81deb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 10:58:57 +0530

    Update AssignDriver.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AssignDriver.jsx
+++ client/src/components/Reservation/AssignDriver.jsx
@@ -12,7 +12,15 @@ const AssignDriver = () => {
 
     if(RoleUser === "SuperAdmin" || RoleUser === "TO" || RoleUser === "Director" || RoleUser === "Secretary"){
         return (
-            <div>AssignDriver</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Assign a Driver</h1>        
+                    <hr className="mb-4" />
+                    <Link to={}>
+                        <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    </Link>
+                </div>
+            </div>
         )
     }
     else{
