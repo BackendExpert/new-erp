9eb55227f8a8818e25dc6a1606939f0b1f3465e9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 09:48:57 +0530

    Update RecReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/RecReservation.jsx
+++ client/src/components/Reservation/RecReservation.jsx
@@ -48,7 +48,7 @@ const RecReservation = () => {
                     <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                       <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
                         <tr className='text-blue-500'>
-                          <th scope='col' className='px-6 py-3'>Leave ID</th>
+                          <th scope='col' className='px-6 py-3'>Reservation ID</th>
                           <th scope='col' className='px-6 py-3'>Name</th>
                           <th scope='col' className='px-6 py-3'>Email</th>                            
                           <th scope='col' className='px-6 py-3'>Leave Type</th>
