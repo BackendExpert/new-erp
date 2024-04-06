0c0407c5a6e7657c02c0b1f9b0eb157aa207b487
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-06 08:54:53 +0530

    Update UpdatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/UpdatePass.jsx
+++ client/src/components/LoginSignUp/UpdatePass.jsx
@@ -15,7 +15,30 @@ const UpdatePass = () => {
 
    
     return (
-        <div>UpdatePass</div>
+        <div className='bg-gray-200 py-4'>
+            <div className="bg-white my-2 lg:mx-40 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                <h1 className="text-2xl font-semibold">Update Password</h1>
+
+                <form>
+                    <div className="my-8 mx-12">
+                        <label htmlFor="">Enter OTP : </label>
+                        <input type="number" name="" id="" className='w-full h-12 border border-blue-500 rounded pl-2 my-2' required placeholder='Enter OTP'
+                        />
+                        <label htmlFor="">Enter OTP : </label>
+                        <input type="number" name="" id="" className='w-full h-12 border border-blue-500 rounded pl-2 my-2' required placeholder='Enter OTP'
+                        />
+                        <label htmlFor="">Enter OTP : </label>
+                        <input type="number" name="" id="" className='w-full h-12 border border-blue-500 rounded pl-2 my-2' required placeholder='Enter OTP'
+                        />
+                        
+                        <div className="my-2">
+                        <button type="submit" className="rounded text-green-500 border border-green-500 py-2 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request OTP</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     )
 
 
