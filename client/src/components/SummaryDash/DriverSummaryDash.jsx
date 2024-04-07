e79d28d9394f6347e238c00d3d0a66aabad3f788
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-07 21:03:42 +0530

    Update DriverSummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/DriverSummaryDash.jsx
+++ client/src/components/SummaryDash/DriverSummaryDash.jsx
@@ -28,6 +28,9 @@ const DriverSummaryDash = () => {
         fetchData();
       }, []);
     
+      const DataList = [
+        {id: 1, name:"User Roles" , value: "2", icons: <Icons name="person" size="large"/>, style:"hover:border-green-200 hover:text-green-600"},
+      ]
 
     if(RoleUser === "Driver" || RoleUser === "TO"){
         return (
