598558e91d9661da788b575e481e74973cbf5804
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-07 21:00:29 +0530

    Update DriverSummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/DriverSummaryDash.jsx
+++ client/src/components/SummaryDash/DriverSummaryDash.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import secureLocalStorage from 'react-secure-storage'
+import CountUp from 'react-countup'
+import Icons from '@reacticons/ionicons'
 
 const DriverSummaryDash = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
   return (
     <div>DriverSummaryDash</div>
   )
