6f436a40febfec58bcf91c2a1a2c08f55b553a7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-07 20:57:39 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/SummaryDash.jsx
+++ client/src/components/SummaryDash/SummaryDash.jsx
@@ -266,6 +266,16 @@ const SummaryDash = () => {
                 ) 
               }
             }
+            // for Driver
+            if(RoleUser === "Driver"){
+              return (
+                <div class={`text-center text-gray-500 shadow-2xl py-12 cursor-pointer rounded duration-500 ${data.style}`}>
+                  <span className="text-3xl" >{data.icons}</span>
+                  <p className="text-xl py-2">{data.name}</p>
+                  <p className="text-2xl font-bold">{data.value}</p>
+                </div>  
+              )
+            }
         })
       }
         <br />  
