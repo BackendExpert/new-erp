ba670e3f69df506afcc7825cac6c060b122d0f1f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 11:37:39 +0530

    Update DriverKm.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverKm.jsx
+++ client/src/components/Driver/DriverKm.jsx
@@ -10,6 +10,11 @@ const DriverKm = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+    const [addDriveKm, SetaddDriveKm] = useState({
+        addKm: '',
+    })
+
+
     if(RoleUser === "Driver"){
         return (
             <div className="bg-gray-200 py-4">
@@ -18,8 +23,10 @@ const DriverKm = () => {
                 <hr className="mb-4" />
 
                 <form>
-                    <div className="">
-                        
+                    <div className="my-2">
+                        <label htmlFor="">Add Drive KM</label>
+                        <input type="number" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Drive KM"
+                        onChange={e => SetaddDriveKm({...addDriveKm, addKm:e.target.value})} />
                     </div>
                 </form>
             </div>
