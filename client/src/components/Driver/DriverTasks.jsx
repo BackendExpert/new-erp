a6b179a8a5619716dd1be035bbcdcf3a534de6b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 07:12:49 +0530

    Update DriverTasks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverTasks.jsx
+++ client/src/components/Driver/DriverTasks.jsx
@@ -24,7 +24,19 @@ const DriverTasks = () => {
         .then(res => SetNewDriveTrip(res.data))
         .catch(err => console.log(err))
     }, [])
-
+    
+    const headleGetDuty = (id) => {
+        axios.post('http://localhost:8081/GetDuty/' + id, EmailUser)
+        .then(res => {
+            if(res.data.Status === "Success"){
+                alert("Get Duty is Successful")
+                navigate('/DriverDash')
+            } 
+            else{
+                alert(res.data.Error)
+            }
+        })
+    }
 
     if(RoleUser === "Driver"){
         return (
@@ -71,7 +83,7 @@ const DriverTasks = () => {
                                                                     <span className="py-1 px-4 rounded bg-yellow-500 text-white">{trip.is_aprove}</span>
                                                                 </td>
                                                                 <td className='px-6 py-4 font-bold'>
-                                                                    <button onClick={() => headleApprove(trip.ID)} className="ml-2 py-2 px-4 rounded border border-green-500 text-green-500 cursor-pointer duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Get Duty</button>
+                                                                    <button onClick={() => headleGetDuty(trip.ID)} className="ml-2 py-2 px-4 rounded border border-green-500 text-green-500 cursor-pointer duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Get Duty</button>
                                                                 </td>
                                                             </tr>
                                                         )
