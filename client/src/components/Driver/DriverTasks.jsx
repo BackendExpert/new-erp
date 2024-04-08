0fb9046395ae4de33355315ccc4998af78e95d05
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 18:21:31 +0530

    Update DriverTasks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverTasks.jsx
+++ client/src/components/Driver/DriverTasks.jsx
@@ -133,7 +133,7 @@ const DriverTasks = () => {
                                                                     <span className="py-2 px-4 rounded bg-yellow-500 text-white">{onDuty.is_aprove}</span>
                                                                 </td>
                                                                 <td className='px-6 py-4 font-bold'>
-                                                                    <button onClick={() => headleGetOffDuty(trip.ID)} className="ml-2 py-2 px-4 rounded border border-red-500 text-red-500 cursor-pointer duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">End Duty</button>
+                                                                    <button onClick={() => headleGetOffDuty(onDuty.ID)} className="ml-2 py-2 px-4 rounded border border-red-500 text-red-500 cursor-pointer duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">End Duty</button>
                                                                 </td>
                                                             </tr>
                                                         )
