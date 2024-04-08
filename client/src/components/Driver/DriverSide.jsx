bb3dd62db1ed5749091e1ce15d36300253749c49
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 06:29:10 +0530

    Update DriverSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverSide.jsx
+++ client/src/components/Driver/DriverSide.jsx
@@ -7,7 +7,7 @@ const DriverSide = () => {
     const navigate = useNavigate();
 
     const sidemenu = [
-        {name: "My Tasks",  link: "/AddLeave", icon: <Icons name="document-text" size="large"></Icons>},
+        {name: "My Tasks",  link: "/DriverTasks", icon: <Icons name="document-text" size="large"></Icons>},
         {name: "Leave",  link: "/AddLeave", icon: <Icons name="calendar" size="large"></Icons>},
         {name: "Reservations",  link: "/AddReservation", icon: <Icons name="document-text" size="large"></Icons>},
         {name: "Work Requests",  link: "#", icon: <Icons name="receipt" size="large"></Icons>},
