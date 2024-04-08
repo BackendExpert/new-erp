da3fccca02bb13d2f9c49942f5a653471e31e8df
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 10:38:12 +0530

    Update ReservationCost.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/ReservationCost.jsx
+++ client/src/components/Reservation/ReservationCost.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 const ReservationCost = () => {
-  return (
-    <div>ReservationCost</div>
-  )
+  const navigate = useNavigate()
+  //check current login user
+  const RoleUser = secureLocalStorage.getItem("loginNew");
+  //get current login user's email
+  const EmailUser = secureLocalStorage.getItem("logiafter");
+
+  if(RoleUser === "TO" || RoleUser === "Director" || RoleUser === "Secretary"){
+    return (
+      <div>ReservationCost</div>
+    )
+  }
 }
 
 export default ReservationCost
