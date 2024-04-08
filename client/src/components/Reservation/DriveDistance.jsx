b3716e55579101ad1905d923a82731f3369c220f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 12:42:51 +0530

    Update DriveDistance.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/DriveDistance.jsx
+++ client/src/components/Reservation/DriveDistance.jsx
@@ -10,11 +10,16 @@ const DriveDistance = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+    const [DriveDistance, SetDriveDistance] = useState({
+        Distance: '',
+    })
+    
+
     if(RoleUser === "TO" || RoleUser === "Director" || RoleUser === "Secretary"){
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Calculate Cost</h1>        
+                    <h1 className="text-xl font-semibold">Drive Distance</h1>        
                     <hr className="mb-4" />
                     <Link to={'/RecReservation'}>
                         <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
@@ -22,9 +27,9 @@ const DriveDistance = () => {
                     <div className="my-4">
                         <form>
                             <div className="my-2">
-                                <label htmlFor="">Scientist 2</label>
-                                <input type="text" className="rounded w-full h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Scientist 2"
-                                onChange={e => SetProgramData({...programData, scient2:e.target.value})} />
+                                <label htmlFor="">Driver Distance : </label><br />
+                                <input type="number" className="rounded w-1/2 h-12 border border-blue-500 pl-2 my-2" required placeholder="Enter Driver Distance"
+                                onChange={e => SetDriveDistance({...DriveDistance, Distance:e.target.value})} />
                             </div>
                         </form>
                     </div>
