7dbd0f5f8610dd29a6caf3ecd3534de519a5a3cc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 06:40:42 +0530

    Update DriverTasks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverTasks.jsx
+++ client/src/components/Driver/DriverTasks.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import  secureLocalStorage  from  "react-secure-storage";
 
@@ -20,7 +20,7 @@ const DriverTasks = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Leaves for Approve</h1>        
+                    <h1 className="text-xl font-semibold">My Tasks</h1>        
                     <hr className="mb-4" />
                     <div className="lg:flex">                
                         <Link to={'/DriverDash'}>
