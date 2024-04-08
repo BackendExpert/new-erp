04bfe2d9c26ec8457c91f83676a04676f8546701
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 06:35:07 +0530

    Update DriverTasks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Driver/DriverTasks.jsx
+++ client/src/components/Driver/DriverTasks.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom";
+import  secureLocalStorage  from  "react-secure-storage";
 
 const DriverTasks = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    
   return (
     <div>DriverTasks</div>
   )
