2c65722e0cc6cd3b624ab9a5967c15a3fb4c7910
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-08 11:30:41 +0530

    Update DriverKm.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/DriverKm.jsx
+++ client/src/components/Reservation/DriverKm.jsx
@@ -12,7 +12,18 @@ const DriverKm = () => {
 
     if(RoleUser === "Driver"){
         return (
-            <div>DriverKm</div>
+            <div className="bg-gray-200 py-4">
+            <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                <h1 className="text-xl font-semibold">Add Drive KM</h1>        
+                <hr className="mb-4" />
+
+                <form>
+                    <div className="">
+                        
+                    </div>
+                </form>
+            </div>
+            </div>
         )
     }
     else{
