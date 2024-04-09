ccf76685da938c2f54d16fa39a553c7080c34ac4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-09 11:34:17 +0530

    Update HODRecLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/HODRecLeaves.jsx
+++ client/src/components/Leaves/HODRecLeaves.jsx
@@ -18,6 +18,11 @@ const HODRecLeaves = () => {
         .catch(err => console.log(err))
     }, [])
 
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
     if(RoleUser === "HOD"){
         return (
             <div className="bg-gray-200 py-4">
@@ -28,6 +33,14 @@ const HODRecLeaves = () => {
                         <Link to={'/AddLeave'}>
                             <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                         </Link>
+                    </div>
+                    <div className="flex pl-2 my-4">
+                        <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Denied')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
+                        <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
+                    </div>
+                    <div className="">
+                        {buttonValue}
                     </div>
                     <div className="relative overflow-x-auto my-8">
                             <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -47,7 +60,9 @@ const HODRecLeaves = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
+                                        {
 
+                                        }
                                 </tbody>
                             </table>
                         </div>
