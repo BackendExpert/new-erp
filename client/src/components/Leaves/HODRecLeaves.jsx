bcc12ca41ab09b2fdfa5ef445e13d32c414b7c55
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-09 11:20:31 +0530

    Update HODRecLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/HODRecLeaves.jsx
+++ client/src/components/Leaves/HODRecLeaves.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const HODRecLeaves = () => {
-  return (
-    <div>HODRecLeaves</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    if(RoleUser === "HOD"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Leaves for Approve</h1>        
+                    <hr className="mb-4" />
+                    <div className="flex">                
+                        <Link to={'/AddLeave'}>
+                            <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default HODRecLeaves
