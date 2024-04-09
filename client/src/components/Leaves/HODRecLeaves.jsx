a5f25efcfda59c3756af9c7b1cc10232bf764c9b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-09 11:21:48 +0530

    Update HODRecLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/HODRecLeaves.jsx
+++ client/src/components/Leaves/HODRecLeaves.jsx
@@ -21,6 +21,28 @@ const HODRecLeaves = () => {
                             <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                         </Link>
                     </div>
+                    <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                        <th scope='col' className='px-6 py-3'>Leave ID</th>
+                                        <th scope='col' className='px-6 py-3'>Name</th>
+                                        <th scope='col' className='px-6 py-3'>Email</th>                            
+                                        <th scope='col' className='px-6 py-3'>Leave Type</th>
+                                        <th scope='col' className='px-6 py-3'>Category</th>
+                                        <th scope='col' className='px-6 py-3'>Starting Date</th>                                        
+                                        <th scope='col' className='px-6 py-3'>Starting Time</th>
+                                        <th scope='col' className='px-6 py-3'>End Date</th>
+                                        <th scope='col' className='px-6 py-3'>No. of Days</th>
+                                        <th scope='col' className='px-6 py-3'>Status</th>
+                                        <th scope='col' className='px-6 py-3'>Action</th>                                        
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
                 </div>
             </div>
         )
