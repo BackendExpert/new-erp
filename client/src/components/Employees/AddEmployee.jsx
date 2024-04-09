0e753a38fbc04cfcb46dec215f0b32d67235bae9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-09 07:24:50 +0530

    Update AddEmployee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Employees/AddEmployee.jsx
+++ client/src/components/Employees/AddEmployee.jsx
@@ -109,9 +109,13 @@ const AddEmployee = () => {
                                     <select className="w-full h-12 border border-blue-400 rounded pl-2" required name="did"
                                     onChange={e => SetEmpData({...empData, dno:e.target.value})}>
                                         <option value="">Select Division</option>
-                                        <option value="1">Select Divission</option>
-                                        <option value="2">Select Divsision</option>
-                                        <option value="3">Select Divission</option>
+                                    {
+                                        divisionData.map((divi => {
+                                            return (
+                                                <option value={divi.did}>{divi.title}</option>
+                                            )
+                                        })) 
+                                    }
                                     </select>
 
                                 </div>
