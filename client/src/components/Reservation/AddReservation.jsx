8b16ad7a8226af2da1ae7931b49330ea5ad78349
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-10 15:40:26 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -123,7 +123,7 @@ const AddReservation = () => {
                   if(RoleUser === "HOD"){
                     return (
                       <Link to={'/HODRecRese'}>
-                        <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
+                        <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
                       </Link>
                     )
                   }
