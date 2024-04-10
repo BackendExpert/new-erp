dead4a65bc938f5c075569ef367afdf0714d0079
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-10 14:28:17 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -116,7 +116,21 @@ const AddReservation = () => {
         <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
             <h1 className="text-xl font-semibold">New Reservation</h1>        
             <hr className="mb-4" />
-            <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+            <div className="flex">
+              <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+              {
+                (() => {
+                  if(RoleUser === "HOD"){
+                    return (
+                      <Link to={'/HODRecRese'}>
+                        <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
+                      </Link>
+                    )
+                  }
+                })()
+              }
+            </div>
+            
             <div className="my-4">
                 <form onSubmit={headleSubmit}>
                     <div className="lg:grid grid-cols-3 gap-4">
