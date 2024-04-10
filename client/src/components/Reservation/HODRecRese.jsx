a5077f89957de06c41707ead08d1841af1c26cdd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-10 20:18:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/HODRecRese.jsx
+++ client/src/components/Reservation/HODRecRese.jsx
@@ -22,6 +22,11 @@ const HODRecRese = () => {
                 <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
              </Link>
           </div>
+          <div className="flex pl-2 my-4">
+            <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Reservations</button>
+            <button onClick={() => HeadleButtonClick('Denied')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
+            <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
+          </div>
       </div>
     </div>
   )
