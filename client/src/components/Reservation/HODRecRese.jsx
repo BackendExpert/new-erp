bef371d1f69b293a24e6f67250de9faaf0cd8c4d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 08:28:48 +0530

    Update HODRecRese.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/HODRecRese.jsx
+++ client/src/components/Reservation/HODRecRese.jsx
@@ -43,79 +43,86 @@ const HODRecRese = () => {
       })
     }
     
-
-  return (
-    <div className="bg-gray-200 py-4">
-      <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-          <h1 className="text-xl font-semibold">Recommended Reservations</h1>        
-          <hr className="mb-4" />
-          <div className="lg:flex">                
-             <Link to={'/AddReservation'}>
-                <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
-             </Link>
-          </div>
-          <div className="flex pl-2 my-4">
-            <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Reservations</button>
-            <button onClick={() => HeadleButtonClick('Denied')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
-            <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
-          </div>
-          <div className="">{buttonValue}</div>
-                <div className="relative overflow-x-auto my-8">
-                    <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                      <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
-                        <tr className='text-blue-500'>
-                          <th scope='col' className='px-6 py-3'>Reservation ID</th>
-                          <th scope='col' className='px-6 py-3'>Name</th>
-                          <th scope='col' className='px-6 py-3'>Designation</th>                            
-                          <th scope='col' className='px-6 py-3'>Email</th>
-                          <th scope='col' className='px-6 py-3'>Purpose</th>                                    
-                          <th scope='col' className='px-6 py-3'>Starting Time</th>
-                          <th scope='col' className='px-6 py-3'>Date</th>
-                          <th scope='col' className='px-6 py-3'>Arrival Date</th>
-                          <th scope='col' className='px-6 py-3'>Route</th>
-                          <th scope='col' className='px-6 py-3'>Status</th>
-                          <th scope='col' className='px-6 py-3'>Action</th>                                        
-                        </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        (() => {
-                          if(buttonValue === "Requested"){
-                            return (
-                              HodRece.map((ReseHOD, index) => {
+    if(RoleUser === "HOD"){
+      return (
+        <div className="bg-gray-200 py-4">
+          <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+              <h1 className="text-xl font-semibold">Recommended Reservations</h1>        
+              <hr className="mb-4" />
+              <div className="lg:flex">                
+                 <Link to={'/AddReservation'}>
+                    <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                 </Link>
+              </div>
+              <div className="flex pl-2 my-4">
+                <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Reservations</button>
+                <button onClick={() => HeadleButtonClick('Denied')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
+                <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
+              </div>
+              <div className="">{buttonValue}</div>
+                    <div className="relative overflow-x-auto my-8">
+                        <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                          <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                            <tr className='text-blue-500'>
+                              <th scope='col' className='px-6 py-3'>Reservation ID</th>
+                              <th scope='col' className='px-6 py-3'>Name</th>
+                              <th scope='col' className='px-6 py-3'>Designation</th>                            
+                              <th scope='col' className='px-6 py-3'>Email</th>
+                              <th scope='col' className='px-6 py-3'>Purpose</th>                                    
+                              <th scope='col' className='px-6 py-3'>Starting Time</th>
+                              <th scope='col' className='px-6 py-3'>Date</th>
+                              <th scope='col' className='px-6 py-3'>Arrival Date</th>
+                              <th scope='col' className='px-6 py-3'>Route</th>
+                              <th scope='col' className='px-6 py-3'>Status</th>
+                              <th scope='col' className='px-6 py-3'>Action</th>                                        
+                            </tr>
+                        </thead>
+                        <tbody>
+                          {
+                            (() => {
+                              if(buttonValue === "Requested"){
                                 return (
-                                    <tr key={index}>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.RID}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.Name}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.designation}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.Email}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.purpose}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.time}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.StartDate}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.EndDate}</td>
-                                        <td className='px-6 py-4 font-bold'>{ReseHOD.loc_route}</td>
-                                        <td className='px-6 py-4 font-bold'>
-                                          <span className="py-2 px-4 rounded bg-yellow-500 text-white">{ReseHOD.Status}</span>
-                                        </td> 
-                                        <td>
-                                          <div className="flex">
-                                            <button  onClick={() => ReseHOD(ReseHOD.RID)} className="ml-2 border border-green-500 rounded py-2 px-4 text-green-500 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Recommended</button>
-                                            <button  onClick={() => HodReject(ReseHOD.RID)} className="ml-2 border border-red-500 rounded py-2 px-4 text-red-500 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Denied</button>
-                                          </div>
-                                        </td>
-                                    </tr>
+                                  HodRece.map((ReseHOD, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.RID}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.Name}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.designation}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.Email}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.purpose}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.time}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.StartDate}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.EndDate}</td>
+                                            <td className='px-6 py-4 font-bold'>{ReseHOD.loc_route}</td>
+                                            <td className='px-6 py-4 font-bold'>
+                                              <span className="py-2 px-4 rounded bg-yellow-500 text-white">{ReseHOD.Status}</span>
+                                            </td> 
+                                            <td>
+                                              <div className="flex">
+                                                <button  onClick={() => ReseHOD(ReseHOD.RID)} className="ml-2 border border-green-500 rounded py-2 px-4 text-green-500 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Recommended</button>
+                                                <button  onClick={() => HodReject(ReseHOD.RID)} className="ml-2 border border-red-500 rounded py-2 px-4 text-red-500 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Denied</button>
+                                              </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
                                 )
-                            })
-                            )
+                              }
+                            })()
                           }
-                        })()
-                      }
-                    </tbody>
-                  </table>
-                </div>
-      </div>
-    </div>
-  )
+                        </tbody>
+                      </table>
+                    </div>
+          </div>
+        </div>
+      )
+    }
+    else{
+      useEffect(() => {
+        navigate('/UnAccess');
+      }, [])
+    }
+
 }
 
 export default HODRecRese
