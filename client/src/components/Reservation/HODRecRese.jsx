1fa3bcc6bdd6f2d90030c7ce441dffa250164546
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 06:40:24 +0530

    Update HODRecRese.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/HODRecRese.jsx
+++ client/src/components/Reservation/HODRecRese.jsx
@@ -40,15 +40,13 @@ const HODRecRese = () => {
                           <th scope='col' className='px-6 py-3'>Name</th>
                           <th scope='col' className='px-6 py-3'>Designation</th>                            
                           <th scope='col' className='px-6 py-3'>Email</th>
-                          <th scope='col' className='px-6 py-3'>Purpose</th>
-                          <th scope='col' className='px-6 py-3'>Division No</th>                                        
+                          <th scope='col' className='px-6 py-3'>Purpose</th>                                    
                           <th scope='col' className='px-6 py-3'>Starting Time</th>
                           <th scope='col' className='px-6 py-3'>Date</th>
                           <th scope='col' className='px-6 py-3'>Arrival Date</th>
                           <th scope='col' className='px-6 py-3'>Route</th>
                           <th scope='col' className='px-6 py-3'>Vehicle Type</th>
                           <th scope='col' className='px-6 py-3'>Funding</th>
-                          <th scope='col' className='px-6 py-3'>HOD</th>
                           <th scope='col' className='px-6 py-3'>Passengers</th>
                           <th scope='col' className='px-6 py-3'>Status</th>
                           <th scope='col' className='px-6 py-3'>Action</th>                                        
