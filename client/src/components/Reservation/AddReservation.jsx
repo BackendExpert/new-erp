8d84dee21fa01427db94ffe539668cab32c19533
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 08:30:16 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -111,150 +111,156 @@ const AddReservation = () => {
       const empUsername = empName.username
       const empRole = empName.role
       
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">New Reservation</h1>        
-            <hr className="mb-4" />
-            <div className="flex">
-              <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-              {
-                (() => {
-                  if(RoleUser === "HOD"){
-                    return (
-                      <Link to={'/HODRecRese'}>
-                        <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
-                      </Link>
-                    )
-                  }
-                })()
-              }
-            </div>
-            
-            <div className="my-4">
-                <form onSubmit={headleSubmit}>
-                    <div className="lg:grid grid-cols-3 gap-4">
-                        <div className="my-2">
-                            <label htmlFor="">Employee Name : </label>
-                            <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Employee Name"
-                            value={empUsername} onChange={e => SetAddRese({...AddRese, Name:e.target.value})}/>
-
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Start Time : </label>
-                            <input type="time" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Start Time"
-                            onChange={e => SetAddRese({...AddRese, Time:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Location and Route : </label>
-                            <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Location and Route"
-                            onChange={e => SetAddRese({...AddRese, Location:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Email : </label>
-                            <input type="email" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Your Email"
-                            value={EmailUser} onChange={e => SetAddRese({...AddRese, Email:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">HOD Email : </label>
-                            <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
-                              onChange={e => SetAddRese({...AddRese, HoDEmail:e.target.value})}>
-                                  <option>Select Option</option>
-                                  {
-                                    emailHOD.map((hod) => {
-                                      return(
-                                        <option value={hod.email}>{hod.email}</option>
-                                      )
-                                    })
-                                  }
-                            </select>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Start Date : </label>
-                            <input type="date" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Start Date"
-                            onChange={e => SetAddRese({...AddRese, StartDate:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">End Date : </label>
-                            <input type="date" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter End Date"
-                            onChange={e => SetAddRese({...AddRese, EndDate:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Travel Mode : </label>
-                            <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
-                            onChange = {e => SetAddRese({...AddRese, Mode:e.target.value})}>
-                                <option>Select Option</option>
-                                <option value="Official">Official</option>
-                                <option value="Air">Air</option>
-                                <option value="Field">Field</option>
-                                <option value="Private Trip">Private Trip</option>
-                                <option value="Other">Other</option>
-
-                            </select>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Other Travellers : </label>
-                            <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Other Travellers"
-                            onChange={e => SetAddRese({...AddRese, Passengers:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Purpose : </label>
-                            <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Purpose"
-                            onChange={e => SetAddRese({...AddRese, Purpose:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Designation : </label>
-                            <input type="text" disabled name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Other Travellers"
-                            value={empName.role} onChange={e => SetAddRese({...AddRese, Designation:e.target.value})}/>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Division : </label>
-                            <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
-                              onChange={e => SetAddRese({...AddRese, Division:e.target.value})}>
-                                  <option>Select Option</option>
-                                  {
-                                    userDivision.map((divi) => {
-                                      return(
-                                        <option value={divi.did}>{divi.title}</option>
-                                      )
-                                    })
-                                  }
-                            </select>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Vehicle Type : </label>
-                            <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
-                            onChange = {e => SetAddRese({...AddRese, Vehicle:e.target.value})}>
-                                <option>Select Option</option>
-                                <option value="Van">Van</option>
-                                <option value="Cab">Cab</option>
-                                <option value="Car">Car</option>
-                                <option value="Three Wheeler">Three Wheeler</option>
-                                <option value="Other">Other</option>
-
-                            </select>
-                        </div>
-                        <div className="my-2">
-                            <label htmlFor="">Funding Source : </label>
-                            <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
-                            onChange = {e => SetAddRese({...AddRese, Funding:e.target.value})}>
-                                <option>Select Option</option>
-                                <option value="NIFS Funds">NIFS Funds</option>
-                                <option value="Grant">Grant</option>
-                                <option value="Earning">Earning</option>
-                                <option value="Private">Private</option>
-
-                            </select>
-                        </div>
-                    </div>
-                    <div className="">
-                        <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Reservation</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-  )
+      if(RoleUser != null){
+        return (
+          <div className="bg-gray-200 py-4">
+              <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                  <h1 className="text-xl font-semibold">New Reservation</h1>        
+                  <hr className="mb-4" />
+                  <div className="flex">
+                    <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    {
+                      (() => {
+                        if(RoleUser === "HOD"){
+                          return (
+                            <Link to={'/HODRecRese'}>
+                              <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
+                            </Link>
+                          )
+                        }
+                      })()
+                    }
+                  </div>
+                  
+                  <div className="my-4">
+                      <form onSubmit={headleSubmit}>
+                          <div className="lg:grid grid-cols-3 gap-4">
+                              <div className="my-2">
+                                  <label htmlFor="">Employee Name : </label>
+                                  <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Employee Name"
+                                  value={empUsername} onChange={e => SetAddRese({...AddRese, Name:e.target.value})}/>
+      
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Start Time : </label>
+                                  <input type="time" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Start Time"
+                                  onChange={e => SetAddRese({...AddRese, Time:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Location and Route : </label>
+                                  <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Location and Route"
+                                  onChange={e => SetAddRese({...AddRese, Location:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Email : </label>
+                                  <input type="email" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Your Email"
+                                  value={EmailUser} onChange={e => SetAddRese({...AddRese, Email:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">HOD Email : </label>
+                                  <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetAddRese({...AddRese, HoDEmail:e.target.value})}>
+                                        <option>Select Option</option>
+                                        {
+                                          emailHOD.map((hod) => {
+                                            return(
+                                              <option value={hod.email}>{hod.email}</option>
+                                            )
+                                          })
+                                        }
+                                  </select>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Start Date : </label>
+                                  <input type="date" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Start Date"
+                                  onChange={e => SetAddRese({...AddRese, StartDate:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">End Date : </label>
+                                  <input type="date" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter End Date"
+                                  onChange={e => SetAddRese({...AddRese, EndDate:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Travel Mode : </label>
+                                  <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
+                                  onChange = {e => SetAddRese({...AddRese, Mode:e.target.value})}>
+                                      <option>Select Option</option>
+                                      <option value="Official">Official</option>
+                                      <option value="Air">Air</option>
+                                      <option value="Field">Field</option>
+                                      <option value="Private Trip">Private Trip</option>
+                                      <option value="Other">Other</option>
+      
+                                  </select>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Other Travellers : </label>
+                                  <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Other Travellers"
+                                  onChange={e => SetAddRese({...AddRese, Passengers:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Purpose : </label>
+                                  <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Purpose"
+                                  onChange={e => SetAddRese({...AddRese, Purpose:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Designation : </label>
+                                  <input type="text" disabled name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter Other Travellers"
+                                  value={empName.role} onChange={e => SetAddRese({...AddRese, Designation:e.target.value})}/>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Division : </label>
+                                  <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
+                                    onChange={e => SetAddRese({...AddRese, Division:e.target.value})}>
+                                        <option>Select Option</option>
+                                        {
+                                          userDivision.map((divi) => {
+                                            return(
+                                              <option value={divi.did}>{divi.title}</option>
+                                            )
+                                          })
+                                        }
+                                  </select>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Vehicle Type : </label>
+                                  <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
+                                  onChange = {e => SetAddRese({...AddRese, Vehicle:e.target.value})}>
+                                      <option>Select Option</option>
+                                      <option value="Van">Van</option>
+                                      <option value="Cab">Cab</option>
+                                      <option value="Car">Car</option>
+                                      <option value="Three Wheeler">Three Wheeler</option>
+                                      <option value="Other">Other</option>
+      
+                                  </select>
+                              </div>
+                              <div className="my-2">
+                                  <label htmlFor="">Funding Source : </label>
+                                  <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
+                                  onChange = {e => SetAddRese({...AddRese, Funding:e.target.value})}>
+                                      <option>Select Option</option>
+                                      <option value="NIFS Funds">NIFS Funds</option>
+                                      <option value="Grant">Grant</option>
+                                      <option value="Earning">Earning</option>
+                                      <option value="Private">Private</option>
+      
+                                  </select>
+                              </div>
+                          </div>
+                          <div className="">
+                              <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Reservation</button>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+        )
+      }
+      else{
+        localStorage.clear()
+        navigate('/')
+      }
 }
 
 export default AddReservation
