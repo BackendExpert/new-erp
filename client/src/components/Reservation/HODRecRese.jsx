994df41557467f613f6bea34fe32be46a9b91fcc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 06:39:32 +0530

    Update HODRecRese.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Reservation/HODRecRese.jsx
+++ client/src/components/Reservation/HODRecRese.jsx
@@ -32,6 +32,34 @@ const HODRecRese = () => {
             <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
           </div>
           <div className="">{buttonValue}</div>
+          <div className="relative overflow-x-auto my-8">
+                    <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                        <tr className='text-blue-500'>
+                          <th scope='col' className='px-6 py-3'>Reservation ID</th>
+                          <th scope='col' className='px-6 py-3'>Name</th>
+                          <th scope='col' className='px-6 py-3'>Designation</th>                            
+                          <th scope='col' className='px-6 py-3'>Email</th>
+                          <th scope='col' className='px-6 py-3'>Purpose</th>
+                          <th scope='col' className='px-6 py-3'>Division No</th>                                        
+                          <th scope='col' className='px-6 py-3'>Starting Time</th>
+                          <th scope='col' className='px-6 py-3'>Date</th>
+                          <th scope='col' className='px-6 py-3'>Arrival Date</th>
+                          <th scope='col' className='px-6 py-3'>Route</th>
+                          <th scope='col' className='px-6 py-3'>Vehicle Type</th>
+                          <th scope='col' className='px-6 py-3'>Funding</th>
+                          <th scope='col' className='px-6 py-3'>HOD</th>
+                          <th scope='col' className='px-6 py-3'>Passengers</th>
+                          <th scope='col' className='px-6 py-3'>Status</th>
+                          <th scope='col' className='px-6 py-3'>Action</th>                                        
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                  </table>
+                </div>
+
       </div>
     </div>
   )
