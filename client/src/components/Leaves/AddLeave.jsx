f698dded0dc627d120065167e732cfc97a69f22b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 13:58:06 +0530

    Update AddLeave.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Leaves/AddLeave.jsx
+++ client/src/components/Leaves/AddLeave.jsx
@@ -79,101 +79,107 @@ const AddLeave = () => {
 
 
 
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">New Leave Request</h1>        
-            <hr className="mb-4" />
-            <div className="flex">
-            <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-            {
-              (() => {
-                if(RoleUser === "HOD"){
-                  return (
-                    <Link to={'/HODRecLeaves'}>
-                        <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">For Recommended</button>
-                    </Link>
-                  )
+    if(RoleUser != null){
+      return (
+        <div className="bg-gray-200 py-4">
+            <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                <h1 className="text-xl font-semibold">New Leave Request</h1>        
+                <hr className="mb-4" />
+                <div className="flex">
+                <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                {
+                  (() => {
+                    if(RoleUser === "HOD"){
+                      return (
+                        <Link to={'/HODRecLeaves'}>
+                            <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">For Recommended</button>
+                        </Link>
+                      )
+                    }
+                    else{
+                      return (
+                        <div className=""></div>
+                      )
+                    }
+                  })()
                 }
-                else{
-                  return (
-                    <div className=""></div>
-                  )
-                }
-              })()
-            }
+                </div>
+                <div className="my-2">
+                  <form onSubmit={headleSubmit}>
+                    <div className="lg:grid grid-cols-3 gap-4">
+    
+                      <div className="">
+                        <label htmlFor="">Start Time</label>
+                        <input type="time" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Time"
+                        onChange={e => SetLeaveData({...LeaveData, StartTime:e.target.value})}/>
+                      </div>
+    
+                      <div className="">
+                        <label htmlFor="">Email</label>
+                        <input type="email" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder=""
+                        value={EmailUser} onChange={e => SetLeaveData({...LeaveData, Email:e.target.value})}/>
+                      </div>
+    
+                      <div className="">
+                        <label htmlFor="">HOD Email</label>
+                        <input type="email" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2"  placeholder="HOD Email"
+                        onChange={e => SetLeaveData({...LeaveData, HoDEmail:e.target.value})}/>
+                      </div>
+    
+                    </div>
+                    <div className="lg:grid grid-cols-2 gap-4">
+    
+                      <div className="">
+                        <label htmlFor="">Leave Type</label>
+                        <select className="w-full h-12 border border-blue-400 rounded pl-2 my-2"
+                          onChange={e => SetLeaveData({...LeaveData, Type:e.target.value})}>
+                            <option>Select Option</option>
+                            <option value="Duty">Duty</option>
+                            <option value="Casual">Casual</option>
+                            <option value="Sick">Duty</option>
+                            <option value="Casual Half Day">Casual Half Day</option>
+                            <option value="Sick Half Day">Sick Half Day</option>
+                            <option value="Short Leave">Short Leave</option>
+                            <option value="Lieu">Lieu</option>
+                            <option value="Other">Other</option>
+                        </select>
+                      </div>     
+    
+                      <div className="">
+                        <label htmlFor="">Start Date</label>
+                        <input type="date" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Date"
+                        onChange={e => SetLeaveData({...LeaveData, StartDate:e.target.value})}/>
+                      </div>
+    
+                    </div>
+                    <div className="lg:grid grid-cols-2 gap-4">
+    
+                      <div className="">
+                        <label htmlFor="">End Date</label>
+                        <input type="date" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="End Date"
+                        onChange={e => SetLeaveData({...LeaveData, EndDate:e.target.value})}/>
+                      </div>
+    
+                      <div className="">
+                        <label htmlFor="">Dutarion</label>
+                        <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Dutarion"
+                        onChange={e => SetLeaveData({...LeaveData, Duration:e.target.value})}/>
+                      </div>
+    
+                    </div>
+                    <div className="">
+                      <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Leave</button>
+                    </div>
+                  </form>
+                </div>                
             </div>
-            <div className="my-2">
-              <form onSubmit={headleSubmit}>
-                <div className="lg:grid grid-cols-3 gap-4">
-
-                  <div className="">
-                    <label htmlFor="">Start Time</label>
-                    <input type="time" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Time"
-                    onChange={e => SetLeaveData({...LeaveData, StartTime:e.target.value})}/>
-                  </div>
-
-                  <div className="">
-                    <label htmlFor="">Email</label>
-                    <input type="email" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder=""
-                    value={EmailUser} onChange={e => SetLeaveData({...LeaveData, Email:e.target.value})}/>
-                  </div>
-
-                  <div className="">
-                    <label htmlFor="">HOD Email</label>
-                    <input type="email" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2"  placeholder="HOD Email"
-                    onChange={e => SetLeaveData({...LeaveData, HoDEmail:e.target.value})}/>
-                  </div>
-
-                </div>
-                <div className="lg:grid grid-cols-2 gap-4">
-
-                  <div className="">
-                    <label htmlFor="">Leave Type</label>
-                    <select className="w-full h-12 border border-blue-400 rounded pl-2 my-2"
-                      onChange={e => SetLeaveData({...LeaveData, Type:e.target.value})}>
-                        <option>Select Option</option>
-                        <option value="Duty">Duty</option>
-                        <option value="Casual">Casual</option>
-                        <option value="Sick">Duty</option>
-                        <option value="Casual Half Day">Casual Half Day</option>
-                        <option value="Sick Half Day">Sick Half Day</option>
-                        <option value="Short Leave">Short Leave</option>
-                        <option value="Lieu">Lieu</option>
-                        <option value="Other">Other</option>
-                    </select>
-                  </div>     
-
-                  <div className="">
-                    <label htmlFor="">Start Date</label>
-                    <input type="date" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Date"
-                    onChange={e => SetLeaveData({...LeaveData, StartDate:e.target.value})}/>
-                  </div>
-
-                </div>
-                <div className="lg:grid grid-cols-2 gap-4">
-
-                  <div className="">
-                    <label htmlFor="">End Date</label>
-                    <input type="date" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="End Date"
-                    onChange={e => SetLeaveData({...LeaveData, EndDate:e.target.value})}/>
-                  </div>
-
-                  <div className="">
-                    <label htmlFor="">Dutarion</label>
-                    <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Dutarion"
-                    onChange={e => SetLeaveData({...LeaveData, Duration:e.target.value})}/>
-                  </div>
-
-                </div>
-                <div className="">
-                  <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Leave</button>
-                </div>
-              </form>
-            </div>                
         </div>
-    </div>
-  )
+      )
+    }
+    else{
+      localStorage.clear()
+      navigate('/')
+    }
 }
 
 export default AddLeave
