218c665482acc6ae7a3a90214d0234a2c0ae502e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-11 13:33:36 +0530

    Update SummaryDash.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/SummaryDash.jsx
+++ client/src/components/SummaryDash/SummaryDash.jsx
@@ -243,7 +243,7 @@ const SummaryDash = () => {
     {id: 22, name:"Leave Requests" , value: <CountUp end={HODRecLeaves}/>, icons: <Icons name="log-out" size="large"/>, style:"hover:border-yellow-500 hover:text-yellow-600" },  
     {id: 23, name:"Reservation Requests" , value: <CountUp end={HODRecRese}/>, icons: <Icons name="car" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },  
     {id: 24, name:"Scientists" , value: <CountUp end={CountScientist}/>, icons: <Icons name="search" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },  
-    {id: 25, name:"Research Assistant" , value: <CountUp end={CountRA}/>, icons: <Icons name="school" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },  
+    {id: 25, name:"Research Assistants" , value: <CountUp end={CountRA}/>, icons: <Icons name="school" size="large"/>, style:"hover:border-green-500 hover:text-green-600" },  
   ]
   
   // hod data
