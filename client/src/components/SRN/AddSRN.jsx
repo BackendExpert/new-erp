03a2284f0c7ea7700d4d47ec560f41237f43976d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 06:57:08 +0530

    Update AddSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/AddSRN.jsx
+++ client/src/components/SRN/AddSRN.jsx
@@ -225,6 +225,9 @@ const AddSRN = () => {
                                     onChange = {e => SetDataSRN({...DataSRN, Description:e.target.value})} />      
                                 </div>
                             </div>
+                            <div className="">
+                                <button type="submit" className="rounded text-green-500 border border-green-500 py-4 px-16 my-2 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request SRN</button>
+                            </div>
                         </form>
                     </div>
                 </div>
