f9c422ffb60a082737aaf320bc77484538273293
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 09:54:23 +0530

    Update AssignSRNNo.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/AssignSRNNo.jsx
+++ client/src/components/SRN/AssignSRNNo.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AssignSRNNo = () => {
-  return (
-    <div>AssignSRNNo</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    if(RoleUser === "Labmanager"){
+        return (
+            <div>AssignSRNNo</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default AssignSRNNo
