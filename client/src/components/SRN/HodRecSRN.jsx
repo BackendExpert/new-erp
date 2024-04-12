af3231aca0ec2fba46d50b81c2666ddfce26aa71
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 07:45:57 +0530

    Update HodRecSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/HodRecSRN.jsx
+++ client/src/components/SRN/HodRecSRN.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const HodRecSRN = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+    
   return (
     <div>HodRecSRN</div>
   )
