1fb39af7ff52616efa7484c004f5eecf8892e2d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 19:07:18 +0530

    Update AddWorkReq.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/WorkRequest/AddWorkReq.jsx
+++ client/src/components/WorkRequest/AddWorkReq.jsx
@@ -1,6 +1,58 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddWorkReq = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    const headleBack = () => {
+        if(RoleUser === "SuperAdmin"){
+          navigate('/superAdmin');
+        }
+        else if(RoleUser === "Admin"){
+          navigate('/admin');
+        }
+        else if(RoleUser === "HOD"){
+          navigate('/hod');
+        }      
+        else if(RoleUser === "TO"){
+          navigate('/to');
+        }  
+        else if(RoleUser === "Librarian"){
+          navigate('/librarian');
+        } 
+        else if(RoleUser === "Labmanager"){
+          navigate('/labManager');
+        }     
+        else if(RoleUser === "Accountant"){
+          navigate('/accountant');
+        } 
+        else if(RoleUser === "User"){
+          navigate('/user');
+        } 
+        else if(RoleUser === "RA"){
+          navigate('/RADash');
+        } 
+        else if(RoleUser === "Scientist"){
+          navigate('/ScientistsDash');
+        } 
+        else if(RoleUser === "NonAcademic"){
+          navigate('/NonAcademic');
+        } 
+        else if(RoleUser === "PDFellow"){
+          navigate('/PDFellow');
+        } 
+        else if(RoleUser === "Driver"){
+          navigate('/DriverDash');
+        } 
+      }
+
+
   return (
     <div>AddWorkReq</div>
   )
