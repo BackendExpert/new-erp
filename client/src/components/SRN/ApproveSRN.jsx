17738932e1d03103921f6da0041fe130920fe2a0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 17:25:07 +0530

    Update ApproveSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/ApproveSRN.jsx
+++ client/src/components/SRN/ApproveSRN.jsx
@@ -46,6 +46,19 @@ const ApproveSRN = () => {
         })
     }
 
+    const headleReject = (id) => {
+        axios.post('http://localhost:8081/SRNReject/' + id)
+        .then(res => {
+            if(res.data.Status === "Success"){
+                alert("The SRN Reject Successful")
+                window.location.reload()
+            }
+            else{
+                alert(res.data.Error)
+            }            
+        })
+    }
+
     if(RoleUser === "Director" || RoleUser === "Secretary"){
         return (
             <div className="bg-gray-200 py-4">
