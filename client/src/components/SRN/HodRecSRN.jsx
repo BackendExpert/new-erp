797253aaea1f27f8bc0441f92732c8e81f827056
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 08:25:28 +0530

    Update HodRecSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/HodRecSRN.jsx
+++ client/src/components/SRN/HodRecSRN.jsx
@@ -77,7 +77,13 @@ const HodRecSRN = () => {
                                                     <td className='px-6 py-4 font-bold'>{hodSrn.description}</td>
                                                     <td className='px-6 py-4 font-bold'>
                                                         <span className="py-2 px-4 rounded bg-yellow-500 text-white">{hodSrn.Status}</span>
-                                                    </td>  
+                                                    </td> 
+                                                    <td className='px-6 py-4 font-bold'>
+                                                        <div className="flex">
+                                                            <button  onClick={() => headleRece(hodSrn.RID)} className="ml-2 border border-green-500 rounded py-2 px-4 text-green-500 duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Recommended</button> 
+                                                            <button  onClick={() => headleDenied(hodSrn.RID)} className="ml-2 border border-red-500 rounded py-2 px-4 text-red-500 duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Denied</button>
+                                                        </div>
+                                                    </td>
                                                 </tr>
                                             )
                                         }
