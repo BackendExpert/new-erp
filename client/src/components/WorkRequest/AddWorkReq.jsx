ecef91b7514b1cdfb6798a1fa436efe4f8f9c350
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 19:08:54 +0530

    Update AddWorkReq.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/WorkRequest/AddWorkReq.jsx
+++ client/src/components/WorkRequest/AddWorkReq.jsx
@@ -52,29 +52,35 @@ const AddWorkReq = () => {
         } 
       }
 
-
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">New Work Request</h1>        
-            <hr className="mb-4" />
-            <div className="flex">
-                <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                {
-                    (() => {
-                    if(RoleUser === "HOD"){
-                        return (
-                        <Link>
-                            <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
-                        </Link>
-                        )
-                    }
-                    })()
-                }
+      if(RoleUser !== null){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">New Work Request</h1>        
+                    <hr className="mb-4" />
+                    <div className="flex">
+                        <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        {
+                            (() => {
+                            if(RoleUser === "HOD"){
+                                return (
+                                <Link>
+                                    <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Reservation</button>
+                                </Link>
+                                )
+                            }
+                            })()
+                        }
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+          )
+      }
+    else{
+        localStorage.clear()
+        navigate('/')
+    }
+
 }
 
 export default AddWorkReq
