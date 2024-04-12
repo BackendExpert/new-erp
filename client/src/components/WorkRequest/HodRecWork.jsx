8561349ceb071d6eddf0e4620c5382f1c423b21f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 20:04:58 +0530

    Update HodRecWork.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/WorkRequest/HodRecWork.jsx
+++ client/src/components/WorkRequest/HodRecWork.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 const HodRecWork = () => {
-  return (
-    <div>HodRecWork</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+    if(RoleUser === "HOD"){
+        return (
+            <div>HodRecWork</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default HodRecWork
