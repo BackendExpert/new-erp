bb8bd705fa8ffd88b70cd542bbb5e127020196ac
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 09:58:55 +0530

    Update AssignSRNNo.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/AssignSRNNo.jsx
+++ client/src/components/SRN/AssignSRNNo.jsx
@@ -10,6 +10,13 @@ const AssignSRNNo = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+    // send data to backend
+    const [SRNnumber, SetSRNnumber] = useState({
+        SRNNum: '',
+        CData: ''
+    })
+
+
     if(RoleUser === "Labmanager"){
         return (
             <div className="bg-gray-200 py-4">
@@ -20,6 +27,22 @@ const AssignSRNNo = () => {
                         <Link to={'/ProcessSRN'}>
                             <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                         </Link>
+                    </div>
+                    <div className="my-4">
+                        <form>
+                            <div className="lg:grid grid-cols-2 gap-4">
+                                <div className="my-2">
+                                    <label htmlFor="">SRN Number : </label>
+                                    <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter SRN Number"
+                                    onChange = {e => SetSRNnumber({...SRNnumber, SRNNum:e.target.value})}/>      
+                                </div>
+                                <div className="my-2">
+                                    <label htmlFor="">Completion Data</label>
+                                    <input type="text" name="" id="" className="w-full h-12 border border-blue-500 rounded pl-2 my-2" required placeholder="Enter SRN Number"
+                                    onChange = {e => SetSRNnumber({...SRNnumber, CData:e.target.value})}/>       
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
