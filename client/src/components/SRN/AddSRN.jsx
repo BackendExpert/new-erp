7c6e27df8c2c6dbe62b8d943aa7b6da23964a06c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 06:40:23 +0530

    Update AddSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/AddSRN.jsx
+++ client/src/components/SRN/AddSRN.jsx
@@ -154,7 +154,7 @@ const AddSRN = () => {
                                     value={SRNHOD}/>      
                                 </div>
                                 <div className="my-2">
-                                    <label htmlFor="">Project : </label>
+                                    <label htmlFor="">SRN Type : </label>
                                     <select className="mt-2 w-full h-12 border border-blue-400 rounded pl-2"
                                         onChange={e => SetAddRese({...AddRese, HoDEmail:e.target.value})}>
                                             <option>Select Option</option>
