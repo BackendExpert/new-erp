3bfe04eecc88d2b8e08296af34591278eefe47fc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-12 11:09:04 +0530

    Update ApproveSRN.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SRN/ApproveSRN.jsx
+++ client/src/components/SRN/ApproveSRN.jsx
@@ -10,9 +10,27 @@ const ApproveSRN = () => {
     //get current login user's email
     const EmailUser = secureLocalStorage.getItem("logiafter");
 
+        //GoBack
+        const GoBack = () => {
+            if(RoleUser === "Director"){
+                navigate('/DirectorDash');
+            }
+            else if(RoleUser === "Secretary"){
+                navigate('/Secretary');
+            }
+        }
+
     if(RoleUser === "Director" || RoleUser === "Secretary"){
         return (
-            <div>ApproveSRN</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Approve SRN</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">                
+                        <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
