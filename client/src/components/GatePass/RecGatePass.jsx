59ad760bbb1aa6f7732aa76f615a08b2fe9470c0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 18:04:11 +0530

    Update RecGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/RecGatePass.jsx
+++ client/src/components/GatePass/RecGatePass.jsx
@@ -15,9 +15,36 @@ const RecGatePass = () => {
         SetButtonValue(clickValue)   
     }
 
+            //GoBack
+    const GoBack = () => {
+        if(RoleUser === "Director"){
+            navigate('/DirectorDash');
+        }
+        else if(RoleUser === "Secretary"){
+            navigate('/Secretary');
+        }
+        else if(RoleUser === "TO"){
+            navigate('/to');
+        }
+    }
+
     if(RoleUser === "SuperAdmin" || RoleUser === "TO" || RoleUser === "Director" || RoleUser === "Secretary"){
         return (
-            <div>RecGatePass</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">GatePass</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">                
+                        <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                    </div>
+                    <div className="flex pl-2 my-4">
+                        <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Leaves</button>
+                        <button onClick={() => HeadleButtonClick('Denied')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
+                        <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
+                    </div>
+                    <div className="">{buttonValue}</div>
+                </div>
+            </div>
         )
     }
     else{
