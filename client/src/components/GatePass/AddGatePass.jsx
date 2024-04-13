69a93275671a055273db1a0155da9b1172270413
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:30:54 +0530

    Update AddGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/AddGatePass.jsx
+++ client/src/components/GatePass/AddGatePass.jsx
@@ -60,23 +60,30 @@ const AddGatePass = () => {
                 <h1 className="text-xl font-semibold">New Gate Pass Request</h1>        
                 <hr className="mb-4" />
                 <div className="flex">
-                <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
-                {
-                  (() => {
-                    if(RoleUser === "HOD"){
-                      return (
-                        <Link>
-                            <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Gate Pass</button>
-                        </Link>
-                      )
+                    <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                    {
+                    (() => {
+                        if(RoleUser === "HOD"){
+                        return (
+                            <Link>
+                                <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Gate Pass</button>
+                            </Link>
+                        )
+                        }
+                        else{
+                        return (
+                            <div className=""></div>
+                        )
+                        }
+                    })()
                     }
-                    else{
-                      return (
-                        <div className=""></div>
-                      )
-                    }
-                  })()
-                }
+                </div>
+                <div className="my-4">
+                    <form>
+                        <div className="my-2">
+                            
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
