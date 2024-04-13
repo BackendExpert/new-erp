5aec3edee72f6cb9840ccdcceeffc4869ed011bb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:41:06 +0530

    Update AddGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/AddGatePass.jsx
+++ client/src/components/GatePass/AddGatePass.jsx
@@ -153,6 +153,11 @@ const AddGatePass = () => {
                                 <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Time"
                                 value={MyDiviSRN} onChange={e => SetGatePass({...GatePass, Division:e.target.value})}/>
                             </div>
+                            <div className="my-2">
+                                <label htmlFor="">Hod Email</label>
+                                <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Start Time"
+                                value={SRNHOD} onChange={e => SetGatePass({...GatePass, HoDEmail:e.target.value})}/>
+                            </div>
                         </div>
                     </form>
                 </div>
