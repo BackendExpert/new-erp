36988283d95eb17c555d2bdf5da9407d74bc04ae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 07:46:58 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -444,6 +444,34 @@ const MyFullStats = () => {
                                 </tbody>
                             </table>
                         </div>
+
+
+                        <div className="text-xl mt-6 my-2 font-semibold">
+                          My SRN Requests 
+                        </div> 
+                      <div className="relative overflow-x-auto my-8">
+                            <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                                    <tr className='text-blue-500'>
+                                      <th scope='col' className='px-6 py-3'>SRN ID</th>
+                                      <th scope='col' className='px-6 py-3'>Name</th>
+                                      <th scope='col' className='px-6 py-3'>Email</th>
+                                      <th scope='col' className='px-6 py-3'>Date</th>                                    
+                                      <th scope='col' className='px-6 py-3'>SRN Type</th>
+                                      <th scope='col' className='px-6 py-3'>Purchase Type</th>
+                                      <th scope='col' className='px-6 py-3'>Item Type</th>
+                                      <th scope='col' className='px-6 py-3'>Estimate</th>
+                                      <th scope='col' className='px-6 py-3'>Vote</th>
+                                      <th scope='col' className='px-6 py-3'>Description</th>                              
+                                      <th scope='col' className='px-6 py-3'>Status</th>
+                                      <th scope='col' className='px-6 py-3'>Action</th> 
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>                        
                 </div>
             </div>
         )
