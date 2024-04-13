0caca2798c543b9bdf6078d17f998d764bc2f2d2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 07:06:17 +0530

    Update ApproveWork.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/WorkRequest/ApproveWork.jsx
+++ client/src/components/WorkRequest/ApproveWork.jsx
@@ -15,16 +15,24 @@ const ApproveWork = () => {
         SetButtonValue(clickValue)   
     }
 
+            //GoBack
+    const GoBack = () => {
+        if(RoleUser === "Director"){
+            navigate('/DirectorDash');
+        }
+        else if(RoleUser === "Secretary"){
+            navigate('/Secretary');
+        }
+    }
+
     if(RoleUser === "Director" || RoleUser === "Secretary"){
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Recommended Work Requests</h1>        
+                    <h1 className="text-xl font-semibold">Approve Work Requests</h1>        
                     <hr className="mb-4" />
                     <div className="lg:flex">                
-                        <Link to={'/AddWorkReq'}>
-                            <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
-                        </Link>
+                        <button onClick={GoBack} className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
                     </div>
                     <div className="flex pl-2 my-4">
                         <button onClick={() => HeadleButtonClick('Requested')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Work</button>
