492481ef661a5a6857297759d57cef7d10046140
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:48:12 +0530

    Update AddGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/AddGatePass.jsx
+++ client/src/components/GatePass/AddGatePass.jsx
@@ -178,6 +178,11 @@ const AddGatePass = () => {
                                 <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Enter Location"
                                 onChange={e => SetGatePass({...GatePass, Location:e.target.value})}/>
                             </div>
+                            <div className="my-2">
+                                <label htmlFor="">End Location</label>
+                                <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Enter Location"
+                                onChange={e => SetGatePass({...GatePass, Location:e.target.value})}/>
+                            </div>
                         </div>
                     </form>
                 </div>
