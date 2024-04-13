103c22f3023b08d052d910ccc922737b9f27daa5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:23:24 +0530

    Update ScientistSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Scientist/ScientistSide.jsx
+++ client/src/components/Scientist/ScientistSide.jsx
@@ -9,7 +9,7 @@ const ScientistSide = () => {
         {name: "Vehicle Reservations",  link: "/AddReservation", icon: <Icons name="car" size="large"></Icons>},
         {name: "SRN Requests",  link: "/AddSRN", icon: <Icons name="newspaper" size="large"></Icons>},
         {name: "Work Requests",  link: "/AddWorkReq", icon: <Icons name="document-text" size="large"></Icons>},
-        {name: "Gatepass Requests",  link: "#", icon: <Icons name="car" size="large"></Icons>},
+        {name: "Gatepass Requests",  link: "/AddGatePass", icon: <Icons name="car" size="large"></Icons>},
         {name: "Increment Requests",  link: "#", icon: <Icons name="cash" size="large"></Icons>},
     ]
   return (
