40926568531925e57c0dc0a88c2cdad671213975
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 06:35:57 +0530

    Update AssignReqNo.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/WorkRequest/AssignReqNo.jsx
+++ client/src/components/WorkRequest/AssignReqNo.jsx
@@ -14,7 +14,7 @@ const AssignReqNo = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">New Work Request</h1>        
+                    <h1 className="text-xl font-semibold">Assign WorkRequest Number</h1>        
                     <hr className="mb-4" />
                     <div className="flex">
                         <Link to={'/RecWork'}>
