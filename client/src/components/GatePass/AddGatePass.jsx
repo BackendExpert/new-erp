f109992c1fd8d717fab1e3921dcfb1764663dcf9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:30:24 +0530

    Update AddGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/AddGatePass.jsx
+++ client/src/components/GatePass/AddGatePass.jsx
@@ -66,7 +66,7 @@ const AddGatePass = () => {
                     if(RoleUser === "HOD"){
                       return (
                         <Link>
-                            <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">For Recommended</button>
+                            <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Gate Pass</button>
                         </Link>
                       )
                     }
