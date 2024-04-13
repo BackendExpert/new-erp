c26008dda123d52da1f81853eb773da93c48d638
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 18:32:12 +0530

    Update ApproveGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/ApproveGatePass.jsx
+++ client/src/components/GatePass/ApproveGatePass.jsx
@@ -40,6 +40,33 @@ const ApproveGatePass = () => {
                         <button onClick={() => HeadleButtonClick('Approve')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Approve</button>
                     </div>
                     <div className="">{buttonValue}</div>
+                    <div className="relative overflow-x-auto my-8">
+                        <table className="text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                          <thead className="text-xs text-gray-700 uppercase bg-blue-100 rounded border-t-4 border-blue-200">
+                            <tr className='text-blue-500'>
+                              <th scope='col' className='px-6 py-3'>GatePass ID</th>
+                              <th scope='col' className='px-6 py-3'>Name</th>
+                              <th scope='col' className='px-6 py-3'>Email</th>
+                              <th scope='col' className='px-6 py-3'>Start Date</th>
+                              <th scope='col' className='px-6 py-3'>End Date</th>                                     
+                              <th scope='col' className='px-6 py-3'>Purpose</th>
+                              <th scope='col' className='px-6 py-3'>Start Location</th>
+                              <th scope='col' className='px-6 py-3'>End Location</th> 
+                              <th scope='col' className='px-6 py-3'>Officer</th>
+                              <th scope='col' className='px-6 py-3'>OutSide Officer</th>        
+                              <th scope='col' className='px-6 py-3'>Item</th>                                     
+                              <th scope='col' className='px-6 py-3'>Item Type</th>
+                              <th scope='col' className='px-6 py-3'>Quantity</th>
+                              <th scope='col' className='px-6 py-3'>Invo No</th> 
+                              <th scope='col' className='px-6 py-3'>Status</th>
+                              <th scope='col' className='px-6 py-3'>Action</th>                                        
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                      </table>
+                    </div>
                 </div>
             </div>
         )
