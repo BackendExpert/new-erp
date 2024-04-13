477378f213b8e2e75ea5265fbfef2857b42f2a93
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 18:29:41 +0530

    Update ApproveGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/ApproveGatePass.jsx
+++ client/src/components/GatePass/ApproveGatePass.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const ApproveGatePass = () => {
-  return (
-    <div>ApproveGatePass</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+    
+    if(RoleUser === "Director" || RoleUser === "Secretary"){
+        return (
+            <div>ApproveGatePass</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default ApproveGatePass
