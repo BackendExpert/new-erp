e61cf86e6ba4ed3771b56ff4b8029036017a93a9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 19:31:02 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -5333,5 +5333,25 @@ app.get('/CountReqGatePass/:id', (req, res) => {
     });
 })
 
+// CountRejectGatePass 
+
+app.get('/CountRejectGatePass/:id', (req, res) => {
+    const UserEmail = req.params.id;
+    const sql = "SELECT COUNT(WID) AS MyReqGate FROM workrequest WHERE Status =? || Status =? && Email = ?";
+    // const sql = "SELECT COUNT(eid) AS emp FROM employee";
+    const status = "Reject"
+    const status1 = "HODReject"
+
+    connection.query(sql, [status, status1, UserEmail], (error, results) => {
+    if (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send({ message: 'Error fetching data' });
+        return;
+    }
+
+    res.json({ MyReqGate: results[0].MyReqGate }); // Send count in JSON format
+    });
+})
+
 //check the server is working
 app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
