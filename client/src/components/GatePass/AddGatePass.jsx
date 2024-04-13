af444fc696ba7b31460615d653ac8de22ed6333a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-13 12:27:53 +0530

    Update AddGatePass.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/GatePass/AddGatePass.jsx
+++ client/src/components/GatePass/AddGatePass.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const AddGatePass = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    
   return (
     <div>AddGatePass</div>
   )
