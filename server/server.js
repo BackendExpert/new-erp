d0fc7c14acdf0f51fa952b41d0e4e542f2aa680c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:57:54 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -5464,17 +5464,17 @@ app.get('/HodIncrementRec/:id', (req, res) => {
 // SetUserStatusInc
 app.post('/SetUserStatusInc/:id', (req, res) => {
     const IncID = req.params.id
-    console.log(req.body)
-    
-    // const sql ="UPDATE increment SET attendance = ?, decipline =?, conduct = ? WHERE =?"
-    // connection.query(sql, [req.body.Attendance, req.body.Decipline, req.body.Conduct, IncID], (err, result) => {
-    //     if(err){
-    //         return res.json({Error: "Error on Server"})
-    //     }
-    //     else{
-    //         return res.json({Status: "Success"})
-    //     }
-    // })
+    // console.log(req.body)
+
+    const sql ="UPDATE increment SET attendance = ?, decipline =?, conduct = ? WHERE =?"
+    connection.query(sql, [req.body.Attendance, req.body.Decipline, req.body.Conduct, IncID], (err, result) => {
+        if(err){
+            return res.json({Error: "Error on Server"})
+        }
+        else{
+            return res.json({Status: "Success"})
+        }
+    })
 })
 
 // ------------------------------ Increamet End -------------------------------
