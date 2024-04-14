ce184165f57d3634eb554e2913ffad9b33343eaf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-15 04:01:45 +0530

    Update FineCalculate.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/FineCalculate.jsx
+++ client/src/components/library/BrrowABook/FineCalculate.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate, useParams } from "react-router-dom"
+import axios from "axios";
 
 const FineCalculate = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    const {id} = useParams
+
+    
   return (
-    <div>FineCalculate</div>
+    <div className="bg-gray-200 py-4">
+        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+            <h1 className="text-xl font-semibold">Calculate Fine</h1>        
+            <hr className="mb-4" />
+            <div className="flex">                   
+                <Link to={'/BrrowBook'}>
+                    <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
