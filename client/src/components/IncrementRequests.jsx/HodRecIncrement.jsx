a5e59d624d795b1fdd0da69b837548d77e942751
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:13:49 +0530

    Update HodRecIncrement.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/HodRecIncrement.jsx
+++ client/src/components/IncrementRequests.jsx/HodRecIncrement.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 const HodRecIncrement = () => {
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    const [buttonValue, SetButtonValue] = useState()
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+    
   return (
     <div>HodRecIncrement</div>
   )
