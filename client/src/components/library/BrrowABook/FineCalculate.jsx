70e78b8d966a984d0119ab843053fe5be40ca201
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-15 04:43:17 +0530

    Update FineCalculate.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/FineCalculate.jsx
+++ client/src/components/library/BrrowABook/FineCalculate.jsx
@@ -26,14 +26,22 @@ const FineCalculate = () => {
     const bname = FineData.bname
     const erdate = FineData.erdate
 
+    const [BookValue, SetBookValue] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:8081/GetBookvalue/' + id)
+        .then(res => SetBookValue(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
     const [CalFineData, SetCalFineData] = useState({
         title:'',
         bname:'', 
         borrower: '',
         bid: '',
         value: '',
-        amount: ''
+        rate: ''
     })
+
 
   return (
     <div className="bg-gray-200 py-4">
@@ -51,34 +59,34 @@ const FineCalculate = () => {
                         <div className="my-2">
                             <label htmlFor="">Book Brrower</label>
                             <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Employee Name "
-                            value={bname} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={bname} onChange={e => SetCalFineData({...CalFineData, Name:e.target.value})}/>
                         </div>
                         <div className="my-2">
                             <label htmlFor="">Book Brrower Email</label>
                             <input type="email" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Book Brrower Email "
-                            value={borrower} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={borrower} onChange={e => SetCalFineData({...CalFineData, Name:e.target.value})}/>
                         </div>
                         <div className="my-2">
                             <label htmlFor="">Book Brrow Date</label>
                             <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Book Brrow Date "
-                            value={bdate} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={bdate} onChange={e => SetCalFineData({...CalFineData, Name:e.target.value})}/>
                         </div>
                         <div className="my-2">
                             <label htmlFor="">Book Title</label>
                             <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Book Title "
-                            value={fineBtitle} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={fineBtitle} onChange={e => SetCalFineData({...CalFineData, Name:e.target.value})}/>
                         </div>
                         <div className="my-2">
                             <label htmlFor="">Return Date</label>
                             <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Return Date "
-                            value={erdate} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={erdate} onChange={e => SetCalFineData({...CalFineData, Name:e.target.value})}/>
                         </div>
                     </div>
                     <div className="lg:grid grid-cols-3 gap-4">
                         <div className="my-2">
                             <label htmlFor="">Rate to Calculate Fine</label>
                             <input type="number" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Fine Rate"
-                            value={erdate} onChange={e => SetGatePass({...GatePass, Name:e.target.value})}/>
+                            value={erdate} onChange={e => SetCalFineData({...CalFineData, rate:e.target.value})}/>
                         </div>
                     </div>
                </form>
