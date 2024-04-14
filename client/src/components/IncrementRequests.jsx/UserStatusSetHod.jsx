6a84329004106794f4fe9dfc42f3ef3b963dfd2e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:39:54 +0530

    Update UserStatusSetHod.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
+++ client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
@@ -12,7 +12,17 @@ const UserStatusSetHod = () => {
 
     if(RoleUser === "HOD"){
         return (
-            <div>UserStatusSetHod</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">New Increment Request</h1>        
+                    <hr className="mb-4" />
+                    <div className="flex">
+                        <Link to={'/'}>
+                            <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
