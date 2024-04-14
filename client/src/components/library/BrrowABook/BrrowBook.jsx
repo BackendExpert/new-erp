d327e68e911a4ce0b905986a7861704fd0d1db05
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 21:49:29 +0530

    Update BrrowBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/BrrowBook.jsx
+++ client/src/components/library/BrrowABook/BrrowBook.jsx
@@ -65,7 +65,7 @@ const BrrowBook = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">Leaves for Recommended</h1>        
+                    <h1 className="text-xl font-semibold">List of Books</h1>        
                     <hr className="mb-4" />
                     <div className="flex">                
                         <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
