823858499e2fefd19ba5ff86297ad9a31e3671f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:12:59 +0530

    Update AddIncrement.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/AddIncrement.jsx
+++ client/src/components/IncrementRequests.jsx/AddIncrement.jsx
@@ -125,7 +125,7 @@ const AddIncrement = () => {
                     (() => {
                         if(RoleUser === "HOD"){
                             return (
-                                <Link>
+                                <Link to={'/HodRecIncrement'}>
                                     <button className="ml-4 border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Recommended Increments</button>
                                 </Link>
                             )
