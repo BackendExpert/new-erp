b8198723ce372d85dbce92d9531f83cdf463c41f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:40:06 +0530

    Update UserStatusSetHod.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
+++ client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
@@ -14,7 +14,7 @@ const UserStatusSetHod = () => {
         return (
             <div className="bg-gray-200 py-4">
                 <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-                    <h1 className="text-xl font-semibold">New Increment Request</h1>        
+                    <h1 className="text-xl font-semibold">Set User Status</h1>        
                     <hr className="mb-4" />
                     <div className="flex">
                         <Link to={'/'}>
