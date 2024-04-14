02094798fba59455e1879033b8f1fefadbb4d9f4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 08:24:11 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -5570,7 +5570,21 @@ app.post('/HodRejectInc/:id', (req, res) => {
 
 // CountHodInc
 app.get('/CountHodInc/:id', (req, res) => {
+    const Email = req.params.id;
     
+    const sql = "SELECT COUNT(IID) AS HodGate FROM increment WHERE status = ? && hodemail = ?";
+    // const sql = "SELECT COUNT(eid) AS emp FROM employee";
+    const is_aprove = "Request"
+    
+    connection.query(sql, [is_aprove, Email], (error, results) => {
+      if (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send({ message: 'Error fetching data' });
+        return;
+      }
+  
+      res.json({ HodGate: results[0].HodGate }); // Send count in JSON format
+    });
 })
 
 // ------------------------------ Increamet End -------------------------------
