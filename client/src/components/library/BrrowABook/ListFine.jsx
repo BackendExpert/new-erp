afc3726273d1e1e31d22f06b9e679ee970ad07d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-15 05:17:31 +0530

    Update ListFine.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/ListFine.jsx
+++ client/src/components/library/BrrowABook/ListFine.jsx
@@ -12,7 +12,17 @@ const ListFine = () => {
 
     if(RoleUser === "Librarian"){
         return (
-            <div>ListFine</div>
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Book Brrowl Fine List</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">                
+                        <Link to={'/Librarian'}>
+                            <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
