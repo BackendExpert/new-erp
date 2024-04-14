950b138ceb83a563953515c236d40b5c6e995cd9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:19:20 +0530

    Update HodRecIncrement.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/HodRecIncrement.jsx
+++ client/src/components/IncrementRequests.jsx/HodRecIncrement.jsx
@@ -45,7 +45,7 @@ const HodRecIncrement = () => {
                               <th scope='col' className='px-6 py-3'>Increment Date</th>
                               <th scope='col' className='px-6 py-3'>Salary Step</th> 
                               <th scope='col' className='px-6 py-3'>New salary</th>
-                              <th scope='col' className='px-6 py-3'>OutSide Officer</th>        
+                              <th scope='col' className='px-6 py-3'>attendance</th>        
                               <th scope='col' className='px-6 py-3'>Item</th>                                     
                               <th scope='col' className='px-6 py-3'>Item Type</th>
                               <th scope='col' className='px-6 py-3'>Quantity</th>
