29659e552eaed12a5e538db8150d7f5cce6e31a9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:38:30 +0530

    Update UserStatusSetHod.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
+++ client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const UserStatusSetHod = () => {
-  return (
-    <div>UserStatusSetHod</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    if(RoleUser === "HOD"){
+        return (
+            <div>UserStatusSetHod</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default UserStatusSetHod
