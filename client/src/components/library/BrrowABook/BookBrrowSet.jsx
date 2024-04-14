334908400ac1ce6693324d0c842c91138a3705d5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 22:03:44 +0530

    Update BookBrrowSet.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/BookBrrowSet.jsx
+++ client/src/components/library/BrrowABook/BookBrrowSet.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import secureLocalStorage from "react-secure-storage"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios";
 
 const BookBrrowSet = () => {
-  return (
-    <div>BookBrrowSet</div>
-  )
+    const navigate = useNavigate()
+    //check current login user
+    const RoleUser = secureLocalStorage.getItem("loginNew");
+    //get current login user's email
+    const EmailUser = secureLocalStorage.getItem("logiafter");
+
+    if(RoleUser != null){
+        return (
+            <div>BookBrrowSet</div>
+        )
+    }
+    else{
+        localStorage.clear()
+        navigate('/')
+    }
+
 }
 
 export default BookBrrowSet
