19463d0f41aec54b8ba7a3e972349ca29b1e2d30
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 07:41:59 +0530

    Update UserStatusSetHod.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
+++ client/src/components/IncrementRequests.jsx/UserStatusSetHod.jsx
@@ -17,9 +17,30 @@ const UserStatusSetHod = () => {
                     <h1 className="text-xl font-semibold">Set User Status</h1>        
                     <hr className="mb-4" />
                     <div className="flex">
-                        <Link to={'/'}>
+                        <Link to={'/HodRecIncrement'}>
                             <button className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                         </Link>
+                    </div>
+                    <div className="my-4">
+                        <form>
+                            <div className="lg:grid grid-cols-2 gap-4">
+                                <div className="my-2">
+                                    <label htmlFor="">Employee Attendance</label>
+                                    <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Employee Email "
+                                    value={EmailUser} onChange={e => SetIncrementData({...IncrementData, email:e.target.value})}/>
+                                </div>
+                                <div className="my-2">
+                                    <label htmlFor="">Employee Decipline</label>
+                                    <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Employee Email "
+                                    value={EmailUser} onChange={e => SetIncrementData({...IncrementData, email:e.target.value})}/>
+                                </div>
+                                <div className="my-2">
+                                    <label htmlFor="">Employee Conduct</label>
+                                    <input type="text" required className="w-full h-12 border border-blue-500 rounded pl-2 my-2" placeholder="Employee Email "
+                                    value={EmailUser} onChange={e => SetIncrementData({...IncrementData, email:e.target.value})}/>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
