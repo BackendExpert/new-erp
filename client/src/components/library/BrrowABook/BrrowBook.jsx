070929299640cb117d858694b5030e35a546f8a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 21:49:16 +0530

    Update BrrowBook.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/library/BrrowABook/BrrowBook.jsx
+++ client/src/components/library/BrrowABook/BrrowBook.jsx
@@ -68,7 +68,7 @@ const BrrowBook = () => {
                     <h1 className="text-xl font-semibold">Leaves for Recommended</h1>        
                     <hr className="mb-4" />
                     <div className="flex">                
-
+                        <button onClick={headleBack} className="border py-4 px-16 border-blue-500 rounded font-semibold text-blue-500 duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl">Back</button>
                     </div>
                 </div>
             </div>
