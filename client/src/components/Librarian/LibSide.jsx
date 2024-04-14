85e5c48fcf7670e108cd1972cfd8aee61de9f337
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 22:55:06 +0530

    Update LibSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Librarian/LibSide.jsx
+++ client/src/components/Librarian/LibSide.jsx
@@ -15,7 +15,6 @@ const LibSide = () => {
         {name: "Thesis List",  link: "/Thesis", icon: <Icons name="copy" size="large"></Icons>},
         {name: "Magazine Lists",  link: "/Magazine", icon: <Icons name="newspaper" size="large"></Icons>},
         {name: "Article Requests",  link: "/Articles", icon: <Icons name="document" size="large"></Icons>},
-        {name: "Calculate Fine",  link: "#", icon: <Icons name="cash" size="large"></Icons>},
         {name: "List of Fine",  link: "#", icon: <Icons name="cash" size="large"></Icons>}
     ]
   return (
