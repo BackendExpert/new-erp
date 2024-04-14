2fa1d669495c3721117dd998bad6ed12bc67c114
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-14 08:40:03 +0530

    Update ApproveInc.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/IncrementRequests.jsx/ApproveInc.jsx
+++ client/src/components/IncrementRequests.jsx/ApproveInc.jsx
@@ -15,25 +15,33 @@ const ApproveInc = () => {
         SetButtonValue(clickValue)   
     }
 
-  return (
-    <div className="bg-gray-200 py-4">
-        <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
-            <h1 className="text-xl font-semibold">Recommended Increment Requests</h1>        
-            <hr className="mb-4" />
-            <div className="lg:flex">                
-                <Link to={'/AddIncrement'}>
-                    <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
-                </Link>
+    if(RoleUser === "Director" || RoleUser === "Secretary"){
+        return (
+            <div className="bg-gray-200 py-4">
+                <div className="bg-white my-2 mx-8 py-6 shadow-xl rounded border-b-4 border-blue-400 px-4">
+                    <h1 className="text-xl font-semibold">Recommended Increment Requests</h1>        
+                    <hr className="mb-4" />
+                    <div className="lg:flex">                
+                        <Link to={'/AddIncrement'}>
+                            <button className="lg:my-0 my-2 border border-blue-500 py-3 px-16 rounded text-blue-500 font-semibold duration-500 hover:bg-blue-500 hover:text-white hover:shadow-xl lg:mx-2">Back</button>
+                        </Link>
+                    </div>
+                    <div className="flex pl-2 my-4">
+                        <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Work</button>
+                        <button onClick={() => HeadleButtonClick('Reject')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
+                        <button onClick={() => HeadleButtonClick('Approve')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
+                    </div>
+                    <div className="">{buttonValue}</div>
+                </div>
             </div>
-            <div className="flex pl-2 my-4">
-                <button onClick={() => HeadleButtonClick('Recommend')} className="ml-2 py-2 px-4 border border-yellow-500 text-yellow-500 rounded duration-500 hover:bg-yellow-500 hover:text-white hover:shadow-xl">Request Work</button>
-                <button onClick={() => HeadleButtonClick('Reject')} className="ml-2 py-2 px-4 border border-red-500 text-red-500 rounded duration-500 hover:bg-red-500 hover:text-white hover:shadow-xl">Request Denied</button>
-                <button onClick={() => HeadleButtonClick('Approve')} className="ml-2 py-2 px-4 border border-green-500 text-green-500 rounded duration-500 hover:bg-green-500 hover:text-white hover:shadow-xl">Request Recommend</button>
-            </div>
-            <div className="">{buttonValue}</div>
-        </div>
-    </div>
-  )
+        )
+    }
+    else{
+        useEffect(() => {
+            navigate('/UnAccess');
+        }, [])
+    }
+
 }
 
 export default ApproveInc
