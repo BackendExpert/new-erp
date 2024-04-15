51922f27bf5089959f89f0cf76edb7b29d4d332b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-15 05:38:27 +0530

    Update Designations.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Designation/Designations.jsx
+++ client/src/components/Designation/Designations.jsx
@@ -17,6 +17,9 @@ const Designations = () => {
         else if(RoleUser === "Admin"){
             navigate('/admin');
         }
+        else if(RoleUser === "Accountant"){
+            navigate('/accountant');
+        }
     }
 
     const [designationData, SetData] = useState([]);
