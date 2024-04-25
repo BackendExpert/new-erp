d7dcd2ff60c80a7f9627100481311a6003e62dc7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 10:32:33 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -350,6 +350,29 @@ const MyFullStats = () => {
           });
         }
 
+        const headleDownloadGatepass = () => {
+          axios.get('http://localhost:8081/DownloadCSVGatePass/' + EmailUser , { responseType: 'blob' })
+          .then(res => {
+              // Create a Blob from the response data
+              const blob = new Blob([res.data], { type: 'text/csv' });
+              const url = window.URL.createObjectURL(blob);
+
+              // Create a link element and click it to trigger the download
+              const link = document.createElement('a');
+              link.href = url;
+              link.setAttribute('download', 'myGatePass_'+ EmailUser + '.csv');
+              document.body.appendChild(link);
+              link.click();
+
+              // Clean up resources
+              window.URL.revokeObjectURL(url);
+              document.body.removeChild(link);
+          })
+          .catch(error => {
+            alert(error)
+          });
+        }
+
 
     if(id === EmailUser){
         return (
