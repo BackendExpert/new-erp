ad24e421d9ad8a476983734facced5a2feb94d92
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-25 10:29:53 +0530

    Update MyFullStats.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/SummaryDash/MyFullStats.jsx
+++ client/src/components/SummaryDash/MyFullStats.jsx
@@ -337,7 +337,7 @@ const MyFullStats = () => {
               // Create a link element and click it to trigger the download
               const link = document.createElement('a');
               link.href = url;
-              link.setAttribute('download', 'mysrns_'+ EmailUser + '.csv');
+              link.setAttribute('download', 'myworkRequests_'+ EmailUser + '.csv');
               document.body.appendChild(link);
               link.click();
 
